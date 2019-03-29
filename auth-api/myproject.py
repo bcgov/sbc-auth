acfc4cb708a64e1cd7b5bc51e2d97c7d2b82e752
Saravan PA <saravanpa@gmail.com>
2019-03-29 16:27:41 -0700

    Update myproject.py

--- auth-api/myproject.py
+++ auth-api/myproject.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! testing it All</h1>"
+    return "<h1 style='color:blue'>Hello There! testing it All!!!</h1>"
 
 if __name__ == "__main__":
     application.run()
