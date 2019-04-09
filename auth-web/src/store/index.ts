cf7bf60b531cc15865b254511cd25c89d00ca859
saravankumarpa <saravanpa@gmail.com>
2019-04-09 13:38:37 -0700

    more on Store , start script added -WIP

--- auth-web/src/store/index.ts
+++ auth-web/src/store/index.ts
@@ -4,8 +4,26 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    getters: {},
-    mutations: {},
+
+    strict: process.env.NODE_ENV !== 'production',
+
+    state: {
+        entityNumber: '',
+        passcode: ''
+
+    },
+    getters: {
+        entityNumber: (state) => state.entityNumber,
+        passcode: (state) => state.passcode
+    },
+    mutations: {
+        entityNumber(state, entityNumber) {
+            state.entityNumber = entityNumber;
+        },
+
+        passcode(state, passcode) {
+            state.passcode = passcode;
+        }
+    },
     actions: {}
 });
