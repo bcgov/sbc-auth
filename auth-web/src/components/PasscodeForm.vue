8454b442701232a2be96941b10b6d868d6d775a1
saravankumarpa <saravanpa@gmail.com>
2019-04-09 13:38:37 -0700

    more on Store , start script added -WIP

--- auth-web/src/components/PasscodeForm.vue
+++ auth-web/src/components/PasscodeForm.vue
@@ -1,66 +1,99 @@
 <template>
-  <v-form class="passcode-form">
-    <div class="mb-2">
-      <v-text-field
-        outline
-        prepend-icon="work"
-        label="Enter your Incorporation Number"
-        hint="Example: 123456789"
-        persistent-hint
-        v-model="entityNum"
-        :rules="entityNumRules"
-      ></v-text-field>
-    </div>
-    <div>
-      <v-text-field
-        outline
-        prepend-icon="lock"
-        label="Enter your Passcode"
-        hint="Example: 123456789"
-        persistent-hint
-        v-model="entityPasscode"
-        :rules="entityPasscodeRules"
-      ></v-text-field>
-    </div>
-    <div class="passcode-form__form-btns">
-      <v-btn
-        color="primary"
-        large
-        depressed
-      >Continue to Filing
-      <v-icon right dark>arrow_forward</v-icon>
-      </v-btn>
-    </div>
-  </v-form>
+    <v-form class="passcode-form" ref="form">
+        <div class="mb-2">
+            <v-text-field
+                    :rules="entityNumRules"
+                    hint="Example: 123456789" req
+                    label="Enter your Incorporation Number"
+                    outline
+                    persistent-hint
+                    prepend-icon="work"
+                    v-model="entityNumber"
+            ></v-text-field>
+        </div>
+        <div>
+            <v-text-field
+                    :rules="entityPasscodeRules"
+                    hint="Example: 123456789"
+                    label="Enter your Passcode"
+                    outline
+                    persistent-hint
+                    prepend-icon="lock"
+                    v-model="passcode"
+            ></v-text-field>
+        </div>
+        <div class="passcode-form__form-btns">
+            <v-btn @click="login"
+                   color="primary"
+                   depressed
+                   large
+            >Continue to Filing
+                <v-icon dark right>arrow_forward</v-icon>
+            </v-btn>
+        </div>
+    </v-form>
 </template>
 
 <script>
-export default {
-  name: 'PasscodeForm',
+    export default {
+        name: 'PasscodeForm',
 
-  data: () => ({
-    valid: false,
-    entityNum: '',
-    entityNumRules: [
-      (v) => !!v || 'Incorporation Number is required',
-      (v) => v.length <= 10 || 'Passcode rules here...'
-    ],
-    entityPasscode: '',
-    entityPasscodeRules: [
-      (v) => !!v || 'Passcode is required',
-      (v) => v.length <= 10 || 'Passcode rules here...'
-    ]
-  })
-};
+
+        data: () => ({
+            valid: false,
+            entityNumRules: [
+                (v) => !!v || 'Incorporation Number is required',
+                (v) => v.length <= 10 || 'Passcode rules here...'
+            ],
+            entityPasscodeRules: [
+                (v) => !!v || 'Passcode is required',
+                (v) => v.length <= 10 || 'Passcode rules here...'
+            ]
+        }),
+
+
+        computed: {
+            entityNumber: {
+                get() {
+                    return this.$store.state.entityNumber;
+                },
+                set(value) {
+                    this.$store.commit('entityNumber', value);
+                }
+            },
+            passcode: {
+                get() {
+                    return this.$store.state.passcode;
+                },
+                set(value) {
+                    this.$store.commit('passcode', value);
+                }
+            }
+
+
+        },
+        methods: {
+            login: () => {
+                if (this.$refs.form.validate()) {
+                    console.log('Login  called -Valid');
+                } else {
+                    console.log('Login  called -Invalid');
+                }
+            }
+
+
+        }
+
+    };
 </script>
 
 <style lang="stylus" scoped>
-  @import "../assets/styl/theme.styl"
+    @import "../assets/styl/theme.styl"
 
-  .v-input
-    max-width 25rem
+    .v-input
+        max-width 25rem
 
-  .passcode-form__form-btns
-    text-align center
+    .passcode-form__form-btns
+        text-align center
 
 </style>
