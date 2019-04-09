640dda7792f304e91f57c8723f5f1d2af1e799bf
saravankumarpa <saravanpa@gmail.com>
2019-04-09 14:28:45 -0700

    Jest coverage added

--- auth-web/tests/unit/example.spec.ts
+++ auth-web/tests/unit/example.spec.ts
@@ -1,4 +1,3 @@
-import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
@@ -6,8 +5,8 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     });
-    expect(wrapper.text()).to.include(msg);
+    expect(wrapper.text()).toMatch(msg);
   });
 });
