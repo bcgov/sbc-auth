59c7ee1b9b847c38442d30a22de89396a85418d7
saravankumarpa <saravanpa@gmail.com>
2019-04-10 15:47:46 -0700

    Jest coverage added

---
+++ auth-web/tests/unit/passcodeForm.spec.ts
@@ -0,0 +1,33 @@
+import PasscodeForm from '@/components/PasscodeForm.vue';
+import Vuex from 'vuex';
+import {mount, createLocalVue} from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+describe('PasscodeForm.vue', () => {
+
+  it('passcode screen', () => {
+
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    const store = new Vuex.Store({
+      state: {
+        entityNumber: '',
+        passcode: ''
+      }
+    });
+
+    const wrapper = mount(PasscodeForm, {
+      store,
+      localVue
+    });
+
+     expect(wrapper.find('.signinbtn').text().startsWith('Sign in')).toBeTruthy();
+    expect(wrapper.isVueInstance()).toBeTruthy();
+
+  });
+
+});
