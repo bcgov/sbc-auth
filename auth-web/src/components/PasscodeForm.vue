9cf4bf8a84cd18ef6f1b171f973c9f3298c32753
Jeremy Dunsdon <twenty15creative@gmail.com>
2019-04-10 11:45:30 -0700

    UI Development

--- auth-web/src/components/PasscodeForm.vue
+++ auth-web/src/components/PasscodeForm.vue
@@ -1,104 +1,117 @@
 <template>
+  <div>
     <v-form class="passcode-form" ref="form">
-        <div class="mb-2">
-            <v-text-field
-                    :rules="entityNumRules"
-                    hint="Example: 123456789" req
-                    label="Enter your Incorporation Number"
-                    outline
-                    persistent-hint
-                    prepend-icon="work"
-                    v-model="entityNumber"
-            ></v-text-field>
-        </div>
-        <div>
-            <v-text-field
-                    :rules="entityPasscodeRules"
-                    hint="Example: 123456789"
-                    label="Enter your Passcode"
-                    outline
-                    persistent-hint
-                    prepend-icon="lock"
-                    v-model="passcode"
-            ></v-text-field>
-        </div>
-        <div class="passcode-form__form-btns">
-            <v-btn @click="login"
-                   color="primary"
-                   depressed
-                   large
-            >Continue to Filing
-                <v-icon dark right>arrow_forward</v-icon>
-            </v-btn>
-        </div>
+      <div class="passcode-form__row">
+        <v-text-field
+          box
+          label="Enter your Incorporation Number"
+          hint="Example: 123456789" req
+          persistent-hint
+          :rules="entityNumRules"
+          v-model="entityNumber"
+        ></v-text-field>
+      </div>
+      <div class="passcode-form__row">
+        <v-text-field
+          box
+          label="Enter your Passcode"
+          hint="Example: 123456789"
+          persistent-hint
+          :rules="entityPasscodeRules"
+          v-model="passcode"
+        ></v-text-field>
+      </div>
+      <div class="passcode-form__form-btns">
+        <!--
+        <a href="#">Forgot your Passcode?</a>
+        -->
+        <v-btn @click="login"
+          color="primary"
+          depressed
+          large
+        >Sign in
+          <v-icon dark right>arrow_forward</v-icon>
+        </v-btn>
+      </div>
     </v-form>
+  </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'PasscodeForm',
+  export default {
+    name: 'PasscodeForm',
 
 
-        data: () => ({
-            valid: false,
-            entityNumRules: [
-                (v) => !!v || 'Incorporation Number is required',
-                (v) => v.length <= 10 || 'Passcode rules here...'
-            ],
-            entityPasscodeRules: [
-                (v) => !!v || 'Passcode is required',
-                (v) => v.length <= 10 || 'Passcode rules here...'
-            ]
-        }),
+    data: () => ({
+      valid: false,
+      entityNumRules: [
+        (v) => !!v || 'Incorporation Number is required',
+        (v) => v.length <= 10 || 'Passcode rules here...'
+      ],
+      entityPasscodeRules: [
+        (v) => !!v || 'Passcode is required',
+        (v) => v.length <= 10 || 'Passcode rules here...'
+      ]
+    }),
 
 
-        computed: {
-            entityNumber: {
-                get() {
-                    return this.$store.state.entityNumber;
-                },
-                set(value) {
-                    this.$store.commit('entityNumber', value);
-                }
-            },
-            passcode: {
-                get() {
-                    return this.$store.state.passcode;
-                },
-                set(value) {
-                    this.$store.commit('passcode', value);
-                }
-            }
-
-
+    computed: {
+      entityNumber: {
+        get() {
+          return this.$store.state.entityNumber;
         },
-        methods: {
-            doSomeWork() {
-
-            },
-            login: () => {
-                if (this.$refs.form.validate()) {
-                    console.log('Login  called -Valid');
-                    return true;
-                } else {
-                    console.log('Login  called -Invalid');
-                    return false;
-                }
-            }
-
-
+        set(value) {
+          this.$store.commit('entityNumber', value);
         }
+      },
+      passcode: {
+        get() {
+          return this.$store.state.passcode;
+        },
+        set(value) {
+          this.$store.commit('passcode', value);
+        }
+      }
 
-    };
+
+    },
+    methods: {
+      doSomeWork() {
+
+      },
+      login: () => {
+        if (this.$refs.form.validate()) {
+          console.log('Login  called -Valid');
+          return true;
+        } else {
+          console.log('Login  called -Invalid');
+          return false;
+        }
+      }
+
+
+    }
+
+  };
 </script>
 
 <style lang="stylus" scoped>
-    @import "../assets/styl/theme.styl"
+  @import "../assets/styl/theme.styl"
 
-    .v-input
-        max-width 25rem
+  .passcode-form__row
+    margin-top 1rem
 
-    .passcode-form__form-btns
-        text-align center
+  .passcode-form__form-btns
+    margin-top 2rem
+    text-align right
+
+    .forgotten-link
+      flex 1 1 auto
+
+  .v-btn
+    margin 0
+
+  .v-input
+    max-width 25rem
 
 </style>
--- auth-web/src/plugins/vuetify.ts
+++ auth-web/src/plugins/vuetify.ts
@@ -1,3 +1,4 @@
+import 'material-icons/iconfont/material-icons.css'; // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
