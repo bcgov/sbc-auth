55a58be2e3e79330cf9135c16dcd788adbc36a94
Saravan PA <saravanpa@gmail.com>
2019-04-12 16:24:56 -0700

    Update authenticate.py

--- auth-api/src/auth_api/resources/authenticate.py
+++ auth-api/src/auth_api/resources/authenticate.py
@@ -33,7 +33,7 @@ tracer = opentracing.tracer
 tracing = FlaskTracing(tracer)
 
 
-@cors_preflight('POST','OPTIONS')
+@cors_preflight('POST,OPTIONS')
 @API.route('')
 class User(Resource):
     """Retrieve user detail information from token and database """
