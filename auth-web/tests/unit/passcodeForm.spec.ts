01c532d8422762fa724d483959ecff574cb7ed9b
saravankumarpa <saravanpa@gmail.com>
2019-04-24 10:22:14 -0700

    some more tests

--- auth-web/tests/unit/passcodeForm.spec.ts
+++ auth-web/tests/unit/passcodeForm.spec.ts
@@ -7,7 +7,9 @@ import VueRouter from 'vue-router'
 Vue.use(Vuetify)
 Vue.use(VueRouter)
 describe('PasscodeForm.vue', () => {
-  it('passcode screen', () => {
+  let cmp
+
+  beforeEach(() => {
     const localVue = createLocalVue()
     localVue.use(Vuex)
 
@@ -15,15 +17,58 @@ describe('PasscodeForm.vue', () => {
       state: {
         entityNumber: '',
         passcode: ''
+
+      },
+      getters: {
+        entityNumber: (state) => state.entityNumber,
+        passcode: (state) => state.passcode
+      },
+      mutations: {
+        entityNumber (state, entityNumber) {
+          state.entityNumber = entityNumber
+        },
+
+        passcode (state, passcode) {
+          state.passcode = passcode
+        }
       }
     })
 
-    const wrapper = mount(PasscodeForm, {
+    cmp = mount(PasscodeForm, {
+      store,
+      localVue
+    })
+  })
+
+  it('passcode screen login button exists', () => {
+    expect(cmp.find('.sign-in-btn').text().startsWith('Sign in')).toBeTruthy()
+    expect(cmp.isVueInstance()).toBeTruthy()
+  })
+
+  it('passcode and entity number is empty', () => {
+    expect(cmp.vm.entityNumber).toBe('')
+    expect(cmp.vm.passcode).toBe('')
+  })
+
+  it('passcode and entity number is the values in store', () => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const store = new Vuex.Store({
+      state: {
+        entityNumber: 'somenumber',
+        passcode: 'somevalue'
+      }
+    })
+
+    cmp = mount(PasscodeForm, {
       store,
       localVue
     })
 
-    expect(wrapper.find('.sign-in-btn').text().startsWith('Sign in')).toBeTruthy()
-    expect(wrapper.isVueInstance()).toBeTruthy()
+    cmp.vm.$refs.form.validate()
+
+    expect(cmp.vm.entityNumber).toBe('somenumber')
+    expect(cmp.vm.passcode).toBe('somevalue')
   })
 })
