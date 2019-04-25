499d9066dc4115dc1ac8aaef91deea00cbb28ba2
Jeremy Dunsdon <twenty15creative@gmail.com>
2019-04-25 08:07:43 -0700

    [entity-403] Finalizing UX & Content for Passcode UI

--- auth-web/src/components/SupportInfoCard.vue
+++ auth-web/src/components/SupportInfoCard.vue
@@ -1,8 +1,18 @@
 <template>
-  <v-card>
+  <v-card class="support-info-card">
     <header>Need Assistance?</header>
     <div class="container">
-      {{ $t('helpText') }}
+      <p>For support or questions about this application, please contact us at:</p>
+      <ul class="contact-list">
+        <li class="contact-list__row">
+          <v-icon small color="primary">phone</v-icon>
+          <span class="contact-info__value">{{ $t('techSupportPhone') }}</span>
+        </li>
+        <li class="contact-list__row">
+          <v-icon small color="primary">email</v-icon>
+          <span class="contact-info__value"><a v-bind:href="'mailto:' + $t('techSupportEmail')">{{ $t('techSupportEmail') }}</a></span>
+        </li>
+      </ul>
     </div>
   </v-card>
 </template>
@@ -16,13 +26,43 @@ export default {
 <style lang="stylus" scoped>
   @import "../assets/styl/theme.styl"
 
-  header
-    padding 1rem 1.25rem
-    color #fff
-    background $BCgovBlue5
-    font-weight 700
+  .support-info-card
+    font-size 1rem
 
-  .v-card
-    font-size: 0.875rem
+  header
+    padding 1.5rem
+    padding-bottom 0
+    font-size 1.5em
+    font-weight 500
+
+  .container
+    padding-top 1.125rem
+    font-size 1em
+
+  @media (min-width 960px)
+    header
+      font-size 1.125em
+
+    .container
+      font-size 0.875em
+
+  .contact-list
+    margin-top 1.5rem
+    padding 0
+    font-weight 500
+    list-style-type none
+
+  .contact-list__row
+    overflow hidden
+    white-space nowrap
+    text-overflow ellipsis
+
+  .contact-list__row .v-icon
+      vertical-align middle
+      margin-top -0.15rem
+      margin-right 1rem
+
+  .contact-list__row + .contact-list__row
+    margin-top 0.2rem
 
 </style>
--- auth-web/src/views/Home.vue
+++ auth-web/src/views/Home.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="view-container">
     <article>
-      <h1>File Annual Reports Online</h1>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-      <section>
-        <h2>Get Started...</h2>
-        <p>Once you have received your <strong>Passcode Notice Letter</strong> you will be able to file start filing online. If you haven't yet received your letter, please contact BC Registries toll-free at 1-800-663-6102 (Monday to Friday, 8am-7pm).</p>
-      </section>
-      <section>
-        <v-card>
-          <v-container class="passcode-container">
-            <h3>Sign In</h3>
-            <PasscodeForm/>
-          </v-container>
-        </v-card>
-      </section>
+      <h1>File BC Co-operative <span>Annual Reports</span></h1>
+      <p class="intro-text">Sign in to start filing your BC Co-operative Annual Report. You will need to provide your <em>Incorporation Number</em> and your <em>Passcode</em> in the letter you received in the mail.</p>
+      <v-card class="sign-in-card">
+        <v-container>
+          <h2>Sign In</h2>
+          <PasscodeForm/>
+        </v-container>
+      </v-card>
     </article>
     <aside>
       <SupportInfoCard/>
@@ -39,41 +33,59 @@ export default ({
 <style lang="stylus" scoped>
   @import "../assets/styl/theme.styl"
 
-  h1
-    margin-bottom 1rem
-    letter-spacing -0.02rem
-
-  h2
-    margin-bottom 0.75rem
-    letter-spacing -0.02rem
-
-  h3
+  h1, h2
     margin-bottom 1.5rem
-
-  section
-    margin-top 2rem
-
-  .view-container
-    display flex
-    flex-flow column nowrap
 
   article
     flex 1 1 auto
 
   aside
     flex 0 0 auto
-    margin-top 3rem
+    margin-top 2rem
+
+  .intro-text
+    margin-bottom 2rem
+    letter-spacing -0.01rem
+    font-size 1rem
+    font-weight 300
+
+    em
+      font-style normal
+      font-weight 400
+
+  .view-container
+    display flex
+    flex-flow column nowrap
+
+  .sign-in-card .container
+    padding 1.5rem
+
+  @media (max-width 480px)
+    h1 span
+      display block
+
+  @media (min-width 768px)
+    h1
+      margin-bottom 2rem
+
+    .intro-text
+      margin-bottom 3rem
+      font-size 1.125rem
 
   @media (min-width: 960px)
-    .view-container
-        flex-flow row nowrap
+    article
+      padding-top 0.8rem
+      padding-bottom 0.8rem
 
     aside
       margin-top 0
       margin-left 2rem
       width 20rem
 
-    .passcode-container
+    .sign-in-card .container
       padding 2rem
+
+    .view-container
+      flex-flow row nowrap
 
 </style>
