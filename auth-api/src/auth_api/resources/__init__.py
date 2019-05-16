2f8f7409b2b9e28e38e059f1ea967efffd05b6db
pwei1018 <44277752+pwei1018@users.noreply.github.com>
2019-05-16 08:40:37 -0700

    Enable tracing for endpoint, service and db model. (#68)
    * 537 tracing (#2)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * 537 tracing (#3)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * Share JWT with coops using iframe messaging
    
    * Stores being split into multiple files.Favicon corrected
    
    * hitting the new services
    
    * wrong url -fixed
    
    * more Test cases and fixes
    
    * Updating github template
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * using the common lib
    
    * Add data migration file and data models for auth. (#67)
    
    * 537 tracing (#2)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * fix an indent error
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * Fix formatting issue

--- auth-api/src/auth_api/resources/__init__.py
+++ auth-api/src/auth_api/resources/__init__.py
@@ -1,13 +1,13 @@
 # Copyright © 2019 Province of British Columbia
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -30,38 +30,35 @@ from .ops import API as OPS_API
 from .token import API as TOKEN_API
 
 
-
 __all__ = ('API_BLUEPRINT', 'OPS_BLUEPRINT')
 
 # This will add the Authorize button to the swagger docs
 # TODO oauth2 & openid may not yet be supported by restplus <- check on this
-AUTHORIZATIONS = {
-    'apikey': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'
-    }
-}
+AUTHORIZATIONS = {'apikey': {'type': 'apiKey', 'in': 'header', 'name': 'Authorization'}}
 
 OPS_BLUEPRINT = Blueprint('API_OPS', __name__, url_prefix='/ops')
 
-API_OPS = Api(OPS_BLUEPRINT,
-              title='Service OPS API',
-              version='1.0',
-              description='The Core API for the Authentication System',
-              security=['apikey'],
-              authorizations=AUTHORIZATIONS)
+API_OPS = Api(
+    OPS_BLUEPRINT,
+    title='Service OPS API',
+    version='1.0',
+    description='The Core API for the Authentication System',
+    security=['apikey'],
+    authorizations=AUTHORIZATIONS,
+)
 
 API_OPS.add_namespace(OPS_API, path='/')
 
 API_BLUEPRINT = Blueprint('API', __name__, url_prefix='/api/v1')
 
-API = Api(API_BLUEPRINT,
-          title='Authentication API',
-          version='1.0',
-          description='The Core API for the Authentication System',
-          security=['apikey'],
-          authorizations=AUTHORIZATIONS)
+API = Api(
+    API_BLUEPRINT,
+    title='Authentication API',
+    version='1.0',
+    description='The Core API for the Authentication System',
+    security=['apikey'],
+    authorizations=AUTHORIZATIONS,
+)
 
 API.add_namespace(META_API, path='/meta')
 API.add_namespace(USERINFO_API, path='/users/info')
