84a044b83172c388475707a1902a9d92724e0406
pwei1018 <44277752+pwei1018@users.noreply.github.com>
2019-05-16 08:40:37 -0700

    Enable tracing for endpoint, service and db model. (#68)
    * 537 tracing (#2)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * 537 tracing (#3)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * Share JWT with coops using iframe messaging
    
    * Stores being split into multiple files.Favicon corrected
    
    * hitting the new services
    
    * wrong url -fixed
    
    * more Test cases and fixes
    
    * Updating github template
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * trying to handle 404 page
    
    * using the common lib
    
    * Add data migration file and data models for auth. (#67)
    
    * 537 tracing (#2)
    
    * Rename tracing id name; Add a tracing class to manage tracing related functions
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * move tracing code to sbc-common-components package and change requirments setting
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * enable tracing
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * fix an indent error
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * Fix formatting issue

--- auth-api/src/auth_api/resources/usersinfo.py
+++ auth-api/src/auth_api/resources/usersinfo.py
@@ -1,48 +1,68 @@
 # Copyright © 2019 Province of British Columbia
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Endpoints to get user information from token and database."""
-from flask import jsonify, g
-from flask_restplus import Resource, Namespace, cors
-from flask_jwt_oidc import AuthError
+import traceback
+from functools import wraps
 
-import opentracing
-from flask_opentracing import FlaskTracing
+from flask import g, jsonify
+from flask_jwt_oidc import AuthError
+from flask_restplus import Namespace, Resource, cors
+
+from sbc_common_components.tracing.exception_tracing import ExceptionTracing
 
 from auth_api import jwt as _jwt
+from auth_api import tracing as _tracing
+from auth_api.exceptions import UserException
 from auth_api.services import User
 from auth_api.utils.util import cors_preflight
-from auth_api.utils.tracing import ExceptionTracing
-
 
 API = Namespace('users/info', description='Authentication System - get User Information')
 
-# get the existing tracer and inject into flask app
-TRACER = opentracing.tracer
-TRACING = FlaskTracing(TRACER)
-EXCEPTION_TRACING = ExceptionTracing(TRACER)
-
 
 @API.errorhandler(AuthError)
-@EXCEPTION_TRACING.trace()
 def handle_auth_error(exception):
-    raise Exception(jsonify(exception), exception.status_code)
+    """TODO just a demo function"""
+    return jsonify(exception), exception.status_code
+
+
+@API.errorhandler(UserException)
+def handle_db_exception(error):
+    """TODO just a demo function"""
+    return {'message': str(error.error)}, error.status_code
 
 
 @API.errorhandler(Exception)
-@EXCEPTION_TRACING.trace()
-def handle_error(exception):
-    raise Exception(jsonify(exception), exception.status_code)
+def handle_exception(exception):
+    """TODO just a demo function"""
+    return {'message': str(exception.error)}, exception.status_code
+
+
+def catch_custom_exception(func):
+    """TODO just a demo function"""
+
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            trace_back = traceback.format_exc()
+
+            ExceptionTracing.trace(e, trace_back)
+
+            raise UserException(e.with_traceback(None), 403, trace_back)
+
+    return decorated_function
 
 
 @cors_preflight('GET')
@@ -52,16 +72,14 @@ class UserInfo(Resource):
 
     @staticmethod
     @cors.crossdomain(origin='*')
+    @_tracing.trace()
+    @catch_custom_exception
     @_jwt.requires_auth
-    @TRACING.trace()
     def get():
         """Return a JSON object that includes user detail information"""
-        current_span = TRACER.active_span
         token = g.jwt_oidc_token_info
         user = User.find_by_jwt_token(token)
         if not user:
             user = User.save_from_jwt_token(token)
-
-        current_span.log_kv({'userinfo': user.asdict()})
 
         return jsonify(user.asdict()), 200
