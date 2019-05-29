c2f258d5e15eef7f92ffcbd8fa51d77bb2b9a605
Saravan PA <saravanpa@gmail.com>
2019-05-29 10:14:31 -0700

    verifying the build branch..making trivial changes

--- auth-web/src/components/PasscodeForm.vue
+++ auth-web/src/components/PasscodeForm.vue
@@ -127,7 +127,7 @@ export default {
     },
     login () {
       if (this.isValidForm()) {
-        console.log('VUE_APP_ROOT_API:' + process.env.VUE_APP_ROOT_API)
+        console.log('VUE_APP_ROOT_API:--------------' + process.env.VUE_APP_ROOT_API)
         console.log('VUE_APP_TEST_VAR_WEBAPP:' + process.env.TEST_VAR_WEBAPP)
         console.log('VUE_APP_VUE_APP_TEST_VAR:' + process.env.VUE_APP_TEST_VAR)
 
