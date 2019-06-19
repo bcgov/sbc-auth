09efc7e1d08deed370dfb22aab8a67c290f9b45e
Andrew Sutherland <sutherlanda@gmail.com>
2019-06-19 10:00:52 -0700

    Fix lint errors in API/KeyCloak unit tests (#84)
    * Fix lint errors in API/KeyCloak unit tests
    
    * Use proper Error status_codes
    
    * Linting fixes
    
    - Fix misordered imports
    - Remove unused imports
    - Remove unnecessary self references
    - Modify setup.cfg to ignore generated members

--- auth-api/src/auth_api/resources/token.py
+++ auth-api/src/auth_api/resources/token.py
@@ -17,15 +17,12 @@ import json
 
 from flask import request
 from flask_restplus import Namespace, Resource, cors
-
+from sbc_common_components.tracing.trace_tags import TraceTags
 from auth_api import status as http_status
 from auth_api import tracing as _tracing
 from auth_api.services.keycloak import KeycloakService
 from auth_api.utils.util import cors_preflight
 from auth_api.exceptions import BusinessException
-
-from sbc_common_components.tracing.trace_tags import TraceTags
-
 
 API = Namespace('token', description='Authentication System - Passcode login')
 KEYCLOAK_SERVICE = KeycloakService()
