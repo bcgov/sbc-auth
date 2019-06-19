dddffc38bfb784bd6bd7244da180395f207301ef
PWEI <patrick.wei@gov.bc.ca>
2019-06-18 17:24:36 -0700

    test

--- auth-api/.pipeline/pytest.groovy
+++ auth-api/.pipeline/pytest.groovy
@@ -60,7 +60,7 @@ def pullrequestStatus(token, state, targetUrl, context, description, pullRequest
     echo "${payload}"
     //def encodedReq = URLEncoder.encode(payload, "UTF-8")
      withCredentials([[$class: 'StringBinding', credentialsId: 'GITHUB_TOKEN', variable: 'GITHUB_TOKEN']]) {
-       sh("curl -s -H \"Authorization: token ${env.GITHUB_TOKEN}\" -H \"Accept: application/json\" -H \"Content-type: application/json\" -X POST -d \'${payload}\' ${pullRequestUrl})
+       sh("curl -s -H \"Authorization: token ${env.GITHUB_TOKEN}\" -H \"Accept: application/json\" -H \"Content-type: application/json\" -X POST -d \'${payload}\' ${pullRequestUrl}")
      }
 }
 
