52463855f2c291ca2a68dedb0bef996b11cc3992
Dong <Meng.Dong@gov.bc.ca>
2019-07-03 15:11:36 -0700

    Add tracing id to header.

--- auth-web/src/util/interceptors.ts
+++ auth-web/src/util/interceptors.ts
@@ -3,8 +3,13 @@ import axios from 'axios'
 export default function setup () {
   axios.interceptors.request.use(function (config) {
     const token = sessionStorage.getItem('KEYCLOAK_TOKEN')
+    const tracingID = sessionStorage.getItem('REGISTRIES_TRACE_ID')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
+    }
+    debugger
+    if (tracingID) {
+      config.headers['registries-tracing-id'] = tracingID
     }
     return config
   }, function (err) {
