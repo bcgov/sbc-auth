39b793eabca1d08ee0bb0a4fa8c09641ee092c93
Patrick Wei <44277752+pwei1018@users.noreply.github.com>
2019-07-04 16:52:08 -0700

    setup a pull request review pipeline (#88)
    * setup a pull request review pipeline
    
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>
    
    * Fixed a linter error and clean up the pipeline
    
    * Added get author function
    
    * Fixe the get changed author function
    
    * Fixed get changed author function
    
    * add get last commit details function
    
    * Fixed the secrets name
    
    * Remove @here from success RC message, it need mention-all premission for bot user
    
    * Create documentation for pull request review pipeline
    
    * Tuning the pipeline and documentation
    
    * Change the rocketchat notification channel when build fail

--- auth-api/src/auth_api/resources/logout.py
+++ auth-api/src/auth_api/resources/logout.py
@@ -31,7 +31,6 @@ API = Namespace('logout', description='Authentication System - Logout User')
 KEYCLOAK_SERVICE = KeycloakService()
 TRACER = Tracer.get_instance()
 
-
 @API.errorhandler(AuthError)
 def handle_auth_error(exception):
     """TODO just a demo function."""
