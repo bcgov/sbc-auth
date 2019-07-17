a13d506a5b6a9db0286e9b79f5ce1cdc27c89364
saravankumarpa <saravanpa@gmail.com>
2019-07-17 15:18:28 -0700

    Fixed tests .Removed caddyfile.Fixed payurl

--- auth-web/src/services/payment.services.ts
+++ auth-web/src/services/payment.services.ts
@@ -4,12 +4,12 @@ import ConfigHelper from '../util/config-helper'
 export default {
 
   createTransaction (paymentId:String, redirectUrl:String) {
-    var url = `${ConfigHelper.getValue('VUE_APP_AUTH_ROOT_API')}/payments/${paymentId}/transactions?redirect_uri=${redirectUrl}`
+    var url = `${ConfigHelper.getValue('VUE_APP_PAY_ROOT_API')}/payments/${paymentId}/transactions?redirect_uri=${redirectUrl}`
     return Axios.post(url, {})
   },
 
   updateTransaction (paymentId:String, transactionId:String, receiptNum?:String) {
-    const url = `${ConfigHelper.getValue('VUE_APP_AUTH_ROOT_API')}/payments/${paymentId}/transactions/${transactionId}?receipt_number=${receiptNum}`
+    const url = `${ConfigHelper.getValue('VUE_APP_PAY_ROOT_API')}/payments/${paymentId}/transactions/${transactionId}?receipt_number=${receiptNum}`
     return Axios.patch(url)
   }
 }
