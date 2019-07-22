31ac983fb584b4902a6578f76d5043c0f1d04a0a
Andrew Sutherland <sutherlanda@gmail.com>
2019-07-22 11:23:40 -0700

    Date model - User management (#101)
    * Temporarily archive old models
    
    * Updates to User model
    
    * Create models and schemas
    
    * New tables, alembic version
    
    * Add roles to user model
    
    - Add alembic version
    
    * Removed archived models
    
    * Add model for contact info

---
+++ auth-api/src/auth_api/models/org_type.py
@@ -0,0 +1,39 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This manages an Org Type record in the Auth service.
+
+This is a mapping between org type codes and descriptions for Org objects.
+"""
+
+from sqlalchemy import Column, String
+
+from .db import db, ma
+
+
+class OrgType(db.Model):  # pylint: disable=too-few-public-methods # Temporarily disable until methods defined
+    """This is the model for an Org Type record."""
+
+    __tablename__ = 'org_type'
+
+    code = Column(String(15), primary_key=True, unique=True)
+    desc = Column(String(100))
+
+
+class OrgTypeSchema(ma.ModelSchema):
+    """This is the Schema for an Org Type record."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Maps all of the Org Type fields to a default schema."""
+
+        model = OrgType
