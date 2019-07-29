7120cefc6e399aa3f0862b2c2f04abcdbfbf1110
Andrew Sutherland <sutherlanda@gmail.com>
2019-07-29 09:57:21 -0700

    Add input validation to business contact UI

--- auth-web/src/components/auth/BusinessContactForm.vue
+++ auth-web/src/components/auth/BusinessContactForm.vue
@@ -17,6 +17,7 @@
         label="Email Address"
         req
         persistent-hint
+        :rules="emailRules"
         v-model="emailAddress"
       >
       </v-text-field>
@@ -27,6 +28,8 @@
         label="Confirm Email Address"
         req
         persistent-hint
+        :error-messages="emailMustMatch()"
+        v-model="confirmedEmailAddress"
       >
       </v-text-field>
     </div>
@@ -35,9 +38,12 @@
         <v-flex xs6 class="mr-5">
           <v-text-field
             box
-            label="Phone"
+            label="Phone e.g. (555)-555-5555"
             persistent-hint
+            type="tel"
+            v-mask="['(###)-###-####']"
             v-model="phoneNumber"
+            :rules="phoneRules"
           >
           </v-text-field>
         </v-flex>
@@ -45,6 +51,8 @@
           <v-text-field
             box label="Extension"
             persistent-hint
+            :rules="extensionRules"
+            v-mask="'###'"
             v-model="extension"
           >
           </v-text-field>
@@ -71,14 +79,41 @@ import { Vue, Component } from 'vue-property-decorator'
 import { getModule } from 'vuex-module-decorators'
 import BusinessModule from '../../store/modules/business'
 import configHelper from '../../util/config-helper'
+import { mask } from 'vue-the-mask'
 
-@Component
+@Component({
+  directives: {
+    mask
+  }
+})
 export default class BusinessContactForm extends Vue {
   private VUE_APP_COPS_REDIRECT_URL = configHelper.getValue('VUE_APP_COPS_REDIRECT_URL')
   private businessStore = getModule(BusinessModule)
   private emailAddress = ''
+  private confirmedEmailAddress = ''
   private phoneNumber = ''
   private extension = ''
+  private formError = ''
+
+  private emailRules = [
+    v => !!v || 'Email address is required',
+    v => {
+      const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return pattern.test(v) || 'Valid email is required'
+    }
+  ]
+
+  private phoneRules = [
+    v => (v.length === 0 || v.length === 14) || 'Phone number is invalid'
+  ]
+
+  private extensionRules = [
+    v => (v.length >= 0 || v.length <= 3) || 'Extension is invalid'
+  ]
+
+  private emailMustMatch (): string {
+    return (this.emailAddress === this.confirmedEmailAddress) ? '' : 'Emails addresses must match'
+  }
 
   private isFormValid (): boolean {
     return (this.$refs.form as Vue & { validate: () => boolean }).validate()
@@ -87,7 +122,7 @@ export default class BusinessContactForm extends Vue {
   mounted () {
     const contact = this.businessStore.currentBusiness.contact1
     if (contact) {
-      this.emailAddress = contact.emailAddress
+      this.emailAddress = this.confirmedEmailAddress = contact.emailAddress
       this.phoneNumber = contact.phoneNumber
       this.extension = contact.extension
     }
@@ -95,7 +130,10 @@ export default class BusinessContactForm extends Vue {
 
   save () {
     if (this.isFormValid()) {
-      // this.businessStore
+      // TODO: Change this to transition to entity dashboard once complete
+      setTimeout(() => {
+        window.location.href = this.VUE_APP_COPS_REDIRECT_URL
+      }, 500)
     }
   }
 
