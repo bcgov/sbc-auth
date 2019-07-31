7f7ebe5cecb80a2350bbf9062e4aa08299620e01
saravankumarpa <saravanpa@gmail.com>
2019-07-31 10:14:37 -0700

    added process back

--- auth-web/vue.config.js
+++ auth-web/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
-  publicPath: '/auth/',
+  publicPath: process.env.VUE_APP_PATH,
   devServer: {
     // not used
     proxy: {
