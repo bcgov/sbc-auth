e360bfc373eb802380b76a4ebf19fa82e9ee4392
PWEI <patrick.wei@gov.bc.ca>
2019-08-01 09:59:57 -0700

    Update the pytest pipeline parameter

--- auth-api/jenkins/dev.groovy
+++ auth-api/jenkins/dev.groovy
@@ -197,9 +197,8 @@ if( run_pipeline ) {
                     script {
                         openshift.withCluster() {
                             openshift.withProject("${NAMESPACE_UNITTEST}") {
-                                def test_pipeline = openshift.selector('bc', 'pytest-pipeline')
-
-                                test_pipeline.startBuild('--wait=true', "-e=component=${APP_NAME}", "-e=tag=${DESTINATION_TAG}", "-e=namespace=${NAMESPACE_APP}", "-e=db_type=PG").logs('-f')
+                                def test_pipeline = openshift.selector('bc', 'pytest-pipeline'),
+                                test_pipeline.startBuild('--wait=true', "-e=component=${APP_NAME}", "-e=component_tag=${DESTINATION_TAG}", "-e=tag=${DESTINATION_TAG}", "-e=namespace=${NAMESPACE_APP}", "-e=db_type=PG").logs('-f')
                                 echo "All tests passed"
                             }
                         }
