138a291f686fd139dfab631ba7bcae24586890f1
saravankumarpa <saravanpa@gmail.com>
2019-08-09 15:12:04 -0700

    Changes for adding system alerts in case of paybc is down

--- auth-web/src/App.vue
+++ auth-web/src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <v-app class="app-container" id="app">
+    <pay-system-alert></pay-system-alert>
     <sbc-layout>
       <router-view/>
     </sbc-layout>
   </v-app>
 </template>
 
-<script>
-import Vue from 'vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 import SbcLayout from 'sbc-common-components/src/components/SbcLayout.vue'
+import PaySystemAlert from '@/components/pay/PaySystemAlert.vue'
 
-export default Vue.extend({
-  name: 'app',
-  components: {
-    SbcLayout
-  }
-})
+  @Component({
+    components: {
+      SbcLayout,
+      PaySystemAlert
+    }
+  })
+
+export default class App extends Vue {
+}
 </script>
 
 <style lang="stylus">
