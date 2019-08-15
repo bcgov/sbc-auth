574f8cdf322ff6ddf3e0c37de32de3a1138dbad7
saravankumarpa <saravanpa@gmail.com>
2019-08-15 16:16:30 -0700

    https://github.com/bcgov/entity/issues/1016 fixes

--- auth-web/src/components/pay/PaymentForm.vue
+++ auth-web/src/components/pay/PaymentForm.vue
@@ -5,7 +5,8 @@
                 <v-layout row justify-center align-center>
                     <v-progress-circular color="primary" :size="50" indeterminate v-if="!errorMessage"></v-progress-circular>
                     <div class="loading-msg" v-if="!errorMessage"> {{ $t('paymentPrepareMsg') }}</div>
-                    <div class="loading-msg" v-if="errorMessage">{{errorMessage}}</div>
+                    <div class="loading-msg" v-if="errorMessage && !showErrorModal">{{errorMessage}}</div>
+                    <sbc-system-error v-on:continue-event="goToReturnUrl()" :showModal=showErrorModal title="Payment Failed" primaryButtonTitle="Continue to Filing" :description="errorHeading" ></sbc-system-error>
                 </v-layout>
             </v-container>
         </v-app>
@@ -14,33 +15,45 @@
 
 <script lang="ts">
 
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import PaymentServices from '@/services/payment.services'
-export default {
-  name: 'PaymentForm',
+import SbcSystemError from 'sbc-common-components/src/components/SbcSystemError.vue'
 
-  props: {
-    paymentId: String,
-    redirectUrl: String
-  },
-  data () {
-    return {
-      errorMessage: ''
-    }
-  },
+@Component({
+  components: {
+    SbcSystemError
+  }
+})
+export default class PaymentForm extends Vue {
+    @Prop() paymentId: string
+    @Prop() redirectUrl: string
+    errorMessage:string = ''
+    showErrorModal:boolean
+    returnUrl:string
 
-  mounted () {
-    if (this.paymentId && this.redirectUrl) {
+    mounted () {
+      if (!this.paymentId || !this.redirectUrl) {
+        this.errorMessage = this.$t('payNoParams').toString()
+        return
+      }
       PaymentServices.createTransaction(this.paymentId, encodeURIComponent(this.redirectUrl))
         .then(response => {
-          window.location.href = response.data.pay_system_url
+          this.returnUrl = response.data.pay_system_url
+          this.goToReturnUrl()
         })
         .catch(response => {
-          this.errorMessage = this.$t('payFailedMessage')
+          if (response.data.code === 'PAY006') { // Transaction is already completed
+            this.showErrorModal = true
+            this.errorMessage = this.$t('payFailedMessage').toString()
+          } else {
+            this.errorMessage = this.$t('payFailedMessage').toString()
+            this.showErrorModal = false
+          }
         })
-    } else {
-      this.errorMessage = this.$t('payNoParams')
     }
-  }
+    goToReturnUrl () {
+      window.location.href = this.returnUrl
+    }
 }
 </script>
 
--- auth-web/src/components/pay/PaymentReturnForm.vue
+++ auth-web/src/components/pay/PaymentReturnForm.vue
@@ -1,49 +1,108 @@
 <template>
     <div id="app">
-        <v-app>
-            <v-container fill-height>
-                <v-layout row justify-center align-center>
-                    <v-progress-circular color="primary" :size="50" indeterminate v-if="!errorMessage"></v-progress-circular>
-                    <div class="loading-msg" v-if="!errorMessage"> {{ $t('paymentDoneMsg') }}</div>
-                    <div class="loading-msg" v-if="errorMessage">{{errorMessage}}</div>
-                </v-layout>
-            </v-container>
-        </v-app>
+        <v-container>
+            <v-layout row justify-center align-center>
+                <v-progress-circular color="primary" :size="50" indeterminate v-if="!errorMessage"></v-progress-circular>
+                <div class="loading-msg" v-if="!showErrorText && !showErrorModal"> {{ $t('paymentDoneMsg') }}</div>
+                <div class="loading-msg" v-if="showErrorText">{{errorMessage}}</div>
+                <sbc-system-error v-on:continue-event="goToReturnUrl()" :showModal=showErrorModal title="Payment Failed" primaryButtonTitle="Continue to Filing" :description="errorHeading" ></sbc-system-error>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
 <script  lang="ts">
 
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import PaymentServices from '@/services/payment.services'
-export default {
-  name: 'PaymentReturnForm',
+import SbcSystemError from 'sbc-common-components/src/components/SbcSystemError.vue'
 
-  props: {
-    paymentId: String,
-    transactionId: String,
-    receiptNum: String
-  },
-  data () {
-    return {
-      errorMessage: ''
-    }
-  },
-  mounted () {
-    if (this.paymentId && this.transactionId) {
-      PaymentServices.updateTransaction(this.paymentId, this.transactionId, this.receiptNum)
-        .then(response => {
-          window.location.href = response.data.client_system_url
-        })
-        .catch(response => {
-          this.errorMessage = this.$t('payFailedMessage')
-        })
-    } else {
-      this.errorMessage = this.$t('payNoParams')
-    }
+@Component({
+  components: {
+    SbcSystemError
   }
+})
+export default class PaymentReturnForm extends Vue {
+        @Prop() paymentId: string
+        @Prop() transactionId: string
+        @Prop() receiptNum: string
+        errorHeading:string = 'Payment failed due to technical reasons.Please contact below number for more details.'
+        returnUrl:string
+        errorMessage:string = ''
+        // show modal when paybc is down..otherwise [all unhandled technical error , show plain text error message..]
+        showErrorText:boolean = false
+        showErrorModal:boolean = false
+
+        mounted () {
+          if (!this.paymentId || !this.transactionId) {
+            this.errorMessage = this.$t('payNoParams').toString()
+            return
+          }
+          PaymentServices.updateTransaction(this.paymentId, this.transactionId, this.receiptNum)
+            .then(response => {
+              this.returnUrl = response.data.client_system_url
+              if (response.data.pay_system_reason_code && response.data.pay_system_reason_code === 'SERVICE_UNAVAILABLE') {
+                // PayBC down time..Show the custom modal
+                this.errorMessage = this.$t('payFailedMessage').toString()
+                this.showErrorModal = true
+              } else {
+                // all good..go back
+                this.goToReturnUrl()
+              }
+            })
+            .catch(response => {
+              // technical error..need not to show the modal
+              this.errorMessage = this.$t('payFailedMessage').toString()
+              this.showErrorText = true
+            })
+        }
+        goToReturnUrl () {
+          window.location.href = this.returnUrl
+        }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+    @import '../../assets/styl/theme.styl';
+    article
+        .v-card
+            line-height: 1.2rem;
+            font-size: 0.875rem;
+    section p
+        // font-size 0.875rem
+        color: $gray6;
+    section + section
+        margin-top: 3rem;
+    h2
+        margin-bottom: 0.25rem;
+    #AR-header
+        margin-bottom: 1.25rem;
+        line-height: 2rem;
+        letter-spacing: -0.01rem;
+        font-size: 2rem;
+        font-weight: 500;
+    #AR-step-1-header, #AR-step-2-header, #AR-step-3-header, #AR-step-4-header
+        margin-bottom: 0.25rem;
+        margin-top: 3rem;
+        font-size: 1.125rem;
+        font-weight: 500;
+    .title-container
+        margin-bottom: 0.5rem;
+    .agm-date
+        margin-left: 0.25rem;
+        font-weight: 300;
+    // Save & Filing Buttons
+    #buttons-container
+        padding-top: 2rem;
+        border-top: 1px solid $gray5;
+        .buttons-left
+            width: 50%;
+        .buttons-right
+            margin-left auto
+        .v-btn + .v-btn
+            margin-left: 0.5rem;
+    .genErr
+        font-size: 0.9rem;
+    .error-dialog-padding
+        margin-left: 1rem;
 </style>
