9a6f5be05abe7cbc3ae721c12d65ef80d73fc724
Andrew Sutherland <sutherlanda@gmail.com>
2019-08-22 09:02:15 -0700

    Add template and temporary menu

---
+++ auth-web/src/components/auth/ManagementMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer
+    value="true"
+    mobile-break-point="960"
+    class="ml-5"
+    :mini-variant="mini()"
+  >
+    <v-list>
+      <v-list-tile
+        v-for="(item, i) in menu"
+        :key="i"
+        @click="item.activate()"
+      >
+        <v-list-title-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-title-action>
+        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'ManagementMenu'
+})
+export default class ManagementMenu extends Vue {
+  @Prop()
+  menu
+
+  mini () {
+    switch (this.$vuetify.breakpoint.name) {
+      case 'xs': return true
+      case 'sm': return true
+      case 'md': return false
+      case 'lg': return false
+      case 'xl': return false
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .v-navigation-drawer {
+    background-color : transparent !important
+  }
+</style>
