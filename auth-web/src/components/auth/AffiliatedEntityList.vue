22822dc65c6c8ee9213d56b6b90ba9a2a18c4e7f
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-08-26 16:23:15 -0700

    Create Dashboard content UI changes

---
+++ auth-web/src/components/auth/AffiliatedEntityList.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <div class="empty-business-panel" v-if="affiliatedEntities.length === 0">
+      <v-card>
+        <v-card-text>
+          <p class="business-list-empty-message">{{ $t('businessListEmptyMessage')}}</p>
+          <p>{{ $t('businessListActionMessage')}}</p>
+        </v-card-text>
+      </v-card>
+    </div>
+    <ul class="org-details" v-if="affiliatedEntities.length > 0">
+      <li
+        class="list-item"
+        v-for="entity in affiliatedEntities"
+        v-bind:key="entity.businessIdentifier">
+        <v-card>
+          <v-card-text class="card-layout">
+            <div class="list-item_entity-number">
+              <a @click="redirectToNext(entity.businessIdentifier)">{{entity.name}}</a>
+            </div>
+            <dl class="meta-container">
+              <dt>Business No:</dt>
+              <dd class="list-item_business-number">{{ entity.businessNumber}}</dd>
+              <dt>Incorporation No:</dt>
+              <dd class="list-item_incorp-number">{{ entity.businessIdentifier}}</dd>
+            </dl>
+          </v-card-text>
+        </v-card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import UserModule from '../../store/modules/user'
+import configHelper from '../../util/config-helper'
+
+@Component
+export default class AffiliatedEntityList extends Vue {
+  private VUE_APP_COPS_REDIRECT_URL = configHelper.getValue('VUE_APP_COPS_REDIRECT_URL')
+  private userStore = getModule(UserModule, this.$store)
+
+  mounted () {
+    this.userStore.getOrganizations()
+  }
+
+  get affiliatedEntities () {
+    let affiliatedEntities = []
+    this.userStore.organizations.forEach(organization => {
+      if (organization.affiliatedEntities) {
+        organization.affiliatedEntities.forEach(entity => {
+          affiliatedEntities.push(entity)
+        })
+      }
+    })
+    return affiliatedEntities
+  }
+
+  redirectToNext (incorporationNumber: String) {
+    if (this.VUE_APP_COPS_REDIRECT_URL) {
+      // Temporary code: Must change once the solution is finalized.
+      const redirectURL = this.VUE_APP_COPS_REDIRECT_URL + '/dashboard?corp=' + incorporationNumber
+      window.location.href = decodeURIComponent(redirectURL)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.org-details {
+  padding: 0
+  list-style-type: none
+  margin-right: 1.5rem
+  margin-bottom: 1.5rem
+}
+
+.list-item {
+  flex-direction: row
+  align-items: center
+  background: #ffffff
+}
+
+.card-layout {
+  padding: 1.5rem
+}
+
+h2 {
+  margin-bottom: 1.5rem
+}
+
+p {
+  text-align: center
+}
+
+.business-list-empty-message {
+  font-weight: 500
+}
+
+.list-item_entity-number {
+  font-weight: 500
+}
+
+a {
+  color: black
+}
+
+.meta-container {
+  overflow: hidden
+  color: $gray6
+  font-size: 0.875rem
+}
+
+dd, dt {
+  float: left
+}
+
+dt {
+  position: relative
+}
+
+dd {
+  margin-left: 0.5rem
+
+  + dt {
+    &:before {
+      content: '•'
+      display: inline-block
+      margin-right: 0.75rem
+      margin-left: 0.75rem
+    }
+  }
+}
+
+.empty-business-panel {
+  margin-right: 1.5rem
+}
+</style>
--- auth-web/src/views/management/EntityManagement.vue
+++ auth-web/src/views/management/EntityManagement.vue
@@ -1,10 +1,16 @@
 <template>
-    <h1>Entity Management</h1>
+  <div>
+    <h2>Manage Businesses</h2>
+    <AffiliatedEntityList />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    name: 'EntityManagement'
+import { Vue, Component } from 'vue-property-decorator'
+import AffiliatedEntityList from '@/components/auth/AffiliatedEntityList.vue'
+
+@Component({
+  components: { AffiliatedEntityList }
 })
+export default class EntityManagement extends Vue {}
 </script>
