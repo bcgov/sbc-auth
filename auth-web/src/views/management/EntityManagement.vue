a4006296a66b879c3e0a4e16d8d1a6429394cb1a
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-08-28 14:03:35 -0700

    Reverting the merge issue

--- auth-web/src/views/management/EntityManagement.vue
+++ auth-web/src/views/management/EntityManagement.vue
@@ -1,10 +1,20 @@
 <template>
-    <h1>Entity Management</h1>
+  <div>
+    <h2>Manage Businesses</h2>
+    <AffiliatedEntityList @add-business="showAddBusinessModal()"/>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  name: 'EntityManagement'
+import { Vue, Component } from 'vue-property-decorator'
+import AffiliatedEntityList from '@/components/auth/AffiliatedEntityList.vue'
+
+@Component({
+  components: { AffiliatedEntityList }
 })
+export default class EntityManagement extends Vue {
+  showAddBusinessModal () {
+    // Todo: Show the add business popup once it is merged.
+  }
+}
 </script>
