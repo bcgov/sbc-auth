4737e603c4386976c1739b637e9fc2b3faeb2b55
IDIR\schen <steven.chen@gov.bc.ca>
2019-08-30 10:14:14 -0700

    1027-Added-Business
    Signed-off-by: IDIR\schen <steven.chen@gov.bc.ca>

--- auth-web/src/services/business.services.ts
+++ auth-web/src/services/business.services.ts
@@ -4,6 +4,7 @@ import configHelper from '@/util/config-helper'
 import { Contact } from '@/models/contact'
 import { Org } from '@/models/org'
 import { Entity } from '@/models/entity'
+import { Affiliation } from '@/models/affiliation'
 
 interface BusinessService {
   getBusiness: (businessNumber: string) => Promise<AxiosResponse<Business>>
@@ -11,8 +12,8 @@ interface BusinessService {
   addContact: (business: Business, contact: Contact) => Promise<AxiosResponse<Business>>
   updateContact: (business: Business, coontact: Contact) => Promise<AxiosResponse<Business>>
   getOrgs: () => Promise<AxiosResponse<any>>
-  createOrg: (org: Org) => Promise<AxiosResponse<Org>>  
-  createAffiliation: (orgIdentifier: string, entity: Entity) => Promise<AxiosResponse<any>>
+  createOrg: (org: Org) => Promise<AxiosResponse<Org>>
+  createAffiliation: (orgIdentifier: string, affiliation: Affiliation) => Promise<AxiosResponse<Affiliation>>
 }
 
 export default {
@@ -28,13 +29,13 @@ export default {
   async updateContact (business: Business, contact: Contact): Promise<AxiosResponse<Business>> {
     return Axios.put(`${configHelper.getValue('VUE_APP_AUTH_ROOT_API')}/entities/${business.businessIdentifier}/contact`, contact)
   },
-  async getOrgs (): Promise<AxiosResponse<any>> {    
+  async getOrgs (): Promise<AxiosResponse<any>> {
     return Axios.get(`${configHelper.getValue('VUE_APP_AUTH_ROOT_API')}/users/orgs`)
   },
   async createOrg (org: Org): Promise<AxiosResponse<Org>> {
     return Axios.post(`${configHelper.getValue('VUE_APP_AUTH_ROOT_API')}/orgs`, org)
   },
-  async createAffiliation (orgIdentifier: string, entity: Entity): Promise<AxiosResponse<any>> {
-    return Axios.post(`${configHelper.getValue('VUE_APP_AUTH_ROOT_API')}/orgs/${orgIdentifier}/affiliations`, entity)
+  async createAffiliation (orgIdentifier: string, affiliation: Affiliation): Promise<AxiosResponse<any>> {
+    return Axios.post(`${configHelper.getValue('VUE_APP_AUTH_ROOT_API')}/orgs/${orgIdentifier}/affiliations`, affiliation)
   }
 } as BusinessService
--- auth-web/src/views/management/EntityManagement.vue
+++ auth-web/src/views/management/EntityManagement.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>Manage Businesses</h2>
+    <v-flex class="text-xs-right">
+        <v-btn @click="addBusiness" color="primary" large>
+          <span>{{'Add Business'}}</span>
+        </v-btn>
+    </v-flex>
     <AffiliatedEntityList @add-business="showAddBusinessModal()"/>
   </div>
 </template>
@@ -15,6 +20,9 @@ import AffiliatedEntityList from '@/components/auth/AffiliatedEntityList.vue'
 export default class EntityManagement extends Vue {
   showAddBusinessModal () {
     // Todo: Show the add business popup once it is merged.
+  }
+  addBusiness () {
+    this.$router.push('/addbusiness')
   }
 }
 </script>
