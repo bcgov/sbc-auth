798122b0a01c58c3c758cd05f9b2a604160e0322
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-09-20 10:34:40 -0700

    #1239 : Changes for securing service and resources with authorization (#178)
    * Adding stylus dependencies back
    
    * Adding file compact option to babel config
    
    * #1239 : Changes for incorporating the fine grained security in services and resources
    
    * #1239 : Changes for incorporating the fine grained security in services and resources
    
    * #1239: Removing the basic and premium reference from auth check

--- auth-api/tests/unit/models/views/test_authorization.py
+++ auth-api/tests/unit/models/views/test_authorization.py
@@ -15,9 +15,11 @@
 
 Test suite to ensure that the Authorizations view routines are working as expected.
 """
-from tests.utilities.factory_utils import *
+import uuid
 
 from auth_api.models.views.authorization import Authorization
+from tests.utilities.factory_utils import factory_user_model, factory_org_model, factory_membership_model, \
+    factory_entity_model, factory_affiliation_model
 
 
 def test_find_user_authorization_by_business_number(session):  # pylint:disable=unused-argument
@@ -71,4 +73,4 @@ def test_find_all_user_authorizations_for_empty(session):  # pylint:disable=unus
 
     authorizations = Authorization.find_all_authorizations_for_user(str(user.keycloak_guid))
     assert authorizations is not None
-    assert len(authorizations) == 0
+    assert authorizations[0].business_identifier is None
