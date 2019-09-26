a0de9db93a003ee1f3880fd6dd71466a5d3a2600
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-09-26 12:46:49 -0700

    Adding Sentry configuration.

--- auth-api/src/auth_api/__init__.py
+++ auth-api/src/auth_api/__init__.py
@@ -27,6 +27,8 @@ from auth_api.models import db, ma
 from auth_api.utils.run_version import get_run_version
 from auth_api.utils.util_logging import setup_logging
 from config import CONFIGURATION, _Config
+import sentry_sdk  # noqa: I001; pylint: disable=ungrouped-imports; conflicts with Flake8
+from sentry_sdk.integrations.flask import FlaskIntegration  # noqa: I001
 
 
 setup_logging(os.path.join(_Config.PROJECT_ROOT, 'logging.conf'))  # important to do this first
@@ -38,6 +40,13 @@ def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
     """Return a configured Flask App using the Factory method."""
     app = Flask(__name__)
     app.config.from_object(CONFIGURATION[run_mode])
+
+    # Configure Sentry
+    if app.config.get('SENTRY_DSN', None):
+        sentry_sdk.init(
+            dsn=app.config.get('SENTRY_DSN'),
+            integrations=[FlaskIntegration()]
+        )
 
     from auth_api.resources import API_BLUEPRINT, OPS_BLUEPRINT
 
