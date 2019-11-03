052fd03d17f81d20c2aef871947a9b103ebeeea2
Andrew Sutherland <sutherlanda@gmail.com>
2019-11-03 15:00:43 -0800

    Add feature flag for user mgmt

--- auth-web/src/views/management/Template.vue
+++ auth-web/src/views/management/Template.vue
@@ -9,6 +9,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import ConfigHelper from '@/util/config-helper'
 import EntityManagement from './EntityManagement.vue'
 import ManagementMenu from '../../components/auth/ManagementMenu.vue'
 import UserManagement from './UserManagement.vue'
@@ -30,16 +31,19 @@ export default class Dashboard extends Vue {
       title: 'Manage Businesses',
       icon: 'business',
       activate: () => { this.setSelectedComponent(EntityManagement) }
-    },
-    {
-      title: 'Manage Teams',
-      icon: 'group',
-      activate: () => { this.setSelectedComponent(UserManagement) }
     }
   ]
 
   mounted () {
     this.setSelectedComponent(EntityManagement)
+    const featureHide = ConfigHelper.getValue('VUE_APP_FEATURE_HIDE')
+    if (!featureHide || !featureHide.USER_MGMT) {
+      this.menu.push({
+        title: 'Manage Teams',
+        icon: 'group',
+        activate: () => { this.setSelectedComponent(UserManagement) }
+      })
+    }
   }
 
   setSelectedComponent (selectedComponent: VueConstructor) {
