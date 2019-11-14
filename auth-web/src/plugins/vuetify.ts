9a5420c5166185456cb36be48ba5757bff81582e
jeznorth <twenty15creative@gmail.com>
2019-11-14 12:55:19 -0800

    [entity1867] Removing Unused Reference

--- auth-web/src/plugins/vuetify.ts
+++ auth-web/src/plugins/vuetify.ts
@@ -1,5 +1,3 @@
-import 'material-icons/iconfont/material-icons.css' // Ensure you are using css-loader
-import 'vuetify/dist/vuetify.min.css'
 import '$assets/scss/base.scss'
 import '$assets/scss/layout.scss'
 import '$assets/scss/overrides.scss'
@@ -10,7 +8,4 @@ import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'md'
-  }
 })
