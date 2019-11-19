9454ae268a056b77eaffe81f237b5838a9cfe80e
pwei1018 <patrick.wei@gov.bc.ca>
2019-11-19 11:57:08 -0800

    update

--- notify-api/main.py
+++ notify-api/main.py
@@ -1,8 +1,15 @@
 import uvicorn
+from starlette.responses import RedirectResponse
 from notify_api import NotifyAPI
 from notify_api.core import config as AppConfig
 
 app = NotifyAPI(bind=AppConfig.SQLALCHEMY_DATABASE_URI)
+
+
+@app.get("/")
+async def root():
+    response = RedirectResponse(url='/docs')
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5002)
