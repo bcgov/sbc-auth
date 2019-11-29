5258623f4770c70829838a5641f94e5e040b37ef
Andrew Sutherland <sutherlanda@gmail.com>
2019-11-29 14:37:48 -0800

    Prevent users from accepting invite when they already have membership

---
+++ auth-web/src/views/DuplicateTeamWarning.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <div class="view-container">
+      <article>
+        <h1 class="mb-5">Already On a Team</h1>
+        <p class="intro-text">You cannot accept this invitation, because you already have membership on a team</p>
+        <v-card class="profile-card">
+          <v-container>
+            <v-card-text>
+              <TeamForm />
+            </v-card-text>
+          </v-container>
+        </v-card>
+      </article>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { mapActions, mapState } from 'vuex'
+import { Organization } from '@/models/Organization'
+import TeamForm from '@/components/auth/TeamForm.vue'
+import { getModule } from 'vuex-module-decorators'
+
+@Component({
+  components: {
+    TeamForm
+  }
+})
+export default class DuplicateTeamWarning extends Vue {
+  async mounted () {}
+}
+</script>
+
+<style lang="scss" scoped>
+  article {
+    flex: 1 1 auto;
+    margin: 0 auto;
+    max-width: 50rem;
+  }
+
+  .v-card__title {
+    font-weight: 700;
+    letter-spacing: -0.01rem;
+  }
+
+  .intro-text {
+    margin-bottom: 3rem;
+  }
+
+  // Profile Card
+  .profile-card .container {
+    padding: 1rem;
+  }
+</style>
