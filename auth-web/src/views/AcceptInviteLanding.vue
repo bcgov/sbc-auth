44df84bf408111eaf930f49af546526023569369
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-12-19 15:47:54 -0800

    2103 : Redirecting the user to leaveteam confirmation.  Created a re-usable component and changed all similar UIs.

--- auth-web/src/views/AcceptInviteLanding.vue
+++ auth-web/src/views/AcceptInviteLanding.vue
@@ -1,35 +1,29 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" lg="8" class="text-center">
-        <div v-if="!tokenExpiry && !tokenError">
-          <v-icon size="48" color="primary" class="mb-6">mdi-login-variant</v-icon>
-          <h1 class="mb-7">{{ $t('acceptInviteLandingTitle') }}</h1>
-          <p class="mb-9">{{ $t('acceptInviteLandingMessage') }}</p>
+  <div>
+    <div v-if="!tokenExpired && !tokenError">
+      <interim-landing :summary="$t('acceptInviteLandingTitle')" :description="$t('acceptInviteLandingMessage')" icon="mdi-login-variant" showHomePageBtn="false">
+        <template v-slot:actions>
           <v-btn v-if="!isUserSignedIn()" large link color="primary" @click="redirectToSignin()">{{ $t('loginBtnLabel') }}</v-btn>
           <v-btn v-if="isUserSignedIn()" large link color="primary" @click="redirectToConfirm()">{{ $t('acceptButtonLabel') }}</v-btn>
-        </div>
-        <div v-if="tokenExpiry">
-          <v-icon size="48" color="error" class="mb-6">mdi-alert-circle-outline</v-icon>
-          <h1 class="mb-7">{{ $t('expiredInvitationTitle')}}</h1>
-          <p class="mb-9">{{ $t('expiredInvitationMessage')}}</p>
-          <v-btn large link color="primary" href="../">{{ $t('homeBtnLabel')}}</v-btn>
-        </div>
-        <div v-if="tokenError">
-          <v-icon size="48" color="error" class="mb-6">mdi-alert-circle-outline</v-icon>
-          <h1 class="mb-7">{{ $t('errorOccurredTitle')}}</h1>
-          <p class="mb-9">{{ $t('invitationProcessingErrorMsg')}}</p>
-          <v-btn large link color="primary" href="../">{{ $t('homeBtnLabel')}}</v-btn>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        </template>
+      </interim-landing>
+    </div>
+    <div v-if="tokenExpired">
+      <interim-landing :summary="$t('expiredInvitationTitle')" :description="$t('expiredInvitationMessage')" icon="mdi-alert-circle-outline" iconColor="error">
+      </interim-landing>
+    </div>
+    <div v-if="tokenError">
+      <interim-landing :summary="$t('errorOccurredTitle')" :description="$t('invitationProcessingErrorMsg')" icon="mdi-alert-circle-outline" iconColor="error">
+      </interim-landing>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import ConfigHelper from '@/util/config-helper'
 import { EmptyResponse } from '@/models/global'
+import InterimLanding from '@/components/auth/InterimLanding.vue'
 import OrgModule from '@/store/modules/org'
 import { getModule } from 'vuex-module-decorators'
 import { mapActions } from 'vuex'
@@ -37,7 +31,8 @@ import { mapActions } from 'vuex'
 @Component({
   methods: {
     ...mapActions('org', ['validateInvitationToken'])
-  }
+  },
+  components: { InterimLanding }
 })
 export default class AcceptInviteLanding extends Vue {
   private orgStore = getModule(OrgModule, this.$store);
@@ -78,16 +73,3 @@ export default class AcceptInviteLanding extends Vue {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-  @import "$assets/scss/theme.scss";
-
-  .container {
-    padding-top: 3rem;
-    padding-bottom: 3rem;
-  }
-
-  .v-icon {
-    font-size: 4rem;
-  }
-</style>
