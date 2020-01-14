5f97327a16940872f05f60f78f7e0d00116f359b
saravankumarpa <saravanpa@gmail.com>
2020-01-14 09:44:57 -0800

    lint fix

--- auth-api/src/auth_api/schemas/contact.py
+++ auth-api/src/auth_api/schemas/contact.py
@@ -27,7 +27,7 @@ class ContactSchema(CamelCaseSchema):  # pylint: disable=too-many-ancestors, too
         """Maps all of the User fields to a default schema."""
 
         model = ContactModel
-        exclude = ('id', )
+        exclude = ('id',)
 
     email = fields.String(data_key='email')
     phone = fields.String(data_key='phone')
