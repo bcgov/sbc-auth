8efa191358bb5f8162c4139f5648e3a2a5769423
Patrick Wei <44277752+pwei1018@users.noreply.github.com>
2020-01-21 21:59:58 -0800

    Update setenv_1pass.sh

--- 1pass/setenv_1pass.sh
+++ 1pass/setenv_1pass.sh
@@ -3,7 +3,7 @@
 # Login to 1Password.
 # Assumes you have installed the OP CLI and performed the initial configuration
 # For more details see https://support.1password.com/command-line-getting-started/
-eval $(echo $3 | op signin https://registries.1password.ca/ $1 $2)
+eval $(echo \'$3\' | op signin registries.1password.ca $1 $2)
 
 # My setup uses a 1Password type of 'Password' and stores all records within a
 # single section. The label is the key, and the value is the value.
