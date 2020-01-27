4b6a9a15d89d6fb620a24a6f9378e000ba404cb0
pwei1018 <patrick.wei@gov.bc.ca>
2020-01-27 14:55:31 -0800

    Update pylint

--- auth-api/src/auth_api/services/codes.py
+++ auth-api/src/auth_api/services/codes.py
@@ -1,85 +1,68 @@
 # Copyright © 2019 Province of British Columbia
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Tests to verify the User Service.
-
-Test-Suite to ensure that the User Service is working as expected.
-"""
+"""Service for retrieving the codes."""
 import importlib
-from unittest.mock import patch
-
-import pytest
 
 from auth_api.exceptions import BusinessException
 from auth_api.exceptions.errors import Error
-from auth_api.services import Codes as CodesService
+from auth_api.models import db
+from auth_api.models.base_model import BaseCodeModel
 
 
-def test_fetch_data_model(session):  # pylint: disable=unused-argument
-    """Assert that code type details can be fetch by table name."""
-    code_type = 'membership_type'
-    code_result = CodesService.fetch_data_model(code_type)
-    assert code_result not None
+class Codes:
+    """Retrieving the codes in DB.
 
+    This service manages retrieving the values from code, type or status tables.
+    """
 
-def test_fetch_data_model_not_found(session):  # pylint: disable=unused-argument
-    """Assert that code type details can be fetch by table name."""
-    code_type = 'membership_type1'
-    code_result = CodesService.fetch_data_model(code_type)
-    assert not code_result
+    def __init__(self):
+        """Return a code service instance."""
 
-    code_type = 'user'
-    code_result = CodesService.fetch_data_model(code_type)
-    assert not code_result
+    @staticmethod
+    def fetch_data_model(code_type: str = None):
+        """Return class reference mapped to table.
 
-    code_type = ''
-    code_result = CodesService.fetch_data_model(code_type)
-    assert not code_result
+        :param table_fullname: String with fullname of table.
+        :return: Class reference or None.
+        """
+        for model_class in db.Model._decl_class_registry.values():  # pylint:disable=protected-access
+            if hasattr(model_class, '__table__') and model_class.__table__.fullname == code_type:
+                if issubclass(model_class, BaseCodeModel):
+                    return model_class
 
+        return None
 
-def test_fetch_codes(session):  # pylint: disable=unused-argument
-    """Assert that code type details can be fetch by table name."""
-    code_type = 'membership_type'
-    data = CodesService.fetch_codes(code_type)
-    assert data is not None
-    assert data[0]['code'] == 'MEMBER'
+    @classmethod
+    def fetch_codes(cls, code_type: str = None) -> []:
+        """Return values from code table."""
+        try:
+            data: [] = None
+            if code_type:
+                code_model = Codes.fetch_data_model(code_type.lower())
 
+                if code_model:
+                    codes = code_model.query.all()
 
-def test_fetch_codes_not_found(session):  # pylint: disable=unused-argument
-    """Assert that code type details can not be fetch by table name."""
-    # Table is not exists
-    code_type = 'membership_type1'
-    data = CodesService.fetch_codes(code_type)
-    assert not data
-
-    data = CodesService.fetch_codes(None)
-    assert not data
-
-    data = CodesService.fetch_codes('')
-    assert not data
-
-    # The table is not the code, type or status table.
-    code_type = 'user'
-    data = CodesService.fetch_codes(code_type)
-    assert not data
-
-
-def test_fetch_codes_with_exception(session):  # pylint: disable=unused-argument
-    """Assert that code type details can not be fetch by table name."""
-    code_type = 'membership_type'
-    with patch.object(importlib, 'import_module', side_effect=Exception(Error.UNDEFINED_ERROR, None)):
-        with pytest.raises(BusinessException) as exception:
-            CodesService.fetch_codes(code_type)
-
-        assert exception.value.code == 'UNDEFINED_ERROR'
+                    data = []
+                    # transform each of entry to a dictionary base on schema.
+                    for entry in codes:
+                        module_name = f'auth_api.schemas.{entry.__tablename__}'
+                        class_name = f'{entry.__class__.__name__}Schema'
+                        schema = getattr(importlib.import_module(module_name), class_name)
+                        code_schema = schema()
+                        data.append(code_schema.dump(entry, many=False))
+                return data
+            return None
+        except Exception as exception:
+            raise BusinessException(Error.UNDEFINED_ERROR, exception)
