965adc3b2175ba81589c9e4e686941bf30911188
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-01-31 10:05:29 -0800

    new content refresh popup added

--- auth-web/src/registerServiceWorker.ts
+++ auth-web/src/registerServiceWorker.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import Swal from 'sweetalert2'
 
 const swPath = (process.env.NODE_ENV === 'production') ? `${process.env.BASE_URL}service-worker.js` : `./service-worker.js`
 
@@ -11,18 +12,23 @@ register(swPath, {
       'For more details, visit https://goo.gl/AFskqB'
     )
   },
-  registered () {
+  registered (event) {
     console.log('Service worker has been registered.')
+    console.log(event)
   },
-  cached () {
+  cached (event) {
     console.log('Content has been cached for offline use.')
+    console.log(event)
   },
-  updatefound () {
+  updatefound (event) {
     console.log('New content is downloading.')
+    console.log(event)
   },
-  updated () {
+  updated (event) {
     // write code for popup here
     console.log('New content is available; please refresh.')
+    showInteractiveToastReload('New content is available!')
+    console.log(event);
   },
   offline () {
     // add offline indication here
@@ -32,3 +38,27 @@ register(swPath, {
     console.error('Error during service worker registration:', error)
   }
 })
+
+
+const showInteractiveToastReload = (text) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-right',
+    showConfirmButton: true,
+    confirmButtonText: 'Refresh',
+    timer: 0,
+    width: '100',
+    grow: 'fullscreen',
+    padding: '1.3rem 3rem',
+  })
+  
+  Toast.fire({
+    icon: 'info',
+    title: text,
+  }).then((result) => {
+    if (result.value) {
+      window.location.reload(true)
+    }
+  })
+
+}
