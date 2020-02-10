2db7e2638c5cc6dae5d674734b967ad9058be58c
jeznorth <twenty15creative@gmail.com>
2020-02-10 10:08:49 -0800

    [entity1917] UI Updates

--- auth-web/src/views/auth/HomeView.vue
+++ auth-web/src/views/auth/HomeView.vue
@@ -32,7 +32,12 @@
                 <v-card>
                   <v-card-title>Create a BC Registries Account</v-card-title>
                   <v-card-text>
-                    To create a BC Registries account you will need to log in using your BC Services Card, the government of British Columbia's trusted way to access online services.
+                    <p class="mb-7">To access this service, you will need to log in using your BC Services Card, the government of British Columbia's trusted way to access online services. Learn how to set up and use your BC Services card here.</p>
+                    <a href="https://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card/login-with-card"
+                      target="_blank">
+                      <v-icon small class="mr-2">mdi-open-in-new</v-icon>
+                      <span>Learn how to set up and use your BC Services Card</span>
+                    </a>
                   </v-card-text>
                   <v-card-actions>
                     <v-spacer></v-spacer>
@@ -479,5 +484,19 @@ export default class HomeView extends Vue {
   // Fix initial display of the dialog container
   .v-dialog__container {
     display: none;
+  }
+
+  a {
+    text-decoration: none;
+    font-weight: 700;
+    font-size: 0.875rem;
+
+    .v-icon {
+      color: inherit;
+    }
+
+    span {
+      text-decoration: underline;
+    }
   }
 </style>
