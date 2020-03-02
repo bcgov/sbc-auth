6f49c5471a4513d426dcf926a5d60cfb29fc0d9b
saravankumarpa <saravanpa@gmail.com>
2020-03-02 15:09:21 -0800

    Added missing roles

--- auth-api/src/auth_api/resources/org_products.py
+++ auth-api/src/auth_api/resources/org_products.py
@@ -39,6 +39,7 @@ class OrgProducts(Resource):
     @staticmethod
     @TRACER.trace()
     @cors.crossdomain(origin='*')
+    @_JWT.has_one_of_roles([Role.STAFF_ADMIN.value])
     def post(org_id):
         """Post a new product subscription to the org using the request body."""
         request_json = request.get_json()
