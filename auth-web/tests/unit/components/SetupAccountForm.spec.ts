e81a99d6c3cfdc745793940d01f3e24e82b7cbe4
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-03-04 13:54:01 -0800

    added test for setup account components

---
+++ auth-web/tests/unit/components/SetupAccountForm.spec.ts
@@ -0,0 +1,96 @@
+import { Wrapper, createLocalVue, mount } from '@vue/test-utils'
+import SetupAccountForm from '@/components/auth/staff/SetupAccountForm.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+describe('SetupAccountForm.vue', () => {
+  let wrapper: Wrapper<SetupAccountForm>
+  const config = {
+    'VUE_APP_ROOT_API': 'https://localhost:8080/api/v1/11',
+    'VUE_APP_COPS_REDIRECT_URL': 'https://coops-dev.pathfinder.gov.bc.ca/',
+    'VUE_APP_PAY_ROOT_API': 'https://pay-api-dev.pathfinder.gov.bc.ca/api/v1'
+  }
+
+  sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const staffModule = {
+      namespaced: true,
+      state: {
+        products: [
+          {
+            'code': 'PPR',
+            'default': false,
+            'desc': 'Personal Property Registry'
+          },
+          {
+            'code': 'DIR_SEARCH',
+            'default': false,
+            'desc': 'Director Search'
+          }
+        ],
+        accountTypes: [
+          {
+            'code': 'IMPLICIT',
+            'default': true,
+            'desc': 'Implicit organization for internal user only'
+          },
+          {
+            'code': 'EXPLICIT',
+            'default': false,
+            'desc': 'Explicity named organization that can have multiple members'
+          },
+          {
+            'code': 'PUBLIC',
+            'default': false,
+            'desc': 'PUBLIC'
+          }
+        ]
+      }
+    }
+
+    const vuetify = new Vuetify({})
+
+    const store = new Vuex.Store({
+      state: {},
+      strict: false,
+      modules: {
+        staff: staffModule
+      }
+    })
+
+    wrapper = mount(SetupAccountForm, {
+      store,
+      localVue,
+      vuetify
+    })
+
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('business contact form has submit and cancel buttons', () => {
+    expect(wrapper.find('.submit-form-btn')).toBeTruthy()
+    expect(wrapper.find('.cancel-btn')).toBeTruthy()
+  })
+
+  it('confirm account name is empty', () => {
+    expect(wrapper.vm.$data.accountName).toBe('')
+  })
+
+  it('confirm email to be empty', () => {
+    expect(wrapper.vm.$data.email).toBe('')
+  })
+})
