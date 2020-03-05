198e143c0ff531d815171d591944bfa306b502db
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-03-05 14:15:22 -0800

    rolled out an api for notification count

---
+++ auth-api/src/auth_api/resources/notifications.py
@@ -0,0 +1,48 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""API endpoints for managing an Org resource."""
+
+from auth_api import status as http_status
+from auth_api.exceptions import BusinessException
+from auth_api.jwt_wrapper import JWTWrapper
+from auth_api.services import Membership as MembershipService
+from auth_api.tracer import Tracer
+from auth_api.utils.roles import Role
+from auth_api.utils.util import cors_preflight
+from flask import g
+from flask_restplus import Namespace, Resource, cors
+
+API = Namespace('notifications', description='Endpoints for notification management')
+TRACER = Tracer.get_instance()
+_JWT = JWTWrapper.get_instance()
+
+
+@cors_preflight('GET,OPTIONS')
+@API.route('', methods=['GET', 'OPTIONS'])
+class Notifications(Resource):
+    """Resource for managing the notifications."""
+
+    @staticmethod
+    @TRACER.trace()
+    @cors.crossdomain(origin='*')
+    @_JWT.has_one_of_roles([Role.SYSTEM.value, Role.STAFF.value, Role.PUBLIC_USER.value])
+    def get(user_id, org_id):
+        """Finds the count of notification remaining.If any details invalid, it returns zero"""
+        try:
+            pending_count = MembershipService.get_pending_member_count_for_org(org_id,
+                                                                               token_info=g.jwt_oidc_token_info)
+            response, status = {'count': pending_count}, http_status.HTTP_200_OK
+        except BusinessException as exception:
+            response, status = {'code': exception.code, 'message': exception.message}, exception.status_code
+        return response, status
