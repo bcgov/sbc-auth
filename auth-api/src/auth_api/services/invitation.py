8981b3066fe6c5ee025666a3994e5180693e1111
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-03-11 15:13:46 -0700

    corrected role

--- auth-api/src/auth_api/services/invitation.py
+++ auth-api/src/auth_api/services/invitation.py
@@ -31,7 +31,7 @@ from auth_api.models.org import Org as OrgModel
 from auth_api.schemas import InvitationSchema
 from auth_api.services.user import User as UserService
 from auth_api.utils.constants import InvitationStatus
-from auth_api.utils.roles import ADMIN, MEMBER, OWNER, Status, Role, InvitationType, STAFF
+from auth_api.utils.roles import ADMIN, MEMBER, OWNER, Status, Role, InvitationType, STAFF, STAFF_ADMIN
 from config import get_named_config
 
 from .authorization import check_auth
@@ -67,7 +67,7 @@ class Invitation:
         for membership in invitation_info['membership']:
             org_id = membership['orgId']
             if invitation_info.get('type') == InvitationType.DIRECTOR_SEARCH.value:
-                check_auth(token_info, org_id=org_id, equals_role=STAFF)
+                check_auth(token_info, org_id=org_id, equals_role=STAFF_ADMIN)
             else:
                 check_auth(token_info, org_id=org_id, one_of_roles=(OWNER, ADMIN))
         # TODO doesnt work when invited to multiple teams.. Re-work the logic when multiple teams introduced
