3e9220de749991939fb6f0f43d5c633daec95fdd
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-03-12 17:04:30 -0700

    create user profile and terms of use page created

---
+++ auth-web/src/components/auth/CreateUserProfileForm.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-form ref="form" lazy-validation>
+    <v-expand-transition>
+      <div class="form_alert-container" v-show="formError">
+        <v-alert type="error" class="mb-0"
+                 :value="true"
+        >
+          {{formError}}
+        </v-alert>
+      </div>
+    </v-expand-transition>
+    <!-- Username -->
+    <v-row>
+      <v-col cols="12" class="pt-0 pb-0">
+        <v-text-field
+            filled
+            label="Username"
+            req
+            persistent-hint
+            :rules="usernameRules"
+            v-model="username"
+            data-test="username"
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <!-- Password -->
+    <v-row>
+      <v-col cols="12" class="pt-0 pb-0">
+        <v-text-field
+            filled
+            label="Password"
+            req
+            persistent-hint
+            :rules="passwordRules"
+            v-model="password"
+            data-test="password"
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <!-- Confirm Password -->
+    <v-row>
+      <v-col cols="12" class="pt-0 pb-0">
+        <v-text-field
+            filled
+            label="Confirm Email Address"
+            req
+            persistent-hint
+            :error-messages="passwordMustMatch()"
+            v-model="confirmPassword"
+            data-test="confirm-password"
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" class="form__btns pt-5">
+          <v-btn large color="primary" class="save-continue-button" :disabled='!isFormValid()' @click="nextStep" data-test="next-button">
+            Next
+          </v-btn>
+          <v-btn large depressed @click="cancel" data-test="cancel-button" class="cancel-button">Cancel</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, Prop, Vue } from 'vue-property-decorator'
+import ModalDialog from '@/components/auth/ModalDialog.vue'
+import NextPageMixin from '@/components/auth/NextPageMixin.vue'
+import { Organization } from '@/models/Organization'
+
+@Component({
+  components: {
+    ModalDialog
+  }
+})
+export default class CreateUserProfileForm extends Mixins(NextPageMixin) {
+    private username = ''
+    private password = ''
+    private confirmPassword = ''
+    private formError = ''
+    private editing = false
+
+    $refs: {
+      form: HTMLFormElement
+    }
+
+    private usernameRules = [
+      v => !!v || 'Username is required'
+    ]
+
+    private passwordRules = [
+      v => !!v || 'Password is required'
+    ]
+
+    private passwordMustMatch (): string {
+      return (this.password === this.confirmPassword) ? '' : 'Passwords must'
+    }
+
+    private isFormValid (): boolean {
+      return this.$refs.form &&
+        this.$refs.form.validate() &&
+        !this.passwordMustMatch()
+    }
+
+    private async mounted () {
+    }
+
+    private async nextStep () {
+      if (this.isFormValid()) {
+        this.redirectToNext()
+      }
+    }
+
+    private redirectToNext () {
+      this.$router.push(this.getNextPageUrl())
+    }
+
+    private cancel () {
+      window.history.back()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '$assets/scss/theme.scss';
+
+  .form__btns {
+    display: flex;
+    justify-content: flex-end;
+
+    .v-btn + .v-btn {
+      margin-left: 0.5rem;
+    }
+  }
+</style>
---
+++ auth-web/src/views/auth/TermsOfServiceView.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="view-container">
+
+    <!-- Loading status -->
+    <v-fade-transition>
+      <div class="loading-container" v-if="isLoading">
+        <v-progress-circular size="50" width="5" color="primary" :indeterminate="isLoading"/>
+      </div>
+    </v-fade-transition>
+    <div class="" v-if="!isLoading">
+      <v-row justify="center">
+        <v-col lg="8" class="pt-0 pb-0">
+          <div class="view-header">
+            <h1>Terms of Use</h1>
+          </div>
+          <v-card class="profile-card" flat>
+            <v-container class="">
+              <v-card-text>
+                <terms-of-use></terms-of-use>
+              </v-card-text>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import TermsOfUse from '@/components/auth/TermsOfUse.vue'
+import Vue from 'vue'
+
+@Component({
+  components: {
+    TermsOfUse
+  }
+})
+export default class TermsOfServiceView extends Vue {
+  private isLoading: boolean = false
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
