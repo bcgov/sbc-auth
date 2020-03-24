f71d0ed78e42665995937692183ad5a1fc195251
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-03-23 21:55:55 -0700

    corrected pylint and fied tests

--- auth-api/tests/unit/api/test_org.py
+++ auth-api/tests/unit/api/test_org.py
@@ -219,6 +219,7 @@ def test_get_org(client, jwt, session, keycloak_mock):  # pylint:disable=unused-
     dictionary = json.loads(rv.data)
     assert dictionary['id'] == org_id
 
+
 def test_get_org_no_auth_returns_401(client, jwt, session, keycloak_mock):  # pylint:disable=unused-argument
     """Assert that an org cannot be retrieved without an authorization header."""
     headers = factory_auth_header(jwt=jwt, claims=TestJwtClaims.public_role)
@@ -230,8 +231,8 @@ def test_get_org_no_auth_returns_401(client, jwt, session, keycloak_mock):  # py
     rv = client.get('/api/v1/orgs/{}'.format(org_id),
                     headers=None, content_type='application/json')
     assert rv.status_code == http_status.HTTP_401_UNAUTHORIZED
-    
-    
+
+
 def test_get_org_no_org_returns_404(client, jwt, session):  # pylint:disable=unused-argument
     """Assert that attempting to retrieve a non-existent org returns a 404."""
     headers = factory_auth_header(jwt=jwt, claims=TestJwtClaims.public_role)
