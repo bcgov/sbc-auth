51b55e59ec0a77fd905998c72e1399960d8de1f1
pwei1018 <patrick.wei@gov.bc.ca>
2020-03-25 09:26:12 -0700

    Merge alembic scripts.

--- auth-api/migrations/versions/09c44f82c03c_add_folio_number_to_entity_table.py
+++ auth-api/migrations/versions/09c44f82c03c_add_folio_number_to_entity_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '09c44f82c03c'
-down_revision = '68e2f43b9d22'
+down_revision = '19ffc2fcc88c'
 branch_labels = None
 depends_on = None
 
