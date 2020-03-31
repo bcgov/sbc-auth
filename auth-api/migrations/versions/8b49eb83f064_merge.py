60e7321b15d9a1e475d0e8779ee8711dec70123f
Andrew Sutherland <sutherlanda@gmail.com>
2020-03-31 08:24:09 -0700

    Merge alembic migrations

---
+++ auth-api/migrations/versions/8b49eb83f064_merge.py
@@ -0,0 +1,24 @@
+"""merge
+
+Revision ID: 8b49eb83f064
+Revises: 3013ef68d7f1, c7d122dc4e06
+Create Date: 2020-03-31 08:17:04.062880
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b49eb83f064'
+down_revision = ('3013ef68d7f1', 'c7d122dc4e06')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
