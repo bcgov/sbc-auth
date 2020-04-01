6eb8a0b663d0b485d3e9a82845652a9ca4a7eed9
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-03-31 17:21:30 -0700

    User accepts the invite and inviter deactivates the account#3193

--- auth-web/src/views/auth/PendingApprovalView.vue
+++ auth-web/src/views/auth/PendingApprovalView.vue
@@ -1,26 +1,47 @@
 <template>
   <div>
-    <div v-if="$route.params.team_name">
-      <interim-landing :summary="$t('pendingInvitationTitle', { team: $route.params.team_name })" :description="$t('pendingInvitationMsg')" icon="mdi-information-outline">
+    <template v-if="isDenied">
+      <interim-landing :summary="$t('deniedInvitationTitle', { team: $route.params.team_name })" :description="$t('deniedInvitationMsg')" icon="mdi-alert-circle-outline" iconColor="error">
       </interim-landing>
-    </div>
-    <div v-if="!$route.params.team_name">
-      <interim-landing :summary="$t('noPendingInvitationTitle')" :description="$t('noPendingInvitationMsg')" icon="mdi-information-outline">
-      </interim-landing>
-    </div>
+    </template>
+    <template v-else>
+      <div v-if="$route.params.team_name">
+        <interim-landing :summary="$t('pendingInvitationTitle', { team: $route.params.team_name })" :description="$t('pendingInvitationMsg')" icon="mdi-information-outline">
+        </interim-landing>
+      </div>
+      <div v-if="!$route.params.team_name">
+        <interim-landing :summary="$t('noPendingInvitationTitle')" :description="$t('noPendingInvitationMsg')" icon="mdi-information-outline">
+        </interim-landing>
+      </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
+import { Member, MembershipStatus } from '@/models/Organization'
 import { Component } from 'vue-property-decorator'
 import InterimLanding from '@/components/auth/InterimLanding.vue'
 import { KCUserProfile } from 'sbc-common-components/src/models/KCUserProfile'
+import OrgModule from '@/store/modules/org'
 import { Role } from '@/util/constants'
 import Vue from 'vue'
 import { mapState } from 'vuex'
 
 @Component({
-  components: { InterimLanding }
+  components: { InterimLanding },
+  computed: {
+    ...mapState('org', [
+      'currentMembership'
+    ])
+  }
 })
-export default class PendingApprovalView extends Vue { }
+
+export default class PendingApprovalView extends Vue {
+  protected readonly currentMembership!: Member
+  private isDenied: boolean = false
+
+  mounted () {
+    this.isDenied = !(this.currentMembership?.membershipStatus)
+  }
+}
 </script>
