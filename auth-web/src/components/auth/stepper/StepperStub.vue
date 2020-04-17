7b916540e7cf433aae8c3cd6533e2711bb7bb4eb
Andrew Sutherland <sutherlanda@gmail.com>
2020-04-17 11:49:52 -0700

    2539 refactor stepper
    * extract into component
    * simplify configuration

---
+++ auth-web/src/components/auth/stepper/StepperStub.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card flat>
+    <v-card-title>Component Title</v-card-title>
+    <v-card-text>
+      <p>Component Content</p>
+      <v-card-actions>
+        <v-btn @click="back()">Back</v-btn>
+        <v-btn @click="next()">Next Step</v-btn>
+      </v-card-actions>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component({
+  name: 'StepperStub'
+})
+export default class StepperStub extends Vue {
+  @Prop() stepForward!: () => void
+  @Prop() stepBack!: () => void
+
+  private next () {
+    this.stepForward()
+  }
+
+  private back () {
+    this.stepBack()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
