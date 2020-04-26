6e38ed5c7993f2c6b94dffe2d3dd9ec8ae984dd3
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-04-26 09:51:53 -0700

    minor refactoring

--- auth-web/.eslintrc.js
+++ auth-web/.eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     '@vue/typescript'
   ],
   rules: {
+    'no-console': 'error',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'sort-imports': 'error',
     'space-before-function-paren': 1
   },
