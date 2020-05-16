7b40447d423c7b1c6267f91a62fc40f1d8caef38
Cameron Bowler <cameron@freshworks.io>
2020-05-15 17:00:27 -0700

    Finished Stepper

--- auth-web/src/components/auth/stepper/InfoStepper.vue
+++ auth-web/src/components/auth/stepper/InfoStepper.vue
@@ -2,13 +2,19 @@
   <v-container id="step-buttons-container">
     <template v-for="(step, index) in steps">
       <div class="step" :key="index">
-        <v-btn class="step__icon" :id=step.id :outlined="true" fab color="#1A5A96" :to=step.to :disabled=step.disabled>
+        <v-btn
+          fab
+          class="step__icon"
+          active-class="no-active filled"
+          :id=step.id :outlined="true"
+          color="#1A5A96"
+          :to=step.to>
           <v-icon>{{step.step}}</v-icon>
         </v-btn>
-        <v-btn class="step__label" text color="#1A5A96" :to=step.to :disabled=step.disabled>
+        <v-btn class="step__label" active-class="no-active selected" text color="#1A5A96" :to=step.to>
           {{step.text}}
         </v-btn>
-        <div class="arrow-down"></div>
+        <span :class="{ 'arrow-down': isCurrentStep(step) }"></span>
       </div>
     </template>
   </v-container>
@@ -24,32 +30,32 @@ export default class Stepper extends Vue {
     {
       id: 'step-1-btn',
       step: 1,
-      icon: 'mdi-number-1',
       text: 'Decide on a Business Type',
       to: '/decide-business-info'
     },
     {
       id: 'step-2-btn',
       step: 2,
-      icon: 'mdi-number-2',
       text: 'Request a Name',
       to: '/request-name-info'
     },
     {
       id: 'step-3-btn',
       step: 3,
-      icon: 'mdi-number-3',
       text: 'Incorporate or Register',
       to: '/incorp-or-register-info'
     },
     {
       id: 'step-4-btn',
       step: 4,
-      icon: 'mdi-number-4',
       text: 'Maintain Your Business',
       to: '/maintain-business-info'
     }
   ]
+
+  private isCurrentStep (step: any): boolean {
+    return this.$route.path === step.to
+  }
 }
 </script>
 
@@ -60,12 +66,25 @@ export default class Stepper extends Vue {
     display: flex;
   }
 
+  .v-btn--active.no-active::before {
+    opacity: 0 !important;
+  }
+
+  .filled {
+    background-color: $BCgoveBueText1!important;
+    color: $grat1B!important;
+  }
+
+  .selected {
+    color: #212529!important;
+    border-bottom: 3px solid $BCgoveBueText1!important;
+  }
+
   .step {
     display: flex;
     flex-direction: column;
     flex: 1;
     align-items: center;
-    justify-content: center;
   }
 
   .step__icon {
@@ -81,7 +100,7 @@ export default class Stepper extends Vue {
     margin-top: 10px;
     padding-bottom: 20px!important;
     text-align: center;
-    border-bottom: 3px solid $BCgoveBueText1;
+    border-bottom: 3px solid #DEE2E6;
     border-bottom-left-radius: 0%;
     border-bottom-right-radius: 0%;
   }
