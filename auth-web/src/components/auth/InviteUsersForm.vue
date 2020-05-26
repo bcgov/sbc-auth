516a83e62ba293df111a8eb9ac9ed2a220958e82
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-05-26 08:40:14 -0700

    wrong vars fixed (#716)

--- auth-web/src/components/auth/InviteUsersForm.vue
+++ auth-web/src/components/auth/InviteUsersForm.vue
@@ -35,7 +35,7 @@
                   </v-list-item-icon>
                   <v-list-item-content>
                     <v-list-item-title>{{ item.displayName }}</v-list-item-title>
-                    <v-list-item-subtitle>{{ item.desc }}</v-list-item-subtitle>
+                    <v-list-item-subtitle>{{ item.label }}</v-list-item-subtitle>
                   </v-list-item-content>
                 </div>
               </template>
