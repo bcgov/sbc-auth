5d7e6a9e47ad55a8566260b3b5e57f1e79b542b2
Cameron Bowler <cameron@freshworks.io>
2020-06-05 12:29:49 -0700

    Flagged the navbar on the home route

--- auth-web/src/router.ts
+++ auth-web/src/router.ts
@@ -64,7 +64,7 @@ export function getRoutes (): RouteConfig[] {
       name: 'home',
       component: getEnvHomeView(),
       children: getEnvChildRoutes(),
-      meta: { showNavBar: true }
+      meta: { showNavBar: !flagCondition }
     },
     { path: '/business',
       name: 'business-root',
