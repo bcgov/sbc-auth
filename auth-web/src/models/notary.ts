93e9247c89cd39d2cd080a78d6728b8c56e44427
Nitheesh T Ganesh <60233274+nitheesh-aot@users.noreply.github.com>
2020-06-11 11:04:45 -0700

    download fail alert (#764)
    * download fail alert
    
    * extra provincial stepper - inprogess
    
    * configured to hit minio
    
    Co-authored-by: saravanpa-aot <saravankumar.pa@aot-technologies.com>

---
+++ auth-web/src/models/notary.ts
@@ -0,0 +1,17 @@
+export interface NotaryInformation {
+    notaryName?: string
+    city?: string
+    country?: string
+    street?: string
+    streetAdditional?: string
+    postalCode?: string
+    region?: string
+    key?: string
+}
+
+export interface NotaryContact {
+    email?: string
+    phone?: string
+    extension?: string
+    key?: string
+}
