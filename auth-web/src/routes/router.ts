b43baaa9e325c5e6c8978329c633208115fda313
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-06-15 11:38:02 -0700

    route changes from master

--- auth-web/src/routes/router.ts
+++ auth-web/src/routes/router.ts
@@ -107,7 +107,10 @@ export function getRoutes (): RouteConfig[] {
         {
           path: 'transactions',
           name: 'transactions',
-          component: transaction
+          component: transaction,
+          meta: {
+            isPremiumOnly: true
+          }
         }
       ]
     },
