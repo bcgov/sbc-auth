4e4e3af332d11c94fe5e4a336b37fecd4c063cf1
pwei1018 <patrick.wei@gov.bc.ca>
2020-07-06 23:12:31 -0700

    Integration test update.

--- integration-tests/tests/conftest.py
+++ integration-tests/tests/conftest.py
@@ -17,25 +17,24 @@ Examples:
         py.visit('https://google.com')
         assert 'Google' in py.title
 """
-
-import _pytest
 import json
+import logging
 import os
 import shutil
+import sys
 import time
-from typing import Dict, Tuple
 
 import pytest
 import requests
 from faker import Faker
+from pytest_reportportal import RPLogger, RPLogHandler
+
 from pylenium import Pylenium
 from pylenium.config import PyleniumConfig, TestCase
-from pylenium.logging import Logger
 
 
 def make_dir(filepath) -> bool:
     """ Make a directory.
-
     Returns:
         True if successful, False if not.
     """
@@ -58,10 +57,31 @@ def api():
     return requests
 
 
+@pytest.fixture(scope="session")
+def logger(request):
+    """ Report Portal Logger """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    # Create handler for Report Portal if the service has been
+    # configured and started.
+    if hasattr(request.node.config, 'py_test_service'):
+        # Import Report Portal logger and handler to the test module.
+        logging.setLoggerClass(RPLogger)
+        rp_handler = RPLogHandler(request.node.config.py_test_service)
+        # Add additional handlers if it is necessary
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.INFO)
+        logger.addHandler(console_handler)
+    else:
+        rp_handler = logging.StreamHandler(sys.stdout)
+    # Set INFO level for Report Portal handler.
+    rp_handler.setLevel(logging.INFO)
+    return logger
+
+
 @pytest.fixture(scope='session', autouse=True)
 def project_root() -> str:
     """ The Project (or Workspace) root as a filepath.
-
     * This conftest.py file should be in the Project Root if not already.
     """
     return os.path.dirname(os.path.abspath(__file__))
@@ -70,11 +90,10 @@ def project_root() -> str:
 @pytest.fixture(scope='session', autouse=True)
 def test_run(project_root, request) -> str:
     """ Creates the `/test_results` directory to store the results of the Test Run.
-
     Returns:
         The `/test_results` directory as a filepath (str).
     """
-    #session = request.node
+    session = request.node
     test_results_dir = f'{project_root}/test_results'
 
     if os.path.exists(test_results_dir):
@@ -84,9 +103,9 @@ def test_run(project_root, request) -> str:
         # create /test_results for this Test Run
         make_dir(test_results_dir)
 
-    # for test in session.items:
+    for test in session.items:
         # make the test_result directory for each test
-        # make_dir(f'{test_results_dir}/{test.name}')
+        make_dir(f'{test_results_dir}/{test.name}')
 
     return test_results_dir
 
@@ -94,7 +113,6 @@ def test_run(project_root, request) -> str:
 @pytest.fixture('session')
 def py_config(project_root, request) -> PyleniumConfig:
     """ Initialize a PyleniumConfig for each test
-
     1. This starts by deserializing the user-created pylenium.json from the Project Root.
     2. If that file is not found, then proceed with Pylenium Defaults.
     3. Then any CLI arguments override their respective key/values.
@@ -105,10 +123,10 @@ def py_config(project_root, request) -> PyleniumConfig:
             _json = json.load(file)
         config = PyleniumConfig(**_json)
     except FileNotFoundError:
-        # pylenium.json not found, proceed with defaults
+        # 2. pylenium.json not found, proceed with defaults
         config = PyleniumConfig()
 
-    # Override with any CLI args/options
+    # 3. Override with any CLI args/options
     # Driver Settings
     cli_remote_url = request.config.getoption('--remote_url')
     if cli_remote_url:
@@ -149,74 +167,29 @@ def py_config(project_root, request) -> PyleniumConfig:
     return config
 
 
-@pytest.fixture(scope='session', autouse=True)
-def logger(test_run, py_config, request) -> Logger:
-    """ Manages data pertaining to the currently running Test Function or Case.
-
-        * Creates the test-specific logger.
-
-    Args:
-        test_run: The Test Run (or Session) this test is connected to.
-
-    Returns:
-        An instance of Logger.
-    """
-    test_name = request.node.name
-    test_result_path = f'{test_run}'
-
-    return Logger(test_name, test_result_path, py_config.logging.pylog_level)
-
-
 @pytest.fixture(scope='session')
-def test_case(test_run, py_config, logger, request) -> TestCase:
+def test_case(test_run, py_config, request) -> TestCase:
     """ Manages data pertaining to the currently running Test Function or Case.
-
         * Creates the test-specific logger.
-
     Args:
         test_run: The Test Run (or Session) this test is connected to.
-
     Returns:
         An instance of TestCase.
     """
     test_name = request.node.name
-    test_result_path = f'{test_run}'
+    test_result_path = f'{test_run}/{test_name}'
     py_config.driver.capabilities.update({'name': test_name})
-
-    test = {
-        'name': test_name,
-        'file_path': test_result_path,
-        'logger': logger
-    }
-    return TestCase(**test)
+    return TestCase(name=test_name, file_path=test_result_path)
 
 
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     """ Yield each test's outcome so we can handle it in other fixtures. """
     outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
-
-
-@pytest.fixture(scope='session')
-def selenium(docker_services):
-    """Spin up a keycloak instance and initialize jwt."""
-    docker_services.start('selenium-hub')
-    docker_services.start('chrome')
-    docker_services.start('firefox')
-    time.sleep(30)
-    docker_services.wait_for_service('selenium-hub', 4444)
-
-
-@pytest.fixture(scope='session')
-def docker_compose_files(pytestconfig):
-    """Get the docker-compose.yml absolute path."""
-    import os
-    return [
-        os.path.join(str(pytestconfig.rootdir), 'tests/docker', 'docker-compose.yml')
-    ]
+    report = outcome.get_result()
+    if report.when == 'call':
+        setattr(item, "report", report)
+    return report
 
 
 def pytest_addoption(parser):
@@ -240,7 +213,6 @@ def pytest_addoption(parser):
         '--caps', action='store',
         default='', help='List of key-value pairs. Ex. \'{"name": "value", "boolean": true}\''
     )
-
     parser.addoption(
         '--page_load_wait_time', action='store',
         default='', help='The amount of time to wait for a page load before raising an error. Default is 0.'
@@ -251,20 +223,23 @@ def pytest_addoption(parser):
     )
 
 
-def pytest_runtest_makereport(item, call):
-    if "incremental" in item.keywords:
-        if call.excinfo is not None:
-            if call.excinfo.type in {_pytest.outcomes.Skipped, _pytest.outcomes.XFailed}:
-                return
+@pytest.fixture(scope='session')
+def selenium(docker_services):
+    """Spin up a keycloak instance and initialize jwt."""
+    docker_services.start('selenium-hub')
+    docker_services.start('chrome')
+    docker_services.start('firefox')
+    time.sleep(30)
+    docker_services.wait_for_service('selenium-hub', 4444)
 
-            parent = item.parent
-            parent._previousfailed = item
 
-
-def pytest_runtest_setup(item):
-    previousfailed = getattr(item.parent, "_previousfailed", None)
-    if previousfailed is not None:
-        pytest.xfail("previous test failed (%s)" % previousfailed.name)
+@pytest.fixture(scope='session')
+def docker_compose_files(pytestconfig):
+    """Get the docker-compose.yml absolute path."""
+    import os
+    return [
+        os.path.join(str(pytestconfig.rootdir), 'tests/docker', 'docker-compose.yml')
+    ]
 
 
 pytest_plugins = [
