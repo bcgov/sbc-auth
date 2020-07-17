80a764781561490fa4fd7d7ad164abb7543c92c9
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-07-16 19:21:24 -0700

    ui clean up - inprogress

--- auth-web/src/components/auth/AccountLoginOptionPicker.vue
+++ auth-web/src/components/auth/AccountLoginOptionPicker.vue
@@ -1,88 +1,43 @@
 <template>
-  <v-container class="view-container">
-    <div>
-      <v-row>
-        <v-col class="d-flex align-stretch" sm="12" md="6">
-          <v-card
-            flat
-            outlined
-            hover
-            class="account-card text-center px-10 pt-9 pb-12 elevation-2 d-flex"
-            @click="selectAuthType(loginSourceEnum.BCSC)"
-            :class="{ active: authType == loginSourceEnum.BCSC }"
+  <v-row>
+    <v-col
+      class="d-flex align-stretch"
+      sm="12" md="6"
+      v-for="authOption in authOptions"
+      :key="authOption.type"
+    >
+      <v-card
+        flat
+        outlined
+        hover
+        class="account-card text-center pa-7 d-flex flex-column"
+        @click="selectAuthType(authOption.type)"
+        :class="{ 'active': authType === authOption.type }"
+      >
+        <div class="account-type__icon mb-6 mt-2">
+          <v-icon color="grey">{{authOption.icon}}</v-icon>
+        </div>
+        <div class="account-type__title mb-6">
+          {{authOption.title}}
+        </div>
+        <div class="account-type__details mb-6 mx-4">
+          {{authOption.description}}
+        </div>
+        <div class="mt-9 mb-2">
+          <v-btn
+            large
+            depressed
+            block
+            color="primary"
+            class="font-weight-bold"
+            :outlined="authType != authOption.type"
           >
-            <div class="account-type d-flex flex-column">
-              <div class="account-type__icon mb-6">
-                <v-icon>mdi-account-circle-outline</v-icon>
-              </div>
-              <div class="account-type__title mb-6">
-                BC Services Card
-              </div>
-              <div class="account-type__details mb-6">
-                Use your BC Services Card with a mobile app or a USB card reader
-                to verify your identity.
-              </div>
-              <div>
-                <a
-                  href="https://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card/log-in-with-card/mobile-card"
-                  target="_blank" rel="noopener noreferrer"
-                  >Learn more about the BC Services card</a
-                >
-              </div>
-              <div class="mt-9 mb-2">
-                <v-btn
-                  large
-                  depressed
-                  block
-                  color="primary"
-                  class="font-weight-bold"
-                  :outlined="authType != loginSourceEnum.BCSC"
-                >
-                  {{ authType == loginSourceEnum.BCSC ? 'SELECTED' : 'SELECT' }}
-                </v-btn>
-              </div>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col class="d-flex align-stretch" sm="12" md="6">
-          <v-card
-            flat
-            outlined
-            hover
-            class="account-card text-center px-10 pt-9 pb-12 elevation-2 d-flex"
-            @click="selectAuthType(loginSourceEnum.BCEID)"
-            :class="{ active: authType == loginSourceEnum.BCEID}"
-          >
-            <div class="account-type d-flex flex-column">
-              <div class="account-type__icon mb-6">
-                <v-icon>mdi-two-factor-authentication</v-icon>
-              </div>
-              <div class="account-type__title mb-6">
-                BCeID and 2-factor authentication app
-              </div>
-              <div class="account-type__details mb-6">
-                Login with a BCeID combined with a verification code in a mobile
-                app, such as Google or Microsoft Authenticator.
-              </div>
-              <!-- State Button (Create Account) -->
-              <div class="mt-9 mb-2">
-                <v-btn
-                  large
-                  depressed
-                  block
-                  color="primary"
-                  class="font-weight-bold"
-                  :outlined="authType != loginSourceEnum.BCEID"
-                >
-                  {{ authType == loginSourceEnum.BCEID ? 'SELECTED' : 'SELECT' }}
-                </v-btn>
-              </div>
-            </div>
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
-  </v-container>
+            {{ authType == authOption.type ? 'SELECTED' : 'SELECT' }}
+          </v-btn>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -123,6 +78,23 @@ export default class AccountLoginOptionPicker extends Mixins(AccountChangeMixin,
 
   private authType = LoginSource.BCSC.toString()
 
+  private authOptions = [
+    {
+      type: LoginSource.BCSC,
+      title: 'BC Services Card',
+      description: `Use your BC Services Card with a mobile app or 
+                    a USB card reader to verify your identity.`,
+      icon: 'mdi-smart-card-outline'
+    },
+    {
+      type: LoginSource.BCEID,
+      title: 'BCeID and 2-factor authentication app',
+      description: `Login with a BCeID combined with a verification code in a mobile app, 
+                    such as Google or Microsoft Authenticator.`,
+      icon: 'mdi-two-factor-authentication'
+    }
+  ]
+
   @Emit('auth-type-selected')
   private selectAuthType (type:string) {
     this.authType = type
@@ -145,97 +117,31 @@ export default class AccountLoginOptionPicker extends Mixins(AccountChangeMixin,
 <style lang="scss" scoped>
 @import '$assets/scss/theme.scss';
 
-.v-application p {
-  margin-bottom: 3rem;
-}
-
-.nv-list {
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-}
-
-.nv-list-item {
-  vertical-align: top;
-
-  .name,
-  .value {
-    display: inline-block;
-    vertical-align: top;
-  }
-
-  .name {
-    min-width: 10rem;
-    font-weight: 700;
-  }
-}
-
-.v-list--dense .v-list-item .v-list-item__title {
-  font-weight: 700;
-}
-
-.form__btns {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  margin-top: 2rem;
-
-  .v-btn {
-    width: 6rem;
-  }
-}
-
-.account-nav-container {
-  height: 100%;
-  border-right: 1px solid #eeeeee;
-}
-
-.header-container {
-  display: flex;
-  flex-direction: row;
-}
-
-// BC Online Account Information
-.bcol-acc__name {
-  font-size: 1.125rem;
-  font-weight: 700;
-}
-
-.bcol-acc__meta {
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-
-  li {
-    position: relative;
-    display: inline-block;
-  }
-
-  li + li {
-    &:before {
-      content: ' | ';
-      display: inline-block;
-      position: relative;
-      top: -2px;
-      left: 2px;
-      width: 2rem;
-      vertical-align: top;
-      text-align: center;
+.account-card {
+  &:hover {
+    border-color: var(--v-primary-base) !important;
+    .account-type__icon {
+      .v-icon {
+        color: var(--v-primary-base) !important;
+      }
     }
   }
-}
-
-.save-btn.disabled {
-  pointer-events: none;
-}
-
-.save-btn__label {
-  padding-left: 0.2rem;
-  padding-right: 0.2rem;
-}
-
-.change-account-link {
-  font-size: 0.875rem;
+  &.active {
+    box-shadow: 0 0 0 2px inset var(--v-primary-base), 0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12) !important;
+    .account-type__icon {
+      .v-icon {
+        color: var(--v-primary-base) !important;
+      }
+    }
+  }
+  .account-type__icon {
+    .v-icon {
+      font-size: 4rem;
+    }
+  }
+  .account-type__title {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
 }
 </style>
