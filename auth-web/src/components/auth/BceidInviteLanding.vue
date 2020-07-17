9db4b6d51d5bee8197fee4d99a78f5bf65b117ef
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-07-16 20:22:47 -0700

    validate token - ui cleanup

--- auth-web/src/components/auth/BceidInviteLanding.vue
+++ auth-web/src/components/auth/BceidInviteLanding.vue
@@ -7,55 +7,58 @@
           <v-progress-circular size="50" width="5" color="primary" :indeterminate="isLoading"/>
         </div>
       </v-fade-transition>
-      <div class="user-profile-container" v-if="!isLoading">
-        <v-container class="view-container">
-          <div class="view-header flex-column mb-10">
-            <h1 class="view-header__title">Log in to BC Registries using BCeID</h1>
-            <p class="mt-5 mb-3">There are a couple of things you'll need to do before logging into BC Registries using BCeID.</p>
-          </div>
-          <v-card
-                  class="step-card my-6"
-                  flat
-                  v-for="step in steps"
-                  :key="step.number"
+      <div class="mt-2" v-if="!isLoading">
+        <div class="view-header flex-column mb-10">
+          <h1 class="view-header__title">Log in to BC Registries using BCeID</h1>
+          <p class="my-3">There are a couple of things you'll need to do before logging into BC Registries using BCeID.</p>
+        </div>
+        <v-card
+          class="step-card my-6"
+          flat
+        >
+          <div
+            v-for="step in steps"
+            :key="step.number"
           >
-            <v-card-text class="pt-4 pb-4 pb-lg-5 px-6 px-lg-8 d-inline-flex align-center">
-              <v-icon x-large color="blue-grey darken-1" class="step-icon mt-1 mr-12 ml-5">
+            <v-card-text
+              class="pt-4 pb-4 pb-lg-5 px-6 px-lg-8 d-inline-flex align-start"
+            >
+              <v-icon class="step-icon mr-8 ml-3">
                 {{step.icon}}
               </v-icon>
               <div>
-                <h2 class="mt-2 mb-4">{{step.number}}.  {{step.stepTitle}}</h2>
+                <h2 class="mt-2 mb-4">{{step.stepTitle}}</h2>
                 <div v-html="step.stepDescription"></div>
               </div>
             </v-card-text>
-          </v-card>
-          <v-divider class="my-9"></v-divider>
-          <div class="d-flex justify-center align-center">
+            <div class="d-flex flex-row mx-9 align-center">
+              <v-divider class=""></v-divider>
+              <v-icon class="divider-icon mx-2 mt-1">mdi-arrow-down</v-icon>
+              <v-divider class=""></v-divider>
+            </div>
+          </div>
+          <div class="d-flex justify-center align-center pt-8 pb-10">
             <v-btn
-                    large
-                    color="primary"
-                    class="next-btn font-weight-bold"
-                    @click="registerForBceid()"
+              min-width="100"
+              color="primary"
+              class="next-btn font-weight-bold"
+              @click="registerForBceid()"
             >
               Register
-              <v-icon class="ml-2">
-                mdi-arrow-right
-              </v-icon>
             </v-btn>
-            OR
+            <div class="mx-4 font-weight-bold">
+              OR
+            </div>
             <v-btn
-                    large
-                    color="primary"
-                    class="next-btn font-weight-bold"
-                    @click="loginWithBceid"
+              min-width="100"
+              color="primary"
+              class="next-btn font-weight-bold"
+              @click="loginWithBceid"
             >
               Login
-              <v-icon class="ml-2">
-                mdi-arrow-right
-              </v-icon>
             </v-btn>
           </div>
-        </v-container>
+        </v-card>
       </div>
     </template>
     <div v-else>
@@ -88,13 +91,13 @@ export default class BceidInviteLanding extends Vue {
       stepTitle: 'Register or use an existing BCeID account',
       stepDescription: '<p>A BCeID account provides secure access to online government services in British Columbia.\n' +
               'You can register a new BCeID or use an existing BCeID account to log into BC Registries.</p>',
-      icon: 'mdi-account-circle-outline'
+      icon: 'mdi-account-plus-outline'
     },
     {
       number: 2,
       stepTitle: 'Use a 2-factor authentication app',
       stepDescription: '<p>Secure your account using a 2-factor authentication app with your BCeID when you log in. Download a 2-factor authentication app to your smartphone such as FreeOTP, Google Authenticator or Microsoft Authenticator.</p>',
-      icon: 'mdi-cellphone-arrow-down'
+      icon: 'mdi-two-factor-authentication'
     }
   ]
 
@@ -123,9 +126,22 @@ export default class BceidInviteLanding extends Vue {
 <style lang="scss" scoped>
    @import '$assets/scss/theme.scss';
 
+  .view-container {
+    max-width: 60rem;
+  }
+
   .v-card__title {
     font-weight: 700;
     letter-spacing: -0.02rem;
+  }
+
+  .step-icon {
+    font-size: 3.6rem !important;
+    color: $BCgovBlue4 !important;
+  }
+
+  .divider-icon {
+    color: $BCgovBlue4 !important;
   }
 
   .user-profile-header {
