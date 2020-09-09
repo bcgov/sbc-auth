f5bbc3eb2bda1d7b7e7d91b4258eaa0ef61c6fcf
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-09-09 08:26:04 -0700

    Wrong test fixed

--- auth-api/tests/unit/api/test_org.py
+++ auth-api/tests/unit/api/test_org.py
@@ -385,7 +385,6 @@ def test_upgrade_org(client, jwt, session, keycloak_mock):  # pylint:disable=unu
                      headers=headers, content_type='application/json')
 
     dictionary = json.loads(rv.data)
-    print('dictionary-----------------------------------------',dictionary)
     assert rv.status_code == http_status.HTTP_201_CREATED
     assert rv.json.get('orgType') == OrgType.BASIC.value
     assert rv.json.get('name') == TestOrgInfo.org1.get('name')
