18419c2929d7dda81b58a032ecce678066fe1f8c
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2020-09-15 14:55:14 -0700

    fixing migration heads

---
+++ auth-api/migrations/versions/66ae9d618842_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 66ae9d618842
+Revises: f91bcb97f2c2, 0bf25945e757
+Create Date: 2020-09-15 14:53:25.808774
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66ae9d618842'
+down_revision = ('f91bcb97f2c2', '0bf25945e757')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
