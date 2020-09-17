03699a4135b70c4b9a0a3268d42216087dfaa041
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-09-17 14:20:32 -0700

    fixing typo

--- auth-web/src/components/auth/staff/StaffActiveAccountsTable.vue
+++ auth-web/src/components/auth/staff/StaffActiveAccountsTable.vue
@@ -112,7 +112,7 @@ export default class StaffActiveAccountsTable extends Mixins(PaginationMixin) {
     {
       text: 'Name',
       align: 'left',
-      fable: true,
+      sortable: true,
       value: 'name'
     },
     {
