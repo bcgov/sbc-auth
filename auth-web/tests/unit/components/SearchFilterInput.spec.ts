d82257c7ad917b17652ed18981dd9e1c3f88d106
Nitheesh T Ganesh <60233274+nitheesh-aot@users.noreply.github.com>
2020-09-24 09:35:17 -0700

    search filter unit tests (#1024)
    * unit tests

---
+++ auth-web/tests/unit/components/SearchFilterInput.spec.ts
@@ -0,0 +1,92 @@
+import { Wrapper, createLocalVue, mount } from '@vue/test-utils'
+import SearchFilterInput from '@/components/auth/common/SearchFilterInput.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+describe('SearchFilterInput.vue', () => {
+  let wrapper: any
+  const config = {
+    'VUE_APP_ROOT_API': 'https://localhost:8080/api/v1/11'
+  }
+
+  const filterParams = [
+    {
+      id: 'username',
+      placeholder: 'User Name',
+      labelKey: 'Name',
+      appliedFilterValue: '',
+      filterInput: ''
+    },
+    {
+      id: 'folio',
+      placeholder: 'Folio Number',
+      labelKey: 'Folio',
+      appliedFilterValue: '',
+      filterInput: ''
+    }
+  ]
+  let filteredRecordsCount = 0
+
+  sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const vuetify = new Vuetify({})
+
+    wrapper = mount(SearchFilterInput, {
+      localVue,
+      vuetify,
+      propsData: {
+        filterParams: filterParams,
+        filteredRecordsCount: filteredRecordsCount
+      },
+      sync: false
+    })
+
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('has search fields and apply button', () => {
+    expect(wrapper.find('.filter-input')).toBeTruthy()
+    expect(wrapper.find('.apply-filter-btn')).toBeTruthy()
+  })
+
+  it('confirm search field count as per the prop given', () => {
+    const filterCount = wrapper.vm.$props.filterParams.length
+    const renderedInputs = wrapper.vm.$el.querySelectorAll('.filter-input')
+    expect(renderedInputs?.length).toEqual(filterCount)
+  })
+
+  it('renders the input fields with correct labels', () => {
+    const renderedInputs = wrapper.vm.$el.querySelectorAll('.filter-input')
+    expect(renderedInputs[0].getElementsByTagName('input')[0].getAttribute('placeholder')).toBe(filterParams[0].placeholder)
+    expect(renderedInputs[1].getElementsByTagName('input')[0].getAttribute('placeholder')).toBe(filterParams[1].placeholder)
+  })
+
+  it('renders the input fields with correct values and apply filter disabled', () => {
+    const renderedInputs = wrapper.vm.$el.querySelectorAll('.filter-input')
+    expect(renderedInputs[0].getElementsByTagName('input')[0].value).toBe('')
+    expect(renderedInputs[1].getElementsByTagName('input')[0].value).toBe('')
+    expect(wrapper.find('.apply-filter-btn').is('[disabled]')).toBe(true)
+  })
+
+  it('input username in the search field', async () => {
+    const userNameSearchField = wrapper.findAll('.filter-input').at(0)
+    userNameSearchField.element.value = 'Jon Snow'
+    userNameSearchField.trigger('input')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.apply-filter-btn').is('[disabled]')).toBe(true)
+  })
+})
