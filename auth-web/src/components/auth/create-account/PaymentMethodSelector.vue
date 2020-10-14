491bce86608c39cd48d63733f015131c0a3a58c3
jeznorth <twenty15creative@gmail.com>
2020-10-14 14:25:00 -0700

    [entity4740] Payment Selector and Stepper Updates (#1070)

--- auth-web/src/components/auth/create-account/PaymentMethodSelector.vue
+++ auth-web/src/components/auth/create-account/PaymentMethodSelector.vue
@@ -1,45 +1,54 @@
 <template>
   <div>
-    <p class="mb-10">
+    <p class="mb-12">
       Select the payment method for this account.
     </p>
     <div>
       <v-card
-        flat
         outlined
         hover
-        class="payment-card py-8 px-8 mb-4"
+        class="payment-card py-8 px-8 mb-4 elevation-1"
         :class="{'selected': isPaymentSelected(payment)}"
         v-for="payment in paymentMethods"
         :key="payment.type"
         @click="selectPayment(payment)"
       >
         <div class="d-flex">
-          <div class="flex--grow">
-            <div class="title font-weight-bold mt-n2 payment-title">{{payment.title}}</div>
-            <div>{{payment.subtitle}}</div>
+          <div class="mt-n2 mr-8">
+            <v-icon x-large color="primary">{{payment.icon}}</v-icon>
           </div>
+
+          <div>
+            <div>
+              <div class="title font-weight-bold mt-n2 payment-title">{{payment.title}}</div>
+              <div>{{payment.subtitle}}</div>
+            </div>
+            <v-expand-transition>
+              <div v-if="isPaymentSelected(payment)">
+                <div class="pt-6">
+                  <v-divider class="mb-6"></v-divider>
+                  <div
+                    v-html="payment.description">
+                  </div>
+                </div>
+              </div>
+            </v-expand-transition>
+          </div>
+
+          <div class="ml-auto pl-8">
           <v-btn
-            large
             depressed
             color="primary"
             width="120"
-            class="font-weight-bold ml-auto"
+            class="font-weight-bold"
             :outlined="!isPaymentSelected(payment)"
             @click="selectPayment(payment)"
+            :aria-label="'Select' + ' ' + payment.title"
           >
-            {{(isPaymentSelected(payment)) ? 'SELECTED' : 'SELECT'}}
+            <span>{{(isPaymentSelected(payment)) ? 'SELECTED' : 'SELECT'}}</span>
           </v-btn>
-        </div>
-        <div>
-          <v-expand-transition>
-            <div v-if="isPaymentSelected(payment)">
-              <v-divider class="transparent-divider mb-4"></v-divider>
-              <div
-                v-html="payment.description">
-              </div>
-            </div>
-          </v-expand-transition>
+          </div>
+
         </div>
       </v-card>
     </div>
@@ -98,6 +107,7 @@ export default class PaymentMethodSelector extends Mixins(Steppable) {
   private paymentMethods = [
     {
       type: PaymentTypes.CREDIT_CARD,
+      icon: 'mdi-credit-card-outline',
       title: 'Credit Card',
       subtitle: 'Pay for transactions individually with your credit card.',
       description: `You don't need to provide any credit card information with your account. Credit card information will be requested when you are ready to complete a transaction.`,
@@ -105,16 +115,16 @@ export default class PaymentMethodSelector extends Mixins(Steppable) {
     },
     {
       type: PaymentTypes.ONLINE_BANKING,
+      icon: 'mdi-bank-outline',
       title: 'Online Banking',
       subtitle: 'Pay for products and services through your financial institutions website.',
       description: `
-          <div class="mb-4">
+          <p>
             Instructions to set up your online banking payment solution will be available in the <strong>Payment Methods</strong> section of your account settings once your account has been created.
-          </div>
-          <div>
-            BC Registries and Online Services <strong>must receive payment in full</strong> from your financial institution prior to the release of items purchased through this service. 
-            Receipt of an online banking payment generally takes 2 days from when you make the payment with your financial institution.
-          <div>`,
+          </p>
+          <p class="mb-0">
+            BC Registries and Online Services <strong>must receive payment in full</strong> from your financial institution prior to the release of items purchased through this service. Receipt of an online banking payment generally takes 3-4 days from when you make the payment with your financial institution.
+          </p>`,
       isSelected: false
     }
   ]
@@ -148,6 +158,9 @@ export default class PaymentMethodSelector extends Mixins(Steppable) {
 
 <style lang="scss" scoped>
 .payment-card {
+  background-color: var(--v-grey-lighten5) !important;
+  transition: all ease-out 0.2s;
+
   &:hover {
     border-color: var(--v-primary-base) !important;
   }
