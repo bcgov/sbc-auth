f35e84da9aaf453654557390535fbd71f488e33c
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-10-21 14:23:11 -0700

    Events listener added (#1073)
    * Events listener added

---
+++ queue_services/events-listener/src/events_listener/config.py
@@ -0,0 +1,142 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""All of the configuration for the service is captured here.
+
+All items are loaded, or have Constants defined here that
+are loaded into the Flask configuration.
+All modules and lookups get their configuration from the
+Flask config, rather than reading environment variables directly
+or by accessing this configuration directly.
+"""
+import os
+import random
+
+from dotenv import find_dotenv, load_dotenv
+
+
+# this will load all the envars from a .env file located in the project root (api)
+load_dotenv(find_dotenv())
+
+CONFIGURATION = {
+    'development': 'events_listener.config.DevConfig',
+    'testing': 'events_listener.config.TestConfig',
+    'production': 'events_listener.config.ProdConfig',
+    'default': 'events_listener.config.ProdConfig'
+}
+
+
+def get_named_config(config_name: str = 'production'):
+    """Return the configuration object based on the name.
+
+    :raise: KeyError: if an unknown configuration is requested
+    """
+    if config_name in ['production', 'staging', 'default']:
+        app_config = ProdConfig()
+    elif config_name == 'testing':
+        app_config = TestConfig()
+    elif config_name == 'development':
+        app_config = DevConfig()
+    else:
+        raise KeyError(f'Unknown configuration: {config_name}')
+    return app_config
+
+
+class _Config():  # pylint: disable=too-few-public-methods
+    """Base class configuration that should set reasonable defaults.
+
+    Used as the base for all the other configurations.
+    """
+
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+    SENTRY_DSN = os.getenv('SENTRY_DSN', None)
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # POSTGRESQL
+    DB_USER = os.getenv('DATABASE_USERNAME', '')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
+    DB_NAME = os.getenv('DATABASE_NAME', '')
+    DB_HOST = os.getenv('DATABASE_HOST', '')
+    DB_PORT = os.getenv('DATABASE_PORT', '5432')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=int(DB_PORT),
+        name=DB_NAME,
+    )
+
+    # Service account details
+    KEYCLOAK_SERVICE_ACCOUNT_ID = os.getenv('KEYCLOAK_SERVICE_ACCOUNT_ID')
+    KEYCLOAK_SERVICE_ACCOUNT_SECRET = os.getenv('KEYCLOAK_SERVICE_ACCOUNT_SECRET')
+
+    # pay-API URL
+    PAY_API_URL = os.getenv('PAY_API_URL')
+
+    NATS_CONNECTION_OPTIONS = {
+        'servers': os.getenv('NATS_SERVERS', 'nats://127.0.0.1:4222').split(','),
+        'name': os.getenv('NATS_CLIENT_NAME', 'entity.events.worker')
+
+    }
+    STAN_CONNECTION_OPTIONS = {
+        'cluster_id': os.getenv('NATS_CLUSTER_ID', 'test-cluster'),
+        'client_id': str(random.SystemRandom().getrandbits(0x58)),
+        'ping_interval': 1,
+        'ping_max_out': 5,
+    }
+
+    SUBSCRIPTION_OPTIONS = {
+        'subject': os.getenv('NATS_SUBJECT', 'account.events'),
+        'queue': os.getenv('NATS_QUEUE', 'account.events.worker'),
+        'durable_name': os.getenv('NATS_QUEUE', 'account-events-worker') + '_durable',
+    }
+
+
+class DevConfig(_Config):  # pylint: disable=too-few-public-methods
+    """Creates the Development Config object."""
+
+    TESTING = False
+    DEBUG = True
+
+
+class TestConfig(_Config):  # pylint: disable=too-few-public-methods
+    """In support of testing only.
+
+    Used by the py.test suite
+    """
+
+    DEBUG = True
+    TESTING = True
+    # POSTGRESQL
+    DB_USER = os.getenv('DATABASE_TEST_USERNAME', '')
+    DB_PASSWORD = os.getenv('DATABASE_TEST_PASSWORD', '')
+    DB_NAME = os.getenv('DATABASE_TEST_NAME', '')
+    DB_HOST = os.getenv('DATABASE_TEST_HOST', '')
+    DB_PORT = os.getenv('DATABASE_TEST_PORT', '5432')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_TEST_URL',
+        default='postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+            user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=int(DB_PORT), name=DB_NAME
+        ),
+    )
+
+    STAN_CLUSTER_NAME = 'test-cluster'
+
+
+class ProdConfig(_Config):  # pylint: disable=too-few-public-methods
+    """Production environment configuration."""
+
+    TESTING = False
+    DEBUG = False
