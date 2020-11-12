dbcc387b084b7e8cde72fa8921ba8960c3dbd854
Andrew Sutherland <sutherlanda@gmail.com>
2020-11-12 12:40:31 -0800

    5301 - Add email template for refunds (#1114)
    * Add email template for refunds
    
    * 5301 fix linting/flake
    
    - Add integration test
    - Fix incorrect dict
    - Cleanup

--- queue_services/account-mailer/tests/integration/utils.py
+++ queue_services/account-mailer/tests/integration/utils.py
@@ -32,3 +32,20 @@ async def helper_add_event_to_queue(stan_client: stan.aio.client.Client,
     }
     await stan_client.publish(subject=subject,
                               payload=json.dumps(payload).encode('utf-8'))
+
+
+async def helper_add_ref_req_to_queue(stan_client: stan.aio.client.Client,
+                                      subject: str,
+                                      invoice_id: str = '1', mail_details: dict = {}):
+    """Add a refund request event to the Queue."""
+    payload = {
+        'specversion': '1.x-wip',
+        'type': 'bc.registry.payment.refundRequest',
+        'source': f'https://api.pay.bcregistry.gov.bc.ca/v1/invoices/{invoice_id}',
+        'id': f'{invoice_id}',
+        'time': '2020-08-28T17:37:34.651294+00:00',
+        'datacontenttype': 'application/json',
+        'data': mail_details
+    }
+    await stan_client.publish(subject=subject,
+                              payload=json.dumps(payload).encode('utf-8'))
