6519a93f6d174d666d37bcc9f660705e7f9eeb95
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-11-24 13:22:07 -0800

    Added org status to the permission table (#1140)
    * Added org status to the permission table
    
    * temporary fix for UI not to break

--- auth-web/src/services/permission.services.ts
+++ auth-web/src/services/permission.services.ts
@@ -3,7 +3,7 @@ import ConfigHelper from '@/util/config-helper'
 import { axios } from '@/util/http-util.ts'
 
 export default class PermissionService {
-  static async getPermissions (role: string): Promise<AxiosResponse<string[]>> {
-    return axios.get(`${ConfigHelper.getAuthAPIUrl()}/permissions/${role}`)
+  static async getPermissions (orgStatus:string, role: string): Promise<AxiosResponse<string[]>> {
+    return axios.get(`${ConfigHelper.getAuthAPIUrl()}/permissions/${orgStatus}/${role}`)
   }
 }
