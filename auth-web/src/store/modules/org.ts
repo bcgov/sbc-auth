7cbfb41bbd0f0fc0ab826e58df3708b0d5f087e0
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-11-24 13:22:07 -0800

    Added org status to the permission table (#1140)
    * Added org status to the permission table
    
    * temporary fix for UI not to break

--- auth-web/src/store/modules/org.ts
+++ auth-web/src/store/modules/org.ts
@@ -249,7 +249,7 @@ export default class OrgModule extends VuexModule {
     if (!kcUserProfile.roles.includes(Role.Staff)) {
       response = await UserService.getMembership(orgId)
       membership = response?.data
-      const res = await PermissionService.getPermissions(membership.membershipTypeCode)
+      const res = await PermissionService.getPermissions('ACTIVE', membership.membershipTypeCode)
       permissions = res?.data
     } else {
       // Check for better approach
