8f895e283f8a9c4534d6f98fa30161848944d1af
jeznorth <twenty15creative@gmail.com>
2020-12-21 11:02:55 -0800

    [entity6010] Develop Static Price List Interface

---
+++ auth-web/src/views/auth/PriceListView.vue
@@ -0,0 +1,218 @@
+<template>
+  <main class="view-container container">
+      <h1 class="mb-4">Price List</h1>
+      <p class="mb-8">Where applicable, tax is included in the price shown. Prices are subject to change without notice.</p>
+      <section class="mb-10">
+        <h2 class="mb-5">Business Registry</h2>
+        <v-card flat>
+          <table aria-label="Price List Table">
+            <thead>
+              <th
+                class="product text-left"
+                scope="col"
+              >
+                Product
+              </th>
+              <th
+                class="fee"
+                scope="col"
+              >
+                Price
+              </th>
+              <th
+                class="fee--service"
+                scope="col"
+              >
+                Service Fee
+              </th>
+              <th
+                class="fee--priority"
+                scope="col"
+              >
+                Priority Fee
+              </th>
+              <th
+                class="fee--future"
+                scope="col"
+              >
+                Future Effective Fee
+              </th>
+            </thead>
+            <tbody>
+              <tr v-for="item in pricelist" :key="item.name">
+                <td
+                  class="product font-weight-bold"
+                >
+                  {{item.name}}
+                </td>
+                <td
+                  class="fee">
+                  <span aria-describedby="Annual Report PRice">{{'$' + item.price}}</span>
+                </td>
+                <td
+                  class="fee--service"
+                >
+                  {{'$' + item.serviceFee}}
+                </td>
+                <td
+                  class="fee--priority"
+                >
+                  <span v-if="item.priorityFee">{{'$' + item.priorityFee}}</span>
+                </td>
+                <td
+                  class="fee--future"
+                >
+                  <span v-if="item.futureEffectiveFee">{{'$' + item.futureEffectiveFee}}</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </v-card>
+      </section>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'PriceListView'
+})
+
+export default class PriceListView extends Vue {
+  private readonly pricelist = [
+    {
+      name: 'Annual Report',
+      price: '43.39',
+      serviceFee: '1.50',
+      priorityFee: '100.00',
+      futureEffectiveFee: '100.00'
+    },
+    {
+      name: 'Correction',
+      price: '20.00',
+      serviceFee: '1.50',
+      priorityFee: '100.00',
+      futureEffectiveFee: ''
+    },
+    {
+      name: 'Change of Address',
+      price: '20.00',
+      serviceFee: '1.50',
+      priorityFee: '100.00',
+      futureEffectiveFee: ''
+    },
+    {
+      name: 'Change of Directors',
+      price: '20.00',
+      serviceFee: '1.50',
+      priorityFee: '100.00',
+      futureEffectiveFee: ''
+    },
+    {
+      name: 'Incorporation',
+      price: '350.00',
+      serviceFee: '1.50',
+      priorityFee: '100.00',
+      futureEffectiveFee: '100.00'
+    }
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+  main {
+    margin: 0 auto;
+  }
+
+  table {
+    width: 100%;
+    background: #ffffff;
+    border-top: 0;
+    border-bottom: 0;
+    border-collapse: collapse;
+    table-layout: fixed;
+  }
+
+  th, td {
+    padding: 1rem;
+    vertical-align: top;
+    border-top: 1px solid var(--v-grey-lighten1);
+    border: 1px solid var(--v-grey-lighten1);
+  }
+
+  th {
+    font-weight: normal;
+    text-align: left;
+    white-space: nowrap;
+  }
+
+  td {
+    padding: 1rem;
+    vertical-align: top;
+  }
+
+  td.fee {
+    font-weight: 700;
+  }
+
+  th[class^='fee'] {
+    width: 200px;
+  }
+
+  th[class^='fee'],
+  td[class^='fee'] {
+    text-align: right;
+  }
+
+  .product {
+    white-space: nowrap;
+    overflow: hidden;
+    background: #f7f8fa;
+  }
+
+  @media (max-width: 1024px) {
+    thead {
+      display: none;
+    }
+
+    table {
+      border-collapse: collapse;
+      border-width: 1px;
+    }
+
+    td {
+      position: relative;
+      margin-top: -1px;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      border-width: 1px;
+    }
+
+    td.product {
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+    }
+
+    td[class^='fee']::before {
+      flex: 0 0 auto;
+    }
+
+    .fee::before {
+      content: 'Price';
+    }
+
+    .fee--service::before {
+      content: 'Service Fee';
+    }
+
+    .fee--priority::before {
+      content: 'Priority';
+    }
+
+    .fee--future::before {
+      content: 'Future Effective Date';
+    }
+  }
+</style>
