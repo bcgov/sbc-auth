711624aac4b3ea02e8c2db7acdf38e2db47ebb48
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2020-12-29 10:51:49 -0800

    User in-app notification of suspended account

--- auth-web/src/views/auth/account-freeze/AccountFreezeView.vue
+++ auth-web/src/views/auth/account-freeze/AccountFreezeView.vue
@@ -2,9 +2,10 @@
   <v-container class="view-container">
     <v-row justify="center">
       <v-col cols="12" sm="6" class="text-center">
-        <v-icon size="40" color="error" class="mb-6">mdi-lock-outline</v-icon>
-        <h1>This account has been temporarily suspended</h1>
-        <p class="mt-8 mb-10">Please contact the account administrator to reactivate this account.</p>
+        <v-icon size="40" color="error" class="mb-6">mdi-alert-circle-outline</v-icon>
+        <h1>Your Account is Temporarily Suspended</h1>
+        <p class="mt-8 mb-10">Your account is temporarily suspended from <b>{{suspendedDate}}</b>. <br />
+        Please contact the account administrator to reactivate your account</p>
       </v-col>
     </v-row>
   </v-container>
@@ -14,6 +15,7 @@
 
 import { Component, Mixins } from 'vue-property-decorator'
 import AccountMixin from '@/components/auth/mixins/AccountMixin.vue'
+import CommonUtils from '@/util/common-util'
 import { Organization } from '@/models/Organization'
 import { Pages } from '@/util/constants'
 import Vue from 'vue'
@@ -26,5 +28,10 @@ import { mapState } from 'vuex'
 })
 export default class AccountCreationSuccessView extends Mixins(AccountMixin) {
   protected readonly currentOrganization!: Organization
+  private formatDate = CommonUtils.formatDisplayDate
+
+  private get suspendedDate () {
+    return (this.currentOrganization?.suspendedOn) ? this.formatDate(new Date(this.currentOrganization.suspendedOn)) : ''
+  }
 }
 </script>
