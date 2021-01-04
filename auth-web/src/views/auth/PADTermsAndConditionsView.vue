3033f0d1e0e1666f674ced21ebc4ae2da313087b
mengdong19 <meng.dong@gov.bc.ca>
2021-01-04 10:48:04 -0800

    5721-Update User Terms. (#1245)
    * 5271-Create migration file.
    
    * 5721-Create migration file.
    
    * 5721-update unit tests.
    
    * [5721-Update-user-terms] Update Terms and Conditions Content
    
    * [5721-Update-user-terms] Standardizing Terms and Conditions Formats
    
    * [5721-Update-user-terms] Standardizing Terms and Conditions Formats
    
    * Update background color.
    
    * [5721-Update-user-terms] Adding API Terms Link
    
    * Update a few links.
    
    * remove h2
    
    * Format mailing info.
    
    * roll back pad and tos version 3 migration files, add changes to new migration file.
    
    * fix links
    
    * Remove link to API terms as it's not avaliable for now.
    
    Co-authored-by: jeznorth <twenty15creative@gmail.com>

---
+++ auth-web/src/views/auth/PADTermsAndConditionsView.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="terms-of-use-container view-container">
+    <h1 class="mb-10">Business Pre-Authorized Debit Terms and Conditions Agreement BC Registries and Online Services</h1>
+    <v-card flat>
+      <v-card-text class="pa-8">
+        <TermsOfUse
+          :tosType="'termsofuse_pad'"
+        ></TermsOfUse>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import TermsOfUse from '@/components/auth/common/TermsOfUse.vue'
+import Vue from 'vue'
+
+@Component({
+  components: {
+    TermsOfUse
+  }
+})
+
+export default class PADTermsAndConditionsView extends Vue {
+}
+
+</script>
+
+<style lang="scss" scoped>
+  .terms-of-use-container {
+    max-width: 65rem;
+  }
+
+  // TODO: Payor Authoriztion section of the PAD Terms does not apply in this content so we don't display it
+  // TODO: Better strategy to show/hide this content VS hacking it out with styling
+  ::v-deep {
+    section:last-child {
+      display: none;
+    }
+  }
+
+</style>
