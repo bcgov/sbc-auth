98aec102cafa66d6bb81c3604a5cea45a4e7d9bd
faiz shabeeb <55506249+shabeeb-aot@users.noreply.github.com>
2021-01-19 08:00:46 -0800

    Frozen account appears in Suspended tab in Staff Account Dashboard#4856 (#1304)

---
+++ auth-web/tests/unit/components/StaffSuspendedAccountsTable.spec.ts
@@ -0,0 +1,81 @@
+import { Wrapper, createLocalVue, shallowMount } from '@vue/test-utils'
+import StaffSuspendedAccountsTable from '@/components/auth/staff/account-management/StaffSuspendedAccountsTable.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+describe('StaffSuspendedAccountsTable.vue', () => {
+  let wrapper: Wrapper<StaffSuspendedAccountsTable>
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const staffModule = {
+      namespaced: true,
+      state: {
+        suspendedStaffOrgs: [
+          {
+            'modified': '2020-12-10T21:05:06.144977+00:00',
+            'name': 'NEW BC ONLINE TECH TEAM',
+            'orgType': 'PREMIUM',
+            'orgStatus': 'NSF_SUSPENDED',
+            'products': [
+              2342,
+              2991
+            ],
+            'statusCode': 'NSF_SUSPENDED',
+            'suspendedOn': '2020-12-01T17:52:03.747200+00:00'
+          }
+        ]
+      },
+      actions: {
+        syncSuspendedStaffOrgs: jest.fn()
+      }
+    }
+
+    const orgModule = {
+      namespaced: true,
+      state: {},
+      actions: {
+        syncOrganization: jest.fn(),
+        syncMembership: jest.fn(),
+        addOrgSettings: jest.fn()
+      }
+    }
+
+    const vuetify = new Vuetify({})
+
+    const store = new Vuex.Store({
+      state: {},
+      strict: false,
+      modules: {
+        staff: staffModule,
+        org: orgModule
+      }
+    })
+
+    const $t = () => ''
+    wrapper = shallowMount(StaffSuspendedAccountsTable, {
+      store,
+      localVue,
+      vuetify,
+      mocks: { $t }
+    })
+
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('Should have data table', () => {
+    expect(wrapper.find('.account-list')).toBeTruthy()
+  })
+})
