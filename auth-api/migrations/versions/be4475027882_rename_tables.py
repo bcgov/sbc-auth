c52b4223591d444cb49284663410e9d613aee7f8
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-01-19 21:47:32 -0800

    rename tables added

--- auth-api/migrations/versions/be4475027882_rename_tables.py
+++ auth-api/migrations/versions/be4475027882_rename_tables.py
@@ -14,14 +14,21 @@ import re
 # revision identifiers, used by Alembic.
 from sqlalchemy.engine.reflection import Inspector
 
+VERSION = '_version'
+
 revision = 'be4475027882'
 down_revision = 'a490b2db8b13'
 branch_labels = None
 depends_on = None
 
-
 ###
 # get_pk_constraint
+
+table_mapping = {'org': 'orgs', 'corp_type': 'corp_types', 'product_code': 'product_codes', 'affidavit': 'affidavits',
+                 'affidavit_status': 'affidavit_statuses', 'affiliation': 'affiliations', 'contact': 'contacts',
+                 'user': 'users'}
+
+skip_table = ['alembic', 'activity', 'transaction']
 
 
 def upgrade():
@@ -33,37 +40,104 @@ def upgrade():
         4. foriegn key name
 
     """
+
     conn = op.get_bind()
     inspector = Inspector.from_engine(conn)
     tables = inspector.get_table_names()
-    name_dict = {'org': 'orgs', 'corp_type': 'corp_types', 'product_code': 'product_codes'}
-    m = MetaData(conn, reflect=True)
-    for table in tables:
-        fks = inspector.get_foreign_keys(table)
-        for fk in fks:
-            fk_name = fk.get('name')
-            referred_table = fk.get('referred_table')
-            if new_table_name := name_dict.get(referred_table, None):
-                new_fk_name = fk_name.replace(referred_table, new_table_name)
-                op.execute(f'ALTER table {table} RENAME CONSTRAINT {fk_name} TO {new_fk_name}')
+    metadata = MetaData(conn, reflect=True)
 
-        if new_table_name := name_dict.get(table, None):
-            for index in inspector.get_indexes(table):
-                old_index_name = index.get('name')
-                new_index_name = old_index_name.replace(table, new_table_name, 1)
-                op.execute(f'ALTER index {old_index_name} RENAME TO {new_index_name}')
-            pk_name = inspector.get_pk_constraint(table).get('name')
-            new_pk_name = pk_name.replace(table, new_table_name, 1)
-            op.execute(f'ALTER index {pk_name} RENAME TO {new_pk_name}')
-            id_column = m.tables[table].columns.get('id')
-            if id_column is not None:
-                seq = id_column.server_default.arg.text
-                # format is 'nextval(\'org_id_seq\'::regclass)'
-                seq_name = re.search("nextval\(\'(.*)\'::regclass", seq).group(1)
-                new_seq_name = seq_name.replace(table, new_table_name, 1)
-                op.execute(f'ALTER sequence {seq_name} RENAME TO {new_seq_name}')
-            op.rename_table(table, new_table_name)
+    table: str
+    for table in tables:
+        if table in skip_table or table.endswith(VERSION):
+            continue
+        print('<<<<Processing starting for table', table)
+        _rename_obj(inspector, metadata, table, table_mapping, tables)
+        print('Processing ended for table>>>', table)
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    metadata = MetaData(conn, reflect=True)
+    table_mapping_reversed = {y: x for x, y in table_mapping.items()}
+    table: str
+    for table in tables:
+        if table in skip_table or table.endswith(VERSION):
+            continue
+        print('<<<<Processing starting for table', table)
+        _rename_obj(inspector, metadata, table, table_mapping_reversed, tables)
+        print('Processing ended for table>>>', table)
+
+
+def _rename_obj(inspector, metadata, table: str, name_dict, tables):
+    _rename_fks(inspector, table, table_mapping)
+    new_table_name: str = name_dict.get(table, '')
+    if new_table_name:
+        print(f'New name found for Table : {table} to {new_table_name}')
+        _rename_indexes(inspector, new_table_name, table)
+        _rename_pk(inspector, new_table_name, table)
+        _rename_sequence(metadata, new_table_name, table)
+        _rename_table(table, new_table_name)
+        versioned_table_name = _suffix_version(table)
+        if versioned_table_name in tables:
+            versioned_table_new_name = _suffix_version(new_table_name)
+            _rename_table(versioned_table_name, versioned_table_new_name)
+
+        print(f'Renaming Table Done for: {table} to {new_table_name}')
+
+
+def _suffix_version(table):
+    return table + VERSION
+
+
+def _rename_table(table, new_table_name):
+    print(f'\t<<<<<<<<Renaming Table : {table} to {new_table_name} ')
+    op.rename_table(table, new_table_name)
+    print(f'\tRenaming Done for Table : {table} to {new_table_name} >>>>>>>>>')
+
+
+def _rename_sequence(m, new_table_name, table):
+    id_column = m.tables[table].columns.get('id')
+    if id_column is not None:
+        seq = id_column.server_default.arg.text
+        # format is 'nextval(\'org_id_seq\'::regclass)'
+        seq_name = re.search("nextval\(\'(.*)\'::regclass", seq).group(1)
+        new_seq_name = seq_name.replace(table, new_table_name, 1)
+        if seq_name != new_seq_name:
+            print(f'\t<<<<<<<<<Renaming PK : {seq_name} to {new_seq_name} of table {table}')
+            op.execute(f'ALTER sequence {seq_name} RENAME TO {new_seq_name}')
+            print(f'\tRenaming PK : {seq_name} to {new_seq_name} of table {table}>>>>>>>>>>>')
+
+
+def _rename_pk(inspector, new_table_name, table):
+    pk_name = inspector.get_pk_constraint(table).get('name')
+    new_pk_name = pk_name.replace(table, new_table_name, 1)
+    if pk_name != new_pk_name:
+        print(f'\t<<<<<<<<<<Renaming PK : {pk_name} to {new_pk_name} of table {table}')
+        op.execute(f'ALTER index {pk_name} RENAME TO {new_pk_name}')
+        print(f'\tRenaming Done for PK : {pk_name} to {new_pk_name} of table {table}>>>>>>>>>')
+
+
+def _rename_indexes(inspector, new_table_name, table):
+    for index in inspector.get_indexes(table):
+        old_index_name = index.get('name')
+        new_index_name = old_index_name.replace(table, new_table_name, 1)
+        if old_index_name != new_index_name:
+            print(f'\t<<<<<<Renaming Index : {old_index_name} to {new_index_name} of table {table}')
+            op.execute(f'ALTER index {old_index_name} RENAME TO {new_index_name}')
+            print(f'\tRenaming Index Done for: {old_index_name} to {new_index_name} of table {table}>>>>>>>>')
+
+
+def _rename_fks(inspector, table: str, name_dict):
+    foreign_keys = inspector.get_foreign_keys(table)
+    for fk in foreign_keys:
+        fk_name = fk.get('name')
+        referred_table = fk.get('referred_table')
+        referred_table_new_name = name_dict.get(referred_table, '')
+        if referred_table_new_name:
+            new_fk_name = fk_name.replace(referred_table, referred_table_new_name)
+            if fk_name != new_fk_name:
+                print(f'\t<<<<<<<<<<Renaming Foriegn Key : {fk_name} to {new_fk_name} of table {table}')
+                op.execute(f'ALTER table "{table}" RENAME CONSTRAINT {fk_name} TO {new_fk_name}')
+                print(f'\tRenaming done for Foriegn Key : {fk_name} to {new_fk_name} of table {table}>>>>>>>>>>')
