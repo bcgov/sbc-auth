ef83d48dc99c8e092d335cf62753f0bfbfd6d392
jeznorth <twenty15creative@gmail.com>
2021-01-25 11:33:34 -0800

    [entity4658] Front-end Clean-up

--- auth-web/src/components/auth/common/AccountSuspendAlert.vue
+++ auth-web/src/components/auth/common/AccountSuspendAlert.vue
@@ -1,23 +1,19 @@
 <template>
-<v-card flat class="" color="error">
-  <v-alert :icon="false" prominent type="error" class="mb-0">
-    <v-row>
-      <v-col cols="9">
-        <div class="banner-info">
-          <v-icon color="link" left x-large>mdi-information-outline</v-icon>
-          <div class="ml-4">
-            <div class="font-weight-bold">Account Suspended</div>
-            <div>Account has been suspended for outstanding balance (NSF).</div>
-          </div>
+  <v-alert class="px-8 py-7" :icon="false" prominent type="error">
+    <div class="account-alert">
+      <div class="account-alert-inner">
+        <v-icon large class="mt-2">mdi-information-outline</v-icon>
+        <div class="account-alert__info ml-7">
+          <div class="font-weight-bold">Account Suspended</div>
+          <div>Account has been suspended for outstanding balance (NSF).</div>
+          <div class="mt-6 title font-weight-bold">BALANCE DUE:  ${{totalAmountToPay.toFixed(2)}}</div>
         </div>
-      </v-col>
-      <v-col class="text-end">{{suspendedDate}}</v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="9" class="font-weight-bold balance">BALANCE DUE:  ${{totalAmountToPay.toFixed(2)}}</v-col>
-    </v-row>
+        <div class="account-alert__date">
+          {{suspendedDate}}
+        </div>
+      </div>
+    </div>
   </v-alert>
-</v-card>
 </template>
 
 <script lang="ts">
@@ -52,17 +48,18 @@ export default class AccountSuspendAlert extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.banner{
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+  .account-alert-inner {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
 
-}
- .banner-info{
-    display: flex
+  .account-alert__info {
+    flex: 1 1 auto;
   }
-.balance{
-    margin-left: 63px;
-    font-size: 1.25rem;
+
+  .account-alert__date {
+    flex: 0 0 auto;
   }
+
 </style>
