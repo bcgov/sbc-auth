a8eeb476c789781bb6c798a986114057bbbf0a07
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-01-27 13:31:59 -0800

    refactor PaymentCard

---
+++ auth-web/src/components/pay/PayWithOnlineBanking.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-card elevation="0">
+    <v-card-text class="heading-info py-7 px-8">
+      <h2 class="mb-2">Balance Due: <span class="ml-2">${{totalBalanceDue.toFixed(2)}}</span></h2>
+      <template>
+        <p class="mb-6">
+          Transaction will be completed when payment is received in full.
+          Online Banking payment methods can expect between <strong>2-5 days</strong> for your payment.
+        </p>
+        <div class="mb-1">
+          <span class="payee-name">
+            <strong>Payee Name:</strong>
+            {{payeeName}}
+          </span>
+          <span>
+            <strong>Account #:</strong>
+            {{cfsAccountId}}
+          </span>
+        </div>
+      </template>
+    </v-card-text>
+    <v-card-text class="pt-7 pb-0 px-8">
+      <template>
+        <h3 class="mb-3">How to pay with online banking:</h3>
+        <ol class="mb-5">
+          <li>Sign in to your financial institution's online banking website or app</li>
+          <li>Go to your financial institution's bill payment page</li>
+          <li>Enter "BC Registries and Online Services" as payee</li>
+          <li>Enter BC Registries and Online Services account number</li>
+          <li>Submit your payment for the balance due</li>
+        </ol>
+      </template>
+      <v-divider class="my-6"></v-divider>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+
+import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator'
+
+@Component({})
+export default class PayWithOnlineBanking extends Vue {
+  @Prop() onlineBankingData: any
+  @Prop({ default: false }) showPayWithOnlyCC: boolean
+  private payWithCreditCard: boolean = false
+  private totalBalanceDue = 0
+  private cfsAccountId: string = ''
+  private payeeName: string = ''
+
+  @Watch('onlineBankingData', { deep: true })
+  async updateonlineBankingData (val, oldVal) {
+    this.setData()
+  }
+
+  private setData () {
+    this.totalBalanceDue = this.onlineBankingData?.totalBalanceDue || 0
+    this.payeeName = this.onlineBankingData.payeeName
+    this.cfsAccountId = this.onlineBankingData?.cfsAccountId || ''
+  }
+
+  private mounted () {
+    this.setData()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '$assets/scss/theme.scss';
+
+.bcol-payment-card {
+  .heading-info {
+    background: var(--v-primary-base);
+    color: #fff !important;
+    h2 {
+      color: #fff !important;
+    }
+    .payee-name {
+      padding-right: 16px;
+      margin-right: 16px;
+      border-right: 1px solid #fff;
+    }
+  }
+  ol {
+    li {
+      margin-bottom: 4px;
+    }
+  }
+  .pay-with-credit-card {
+    ::v-deep {
+      .v-input__slot {
+        align-items: flex-start;
+      }
+      .v-label {
+        display: block;
+        color: #000;
+        .subtxt {
+          font-size: .875rem;
+          margin-top: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
