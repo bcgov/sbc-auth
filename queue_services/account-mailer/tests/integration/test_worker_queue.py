a6298722323c1bc79193beff66ace40af719c6e7
faiz shabeeb <55506249+shabeeb-aot@users.noreply.github.com>
2021-01-29 13:50:13 -0800

    Feature/6043 (#1329)
    * Email Reminder of outstanding invoice for online banking #6043

--- queue_services/account-mailer/tests/integration/test_worker_queue.py
+++ queue_services/account-mailer/tests/integration/test_worker_queue.py
@@ -423,3 +423,48 @@ async def test_pad_failed_emails(app, session, stan_server, event_loop, client_i
         await helper_add_event_to_queue(events_stan, events_subject, org_id=id,
                                         msg_type=MessageType.PAD_SETUP_FAILED.value,
                                         mail_details=mail_details)
+
+
+@pytest.mark.asyncio
+async def test_payment_pending_emails(app, session, stan_server, event_loop, client_id, events_stan, future):
+    """Assert that events can be retrieved and decoded from the Queue."""
+    # Call back for the subscription
+    from account_mailer.worker import cb_subscription_handler
+
+    # vars
+    user = factory_user_model_with_contact()
+    org = factory_org_model()
+    factory_membership_model(user.id, org.id)
+    id = org.id
+
+    events_subject = 'test_subject'
+    events_queue = 'test_queue'
+    events_durable_name = 'test_durable'
+    with patch.object(notification_service, 'send_email', return_value=None) as mock_send:
+        # register the handler to test it
+        await subscribe_to_queue(events_stan,
+                                 events_subject,
+                                 events_queue,
+                                 events_durable_name,
+                                 cb_subscription_handler)
+
+        # add an event to queue
+        mail_details = {
+            'accountId': id,
+            'cfsAccountId': '12345678',
+            'transactionAmount': 20.00
+        }
+        await helper_add_event_to_queue(events_stan, events_subject, org_id=id,
+                                        msg_type=MessageType.PAYMENT_PENDING.value,
+                                        mail_details=mail_details)
+
+        mock_send.assert_called
+        assert mock_send.call_args.args[0].get('recipients') == 'foo@bar.com'
+        assert mock_send.call_args.args[0].get('content').get(
+            'subject') == SubjectType.PAYMENT_PENDING.value
+        assert mock_send.call_args.args[0].get('attachments') is None
+        assert mock_send.call_args.args[0].get('content').get('body') is not None
+
+        await helper_add_event_to_queue(events_stan, events_subject, org_id=id,
+                                        msg_type=MessageType.PAYMENT_PENDING.value,
+                                        mail_details=mail_details)
