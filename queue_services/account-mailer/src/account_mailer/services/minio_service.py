6240c66438d5c6ebe92bd180db666594d5a56c50
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2021-02-26 07:26:23 -0800

    Changes for sending email for JV failures (#1367)

--- queue_services/account-mailer/src/account_mailer/services/minio_service.py
+++ queue_services/account-mailer/src/account_mailer/services/minio_service.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 """This module is a wrapper for Minio."""
 
+import io
+import os
+
 from flask import current_app
 from minio import Minio
 
@@ -29,11 +32,24 @@ class MinioService:
         return minio_client.get_object(bucket_name, file_name)
 
     @staticmethod
+    def put_minio_file(bucket_name: str, file_name: str, value_as_bytes: bytearray):
+        """Return the file from Minio."""
+        minio_client: Minio = MinioService._get_client()
+        current_app.logger.debug(f'Put Minio file {bucket_name}/{file_name}')
+
+        value_as_stream = io.BytesIO(value_as_bytes)
+        minio_client.put_object(current_app.config['MINIO_BUCKET_NAME'], file_name, value_as_stream,
+                                os.stat(file_name).st_size)
+
+        return minio_client.get_object(bucket_name, file_name)
+
+    @staticmethod
     def _get_client() -> Minio:
         """Return a minio client."""
         minio_endpoint = current_app.config['MINIO_ENDPOINT']
         minio_key = current_app.config['MINIO_ACCESS_KEY']
         minio_secret = current_app.config['MINIO_ACCESS_SECRET']
         minio_secure = current_app.config['MINIO_SECURE']
+
         return Minio(minio_endpoint, access_key=minio_key, secret_key=minio_secret,
                      secure=minio_secure)
