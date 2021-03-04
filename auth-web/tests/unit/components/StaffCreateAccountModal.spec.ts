b25a6c636e47b2c974e1f833b75c050fa331f3db
faiz shabeeb <55506249+shabeeb-aot@users.noreply.github.com>
2021-03-04 14:50:58 -0800

    Feature/6598 4 (#1403)

---
+++ auth-web/tests/unit/components/StaffCreateAccountModal.spec.ts
@@ -0,0 +1,42 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import StaffCreateAccountModal from '@/components/auth/staff/account-management/StaffCreateAccountModal.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+const vuetify = new Vuetify({})
+const router = new VueRouter()
+
+// Prevent the warning "[Vuetify] Unable to locate target [data-app]"
+document.body.setAttribute('data-app', 'true')
+
+describe('StaffCreateAccountModal.vue', () => {
+  let wrapper: any
+  let store: any
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  afterEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('Should have a Modal', () => {
+    wrapper = mount(StaffCreateAccountModal, {
+      store,
+      vuetify,
+      localVue,
+      router,
+
+      mocks: {
+        $t: (mock) => mock
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.find('[data-test="create-account-modal"]')).toBeTruthy()
+  })
+})
