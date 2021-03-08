0fa608e634be7c33040ced7ebab0309a1161e74d
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-03-08 07:17:38 -0800

    fixing index

--- auth-api/migrations/versions/69a7e464fef3_product_role_changes.py
+++ auth-api/migrations/versions/69a7e464fef3_product_role_changes.py
@@ -1,9 +1,7 @@
 """product_role_changes
-
 Revision ID: 69a7e464fef3
 Revises: 5053985bdfc6
 Create Date: 2021-03-02 12:16:09.152924
-
 """
 import sqlalchemy as sa
 from alembic import op
@@ -63,22 +61,16 @@ def upgrade():
     op.execute(f'DROP VIEW IF EXISTS {authorizations_view.name}')
     op.execute(f'CREATE VIEW {authorizations_view.name} AS {authorizations_view.sql}')
 
-    op.create_table('product_subscription_roles',
-                    sa.Column('created', postgresql.TIMESTAMP(), autoincrement=False, nullable=True),
-                    sa.Column('modified', postgresql.TIMESTAMP(), autoincrement=False, nullable=True),
-                    sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-                    sa.Column('product_subscription_id', sa.INTEGER(), autoincrement=False, nullable=False),
-                    sa.Column('product_role_id', sa.INTEGER(), autoincrement=False, nullable=False),
-                    sa.Column('created_by_id', sa.INTEGER(), autoincrement=False, nullable=True),
-                    sa.Column('modified_by_id', sa.INTEGER(), autoincrement=False, nullable=True),
-                    sa.ForeignKeyConstraint(['created_by_id'], ['users.id'], name='pproduct_role_created_by_id_fkey'),
-                    sa.ForeignKeyConstraint(['modified_by_id'], ['users.id'], name='pproduct_role_modified_by_id_fkey'),
-                    sa.ForeignKeyConstraint(['product_role_id'], ['product_role_codes.id'],
-                                            name='pproduct_role_product_role_id_fkey'),
-                    sa.ForeignKeyConstraint(['product_subscription_id'], ['product_subscriptions.id'],
-                                            name='pproduct_role_product_subscription_id_fkey'),
-                    sa.PrimaryKeyConstraint('id', name='product_role_pkey')
-                    )
+    op.execute('delete from product_subscription_roles')
+    op.execute('delete from product_role_codes')
+    op.drop_table('product_subscription_roles')
+    op.drop_table('product_subscription_roles_version')
+
+    op.drop_table('product_role_codes')
+
+    # Create PPR product for PREMIUM accounts.
+    # Delete PPR subscription from all accounts.
+    op.execute("delete from product_subscriptions where product_code='PPR'")
 
     conn = op.get_bind()
     res = conn.execute("select id from orgs where type_code='PREMIUM' ")
