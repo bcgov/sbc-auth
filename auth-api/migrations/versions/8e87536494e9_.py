43db712570be0f332dc91950898e494c50047dbe
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2021-04-01 09:47:09 -0700

    Merge head

---
+++ auth-api/migrations/versions/8e87536494e9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8e87536494e9
+Revises: 379f1d6b24d4, 129eb4978d07
+Create Date: 2021-04-01 09:46:58.354906
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8e87536494e9'
+down_revision = ('379f1d6b24d4', '129eb4978d07')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
