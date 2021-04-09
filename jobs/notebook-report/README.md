7f3ce4c478feb90707aa8caa74b4fd222a6dd81f
Chen <Steven.Chen@gov.bc.ca>
2021-04-09 00:26:48 -0700

    Automated Query Request #6972
    Signed-off-by: Chen <Steven.Chen@gov.bc.ca>

---
+++ jobs/notebook-report/README.md
@@ -0,0 +1,78 @@
+# Notebook Report
+
+Generate notebook report
+
+## Development Environment
+
+Follow the instructions of the [Development Readme](https://github.com/bcgov/entity/blob/master/docs/development.md)
+to setup your local development environment.
+
+## Development Setup
+
+1. Follow the [instructions](https://github.com/bcgov/entity/blob/master/docs/setup-forking-workflow.md) to checkout the project from GitHub.
+2. Open the notebook-report directory in VS Code to treat it as a project (or WSL projec). To prevent version clashes, set up a virtual environment to install the Python packages used by this project.
+3. Run `make setup` to set up the virtual environment and install libraries.
+
+## Running Notebook Report
+
+1. Run `. venv/bin/activate` to change to `venv` environment.
+2. Run notebook with `(python notebookreport.py)`
+
+## Running Unit Tests
+
+1. Run `python -m pytest` or `pytest` command.
+
+### Important: Please remember to do "git update-index --add --chmod=+x run.sh" before run.sh is commit to github on first time. 
+### Build API - can be done in VS Code
+
+1. Login to openshift
+
+   ```sh
+   oc login xxxxxxx
+   ```
+
+2. switch to tools namespace
+
+   ```sh
+   oc project 6e0e49-tools
+   ```
+
+3. Create build image
+
+   ```sh
+   oc process -f openshift/templates/bc.yaml \
+	  -p GIT_REPO_URL=https://github.com/bcgov/sbc-auth.git \
+	  -p GIT_REF=development
+	  -o yaml \
+   | oc apply -f - -n 6e0e49-tools     
+   ```
+
+4. Checking log for building process at Console => Administrator => Builds => Builds => click image 'notebook-report' => logs
+
+5. Tag image to dev: 'oc tag notebook-report:latest notebook-report:dev'
+
+
+### Create cron
+
+1. Login to openshift
+
+   ```sh
+   oc login xxxxxxx
+   ```
+
+2. switch to dev namespace
+
+   ```sh
+   oc project 6e0e49-dev
+   ```
+
+3. Create cron
+   ### please remember that SCHEDULE is UTC which is 7 hour ahead of PST
+   ```sh
+   oc process -f openshift/templates/cronjob.yaml \
+     -p TAG=dev \
+     -p SCHEDULE="30 14 * * *" \
+     -o yaml \
+     | oc apply -f - -n 6e0e49-dev
+   ```
+4. Create a job to run and test it: 'oc create job notebook-report-dev-1 --from=cronjob/notebook-report-dev -n 6e0e49-dev'
---
+++ jobs/notebook-report/notebookreport.py
@@ -0,0 +1,164 @@
+"""The Notebook Report - This module is the API for the Auth Notebook Report."""
+
+import ast
+import fnmatch
+import logging
+import os
+import smtplib
+import sys
+import time
+import traceback
+from datetime import datetime, timedelta, date
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import papermill as pm
+from dateutil.relativedelta import relativedelta
+from flask import Flask, current_app
+
+from config import Config
+from util.logging import setup_logging
+
+setup_logging(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logging.conf'))  # important to do this first
+
+# Notebook Scheduler
+# ---------------------------------------
+# This script helps with the automated processing of Jupyter Notebooks via
+# papermill (https://github.com/nteract/papermill/)
+
+
+def create_app(config=Config):
+    """Create app."""
+    app = Flask(__name__)
+    app.config.from_object(config)
+    # db.init_app(app)
+    app.app_context().push()
+    current_app.logger.debug('created the Flask App and pushed the App Context')
+
+    return app
+
+
+def findfiles(directory, pattern):
+    """Find files matched."""
+    for filename in os.listdir(directory):
+        if fnmatch.fnmatch(filename.lower(), pattern):
+            yield os.path.join(directory, filename)
+
+
+def send_email(note_book, emailtype, errormessage):
+    """Send email for results."""
+    message = MIMEMultipart()
+    date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')    
+    ext = ''
+    if not os.getenv('ENVIRONMENT', '') == 'prod':
+        ext = ' on ' + os.getenv('ENVIRONMENT', '')
+
+    if emailtype == 'ERROR':
+        subject = "Jupyter Notebook Error Notification from sbc-auth for processing '" \
+            + note_book + "' on " + date + ext
+        recipients = os.getenv('ERROR_EMAIL_RECIPIENTS', '')
+        message.attach(MIMEText('ERROR!!! \n' + errormessage, 'plain'))
+    else:
+        file_processing = note_book.split('.ipynb')[0]
+
+        if file_processing == 'auth':
+            subject = 'SBC-AUTH Weekly Stats till ' + datetime.strftime(datetime.now()-timedelta(1), '%Y-%m-%d') + ext
+            filename = 'auth_weekly_stats_till_' + datetime.strftime(datetime.now()-timedelta(1), '%Y-%m-%d') + '.csv'
+            recipients = os.getenv('WEEKLY_REPORT_RECIPIENTS', '')
+        
+        # Add body to email
+        message.attach(MIMEText('Please see attached.', 'plain'))
+
+        # Open file in binary mode
+        with open(os.getenv('DATA_DIR', '')+filename, 'rb') as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            'Content-Disposition',
+            f'attachment; filename= {filename}',
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+
+    message['Subject'] = subject
+    server = smtplib.SMTP(os.getenv('EMAIL_SMTP', ''))
+    email_list = recipients.strip('][').split(', ')
+    logging.info('Email recipients list is: %s', email_list)
+    server.sendmail(os.getenv('SENDER_EMAIL', ''), email_list, message.as_string())
+    logging.info("Email with subject \'%s\' has been sent successfully!", subject)
+    server.quit()
+    os.remove(os.getenv('DATA_DIR', '')+filename)
+
+def processnotebooks(notebookdirectory):    
+    """Process Notebook."""
+    status = False
+    
+    try:
+        retry_times = int(os.getenv('RETRY_TIMES', '1'))
+        retry_interval = int(os.getenv('RETRY_INTERVAL', '60'))
+        if notebookdirectory == 'weekly':
+            week_report_dates = ast.literal_eval(os.getenv('WEEKLY_REPORT_DATES', ''))
+    except Exception:
+        logging.exception('Error processing notebook for %s', notebookdirectory)
+        send_email(notebookdirectory, 'ERROR', traceback.format_exc())
+        return status
+    
+   
+    # For weekly tasks, we only run on the specified days
+    if notebookdirectory == 'weekly' and date.today().weekday() in week_report_dates:        
+        logging.info('Processing: %s', notebookdirectory)
+        
+        num_files = len(os.listdir(notebookdirectory))
+        file_processed = 0
+        
+        for file in findfiles(notebookdirectory, '*.ipynb'):
+            file_processed += 1
+            note_book = os.path.basename(file)
+            for attempt in range(retry_times):
+                try:                    
+                    pm.execute_notebook(file, os.getenv('DATA_DIR', '')+'temp.ipynb', parameters=None)
+                    send_email(note_book, '', '')
+                    os.remove(os.getenv('DATA_DIR', '')+'temp.ipynb')                    
+                    status = True                    
+                    break
+                except Exception:
+                    if attempt + 1 == retry_times:
+                        # If any errors occur with the notebook processing they will be logged to the log file
+                        logging.exception(
+                            'Error processing notebook %s at %s/%s try.', notebookdirectory, attempt + 1,
+                            retry_times)
+                        send_email(notebookdirectory, 'ERROR', traceback.format_exc())
+                    else:
+                        # If any errors occur with the notebook processing they will be logged to the log file
+                        logging.exception('Error processing notebook %s at %s/%s try. '
+                                          'Sleeping for %s secs before next try', notebookdirectory, attempt + 1,
+                                          retry_times, retry_interval)
+                        time.sleep(retry_interval)
+                        continue
+            if not status and num_files == file_processed:
+                break
+    return status
+
+if __name__ == '__main__':
+    start_time = datetime.utcnow()
+  
+    # Check if the subfolders for notebooks exist, and create them if they don't
+    for subdir in ['weekly']:
+        if not os.path.isdir(subdir):
+            os.mkdir(subdir)
+
+        processnotebooks(subdir)
+
+    end_time = datetime.utcnow()
+    logging.info('job - jupyter notebook report completed in: %s', end_time - start_time)
+    sys.exit()
