3f2d355372470132d22dbadf81e46c5f54db8622
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2021-04-09 19:12:17 -0700

    Feature/6886 test cases (#1504)
    6886 - test cases

--- auth-api/tests/unit/services/test_task.py
+++ auth-api/tests/unit/services/test_task.py
@@ -18,8 +18,13 @@ Test suite to ensure that the Task service routines are working as expected.
 
 from datetime import datetime
 from auth_api.services import Task as TaskService
-from auth_api.utils.enums import TaskStatus, TaskType, TaskRelationshipType
-from tests.utilities.factory_utils import factory_task_service, factory_org_model, factory_user_model
+from auth_api.services import Org as OrgService
+from auth_api.services import Affidavit as AffidavitService
+from auth_api.models import Task as TaskModel
+from auth_api.utils.enums import TaskStatus, TaskType, TaskRelationshipType, OrgStatus, LoginSource, AffidavitStatus
+from tests.utilities.factory_scenarios import TestUserInfo, TestJwtClaims, TestAffidavit, TestOrgInfo
+from tests.utilities.factory_utils import factory_task_service, factory_org_model, factory_user_model, \
+    factory_user_model_with_contact
 
 
 def test_fetch_tasks(session, auth_mock):  # pylint:disable=unused-argument
@@ -54,3 +59,43 @@ def test_create_task(session, keycloak_mock):  # pylint:disable=unused-argument
     assert task
     dictionary = task.as_dict()
     assert dictionary['name'] == test_org.name
+
+
+def test_update_task(session, keycloak_mock):  # pylint:disable=unused-argument
+    """Assert that a task can be updated."""
+    user_with_token = TestUserInfo.user_bceid_tester
+    user_with_token['keycloak_guid'] = TestJwtClaims.public_bceid_user['sub']
+    user = factory_user_model_with_contact(user_with_token)
+
+    affidavit_info = TestAffidavit.get_test_affidavit_with_contact()
+    AffidavitService.create_affidavit(token_info=TestJwtClaims.public_bceid_user, affidavit_info=affidavit_info)
+
+    org = OrgService.create_org(TestOrgInfo.org_with_mailing_address(), user_id=user.id,
+                                token_info=TestJwtClaims.public_bceid_user)
+    org_dict = org.as_dict()
+    assert org_dict['org_status'] == OrgStatus.PENDING_STAFF_REVIEW.value
+    org_id = org_dict['id']
+
+    token_info = TestJwtClaims.get_test_user(sub=user.keycloak_guid, source=LoginSource.STAFF.value)
+
+    tasks = TaskService.fetch_tasks(task_type=TaskType.PENDING_STAFF_REVIEW.value,
+                                    task_status=TaskStatus.OPEN.value)
+    fetched_task = tasks[0]
+
+    task_info = {
+        'id': fetched_task.id,
+        'name': 'bar',
+        'dateSubmitted': '2020-11-23T15:14:20.712096+00:00',
+        'relationshipType': TaskRelationshipType.ORG.value,
+        'relationshipId': org_id,
+        'type': TaskType.PENDING_STAFF_REVIEW.value,
+        'status': TaskStatus.COMPLETED.value,
+        'relationshipStatus': AffidavitStatus.APPROVED.value
+    }
+    task: TaskModel = TaskModel.find_by_task_id(fetched_task.id)
+
+    task = TaskService.update_task(TaskService(task), task_info=task_info,
+                                   token_info=token_info)
+    dictionary = task.as_dict()
+    assert dictionary['name'] == 'bar'
+    assert dictionary['status'] == TaskStatus.COMPLETED.value
