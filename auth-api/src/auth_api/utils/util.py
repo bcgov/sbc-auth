0024da99b62306931f75689427acd27614fdf12e
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-04-13 07:34:55 -0700

    WAM friendly fix - corrects ..Ordered the replacement

--- auth-api/src/auth_api/utils/util.py
+++ auth-api/src/auth_api/utils/util.py
@@ -69,5 +69,5 @@ def digitify(payload: str) -> int:
 
 def escape_wam_freindly_url(org_name):
     """Return encoded/escaped url."""
-    encode_org_name = org_name.replace('.', '%2E')
-    return urllib.parse.quote(encode_org_name)
+    encode_org_name = urllib.parse.quote(org_name)
+    return encode_org_name.replace('.', '%2E')
