a4f03e39ea891776e0ddc87865c5eab3023a2890
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-04-19 14:36:26 -0700

    Review Requests to Access Wills Search & Registration Application#4118 | test case

---
+++ auth-web/tests/unit/components/AgreementInformation.spec.ts
@@ -0,0 +1,66 @@
+
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+
+import AgreementInformation from '@/components/auth/staff/review-task/AgreementInformation.vue'
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
+const vuetify = new Vuetify({})
+
+// Prevent the warning "[Vuetify] Unable to locate target [data-app]"
+document.body.setAttribute('data-app', 'true')
+
+describe('AgreementInformation.vue', () => {
+  let wrapper: any
+  let wrapperFactory: any
+  const props = {
+    tabNumber: 2,
+    title: 'Account Information',
+    userName: 'user1',
+    orgName: 'testorg',
+    isTOSAlreadyAccepted: false,
+    isApprovalFlow: true
+  }
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const store = new Vuex.Store({
+      strict: false
+    })
+
+    wrapperFactory = (propsData) => {
+      return shallowMount(AgreementInformation, {
+        localVue,
+        store,
+        vuetify,
+        propsData: {
+          ...propsData
+        }
+      })
+    }
+
+    wrapper = wrapperFactory(props)
+  })
+
+  afterEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('renders the TOS components properly ', () => {
+    expect(wrapper.find(AgreementInformation).exists()).toBe(true)
+  })
+
+  it('renders proper header content', () => {
+    expect(wrapper.find('h2').text()).toBe(`${props.tabNumber}. ${props.title}`)
+  })
+})
---
+++ auth-web/tests/unit/services/task.service.spec.ts
@@ -0,0 +1,81 @@
+
+import { Task } from '@/models/Task'
+import TaskService from '../../../src/services/task.services'
+import axios from 'axios'
+
+var mockob = {
+  'VUE_APP_COPS_REDIRECT_URL': 'https://coops-dev.pathfinder.gov.bc.ca/',
+  'VUE_APP_PAY_ROOT_API': 'https://pay-api-dev.pathfinder.gov.bc.ca/api/v1',
+  'VUE_APP_AUTH_ROOT_API': 'https://auth-api-dev.pathfinder.gov.bc.ca/api/v1'
+}
+const task: Task[] = [{
+  'accountId': 2628,
+  'created': new Date('2021-04-19T16:21:28.989168+00:00'),
+  'createdBy': 'BCREGTEST Jing SIXTEEN',
+  'dateSubmitted': new Date('2021-04-19T16:21:28.989168+00:00'),
+  'id': 44,
+  'modified': new Date('2021-04-19T16:21:28.989168+00:00'),
+  'name': 'sb 16.3',
+  'relationshipId': 3674,
+  'relationshipStatus': 'PENDING_STAFF_REVIEW',
+  'relationshipType': 'PRODUCT',
+  'status': 'OPEN',
+  'type': 'Wills Registry',
+  'user': 31
+}]
+
+describe('Task service', () => {
+  beforeEach(() => {
+    sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(mockob)
+    // @ts-ignore
+    jest.clearAllMocks()
+  })
+
+  it('call getTaskById() for task Details ', () => {
+    jest.mock('axios')
+
+    const resp = { data: task }
+    axios.get = jest.fn().mockReturnValue(resp)
+
+    TaskService.getTaskById(1).then((response) => {
+      expect(response).toEqual(task)
+    })
+  })
+  it('call fetchTasks() for all tasks  ', () => {
+    jest.mock('axios')
+
+    const resp = { data: task }
+    axios.get = jest.fn().mockReturnValue(resp)
+    const taskFilter = {
+      status: 'PENDING_STAFF_REVIEW',
+      type: 'OPEN',
+      pageNumber: 1,
+      pageLimit: 10
+    }
+
+    TaskService.fetchTasks(taskFilter).then((response) => {
+      expect(response).toEqual(task)
+    })
+  })
+
+  it('call approvePendingTask() to approve request ', async () => {
+    jest.mock('axios')
+    const resp = { data: task }
+    axios.put = jest.fn().mockReturnValue(resp)
+
+    TaskService.approvePendingTask(task).then((response) => {
+      expect(response).toEqual(task)
+    })
+  })
+
+  it('call rejectPendingTask() to reject request ', async () => {
+    jest.mock('axios')
+
+    const resp = { data: task }
+    axios.put = jest.fn().mockReturnValue(resp)
+
+    TaskService.rejectPendingTask(task).then((response) => {
+      expect(response).toEqual(task)
+    })
+  })
+})
