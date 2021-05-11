bcaaa109df68e7c263853d088ea2b3f4150b8336
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-05-11 13:21:00 -0700

    Show Details of Transactions / Statements for PPR and Name Request#6939

--- auth-web/tests/unit/components/TransactionsDataTable.spec.ts
+++ auth-web/tests/unit/components/TransactionsDataTable.spec.ts
@@ -1,25 +1,25 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils'
-import BusinessModule from '@/store/modules/business'
 import TransactionsDataTable from '@/components/auth/account-settings/transaction/TransactionsDataTable.vue'
 import UserService from '../../../src/services/user.services'
+
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
 
-Vue.use(Vuetify)
-Vue.use(VueRouter)
 Vue.use(VueI18n)
+Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 describe('TransactionsDataTable.vue', () => {
   let localVue
   let store
 
   const config = {
-    VUE_APP_ROOT_API: 'https://localhost:8080/api/v1/11',
-    VUE_APP_COPS_REDIRECT_URL: 'https://coops-dev.pathfinder.gov.bc.ca/',
-    VUE_APP_PAY_ROOT_API: 'https://pay-api-dev.pathfinder.gov.bc.ca/api/v1'
+    VUE_APP_ROOT_API: 'https://localhost:8080/api/v1/app',
+    VUE_APP_COPS_REDIRECT_URL: 'https://test.gov.bc.ca/',
+    VUE_APP_PAY_ROOT_API: 'https://pay-api.gov.bc.ca/api/v1'
   }
 
   sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
