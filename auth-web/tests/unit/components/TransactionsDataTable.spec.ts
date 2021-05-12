e897e9aaf3948fb416600a2f1e41ca7fb666c343
faiz shabeeb <55506249+shabeeb-aot@users.noreply.github.com>
2021-05-12 08:51:08 -0700

    Feature/6939 (#1606)
    * Show Details of Transactions / Statements for PPR and Name Request#6939

---
+++ auth-web/tests/unit/components/TransactionsDataTable.spec.ts
@@ -0,0 +1,95 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import TransactionsDataTable from '@/components/auth/account-settings/transaction/TransactionsDataTable.vue'
+import UserService from '../../../src/services/user.services'
+
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(VueI18n)
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+
+describe('TransactionsDataTable.vue', () => {
+  let localVue
+  let store
+
+  const config = {
+    VUE_APP_ROOT_API: 'https://localhost:8080/api/v1/app',
+    VUE_APP_COPS_REDIRECT_URL: 'https://test.gov.bc.ca/',
+    VUE_APP_PAY_ROOT_API: 'https://pay-api.gov.bc.ca/api/v1'
+  }
+
+  sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+    const orgModule = {
+      namespaced: true,
+      actions: {
+        getTransactionList: jest.fn(() => {
+          return [
+            {
+              'id': 9656,
+              'transactionNames': [
+                'Priority Request fee'
+              ],
+              'folioNumber': '',
+              'businessIdentifier': '',
+              'initiatedBy': 'BCREGTEST Bena TEST',
+              'transactionDate': '2021-05-10T21:49:21.412338+00:00',
+              'totalAmount': '101.50',
+              'status': 'PAD Invoice Approved',
+              'details': [
+                {
+                  'label': 'NR Number:',
+                  'value': 'NR1234567'
+                },
+                {
+                  'label': 'Name Choices:',
+                  'value': ''
+                },
+                {
+                  'label': '1.',
+                  'value': 'CINEXTREME LEGAL SERVICES LIMITED'
+                },
+                {
+                  'label': '2.',
+                  'value': 'BRUNETTE HUNTING AND TRAPPING LIMITED'
+                },
+                {
+                  'label': '3.',
+                  'value': 'ALKEM CLOTHING STORES LIMITED'
+                }
+              ]
+            }]
+        })
+      },
+      state: {
+        currentOrganization: []
+      }
+    }
+    store = new Vuex.Store({
+      strict: false,
+      modules: {
+        org: orgModule
+      }
+    })
+
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('Shows empty panel message', () => {
+    UserService.getOrganizations = jest.fn().mockResolvedValue({ orgs: [] })
+    const $t = () => ''
+    const wrapper = shallowMount(TransactionsDataTable, {
+      store,
+      localVue,
+      mocks: { $t }
+    })
+    expect(wrapper.text()).toContain('')
+  })
+})
