3ae8d076f1d02605a6a48618f68e07d25ae6e87a
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-05-17 10:14:25 -0700

    Show PAD Credits in Transactions UI

--- auth-web/tests/unit/components/Statements.spec.ts
+++ auth-web/tests/unit/components/Statements.spec.ts
@@ -17,7 +17,7 @@ describe('Statements.vue', () => {
   let store
 
   const config = {
-    VUE_APP_ROOT_API: 'https://localhost:8080/api/v1/app',
+    VUE_APP_ROOT_API: 'https://localhost:8080/api/v1/',
     VUE_APP_COPS_REDIRECT_URL: 'https://test.gov.bc.ca/',
     VUE_APP_PAY_ROOT_API: 'https://pay-api.gov.bc.ca/api/v1'
   }
@@ -30,56 +30,19 @@ describe('Statements.vue', () => {
       namespaced: true,
       actions: {
         getStatementsList: jest.fn(),
-        getStatement: jest.fn(),
-        getTransactionList: jest.fn(() => {
-          return [
-            {
-              'id': 9656,
-              'transactionNames': [
-                'Priority Request fee'
-              ],
-              'folioNumber': '',
-              'businessIdentifier': '',
-              'initiatedBy': 'BCREGTEST Bena TEST',
-              'transactionDate': '2021-05-10T21:49:21.412338+00:00',
-              'totalAmount': '101.50',
-              'status': 'PAD Invoice Approved',
-              'details': [
-                {
-                  'label': 'NR Number:',
-                  'value': 'NR1234567'
-                },
-                {
-                  'label': 'Name Choices:',
-                  'value': ''
-                },
-                {
-                  'label': '1.',
-                  'value': 'CINEXTREME LEGAL SERVICES LIMITED'
-                },
-                {
-                  'label': '2.',
-                  'value': 'BRUNETTE HUNTING AND TRAPPING LIMITED'
-                },
-                {
-                  'label': '3.',
-                  'value': 'ALKEM CLOTHING STORES LIMITED'
-                }
-              ]
-            }]
-        })
+        getStatement: jest.fn()
       },
       state: {
         currentOrganization: {
           'accessType': 'REGULAR',
           'billable': true,
           'created': '2021-05-02T20:54:15.936923+00:00',
-          'createdBy': 'BCREGTEST Bena TEST',
+          'createdBy': 'user1',
           'hasApiAccess': false,
-          'id': 2663,
+          'id': 124,
           'loginOptions': [],
           'modified': '2021-05-02T20:54:15.936935+00:00',
-          'name': 'NEW BC ONLINE TECH TEAM - DEV1',
+          'name': 'Org 2',
           'orgType': 'PREMIUM',
           'orgStatus': 'ACTIVE',
           'products': [
@@ -93,23 +56,23 @@ describe('Statements.vue', () => {
           'bcolAccountDetails': { }
         },
         currentMembership: {
-          'id': 3458,
+          'id': 123,
           'membershipStatus': 'ACTIVE',
           'membershipTypeCode': 'ADMIN',
           'user': {
             'contacts': [
               {
                 'created': '2021-04-30T20:54:17.109390+00:00',
-                'createdBy': 'BCREGTEST Bena TEST',
+                'createdBy': 'user1',
                 'email': 'test1@test.com',
                 'modified': '2021-04-30T20:54:17.109398+00:00',
                 'phone': '',
                 'phoneExtension': ''
               }
             ],
-            'firstname': 'BCREGTEST Bena',
+            'firstname': 'user',
             'id': 4,
-            'lastname': 'TEST',
+            'lastname': 'one',
             'loginSource': 'BCSC',
             'modified': '2021-05-11T19:38:35.067210+00:00',
             'username': 'bcsc/fyd76wbcng76cpxbu42hhua4qphtivb5'
