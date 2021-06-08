63d9bc6037cc92aa75c46bb7e7da24b9ffa6ad2a
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-06-08 15:38:12 -0700

    Fix for migrations not working

--- auth-api/migrations/versions/e7f46c30d356_restricted_ppr.py
+++ auth-api/migrations/versions/e7f46c30d356_restricted_ppr.py
@@ -17,9 +17,9 @@ depends_on = None
 
 def upgrade():
     op.execute("INSERT INTO public.product_codes "
-               "(code, description, type_code, hidden, need_review, premium_only, url) "
+               "(code, description,\"default\", type_code, hidden, need_review, premium_only, url) "
                "VALUES "
-               "('RPPR', 'Restricted Personal Property Registry', 'INTERNAL', true,false, true, "
+               "('RPPR', 'Restricted Personal Property Registry',false, 'INTERNAL', true,false, true, "
                "'https://www.bcregistry.ca/ppr')")
     op.execute("commit")
 
