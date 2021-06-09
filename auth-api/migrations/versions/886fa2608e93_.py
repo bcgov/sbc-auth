9d4b348790e7ffd0f6be371b24baf8ae9a25f7c3
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2021-06-09 12:20:02 -0700

    Adding new fields to the org, removing unwanted columns from code tables.
     Please enter the commit message for your changes. Lines starting

---
+++ auth-api/migrations/versions/886fa2608e93_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 886fa2608e93
+Revises: a0198b1e2c51, 7f3d5d328fd5
+Create Date: 2021-06-09 12:19:44.556067
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '886fa2608e93'
+down_revision = ('a0198b1e2c51', '7f3d5d328fd5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
