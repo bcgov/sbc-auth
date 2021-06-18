470fe64a7f2646d52fde08fb65a5b643ecf1c64c
faiz shabeeb <55506249+shabeeb-aot@users.noreply.github.com>
2021-06-18 15:24:52 -0700

    Client resubmit the Affidavit Info or update Account Info#7781 (#1666)

--- auth-web/tests/unit/components/PaymentMethodSelector.spec.ts
+++ auth-web/tests/unit/components/PaymentMethodSelector.spec.ts
@@ -6,9 +6,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
+import displayMode from '@/directives/displayMode'
 
 Vue.use(Vuetify)
 Vue.use(VueRouter)
+Vue.directive('displayMode', displayMode)
 
 describe('PaymentMethodSelector.vue', () => {
   let wrapper: any
@@ -77,5 +79,10 @@ describe('PaymentMethodSelector.vue', () => {
   it('should select payment method correctly', () => {
     wrapper.vm.setSelectedPayment(PaymentTypes.CREDIT_CARD)
     expect(wrapper.vm.$data.selectedPaymentMethod).toBe(PaymentTypes.CREDIT_CARD)
+  })
+
+  it('should render payment page subtitle correctly', async () => {
+    await wrapper.setProps({ readOnly: true })
+    expect(wrapper.find('[data-test="save-button"]').text()).toBe('Submit')
   })
 })
