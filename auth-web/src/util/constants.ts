7fa542933cb2fde3fc4b1d30cb3b48fa9a64a80d
CameronFreshworks <53542131+cameron-freshworks@users.noreply.github.com>
2021-07-08 11:13:49 -0700

    Business Table Framework (#1716)

--- auth-web/src/util/constants.ts
+++ auth-web/src/util/constants.ts
@@ -168,7 +168,8 @@ export enum LDFlags {
     EnableMandatoryAddress = 'enable-mandatory-address',
     EnableGovmInvite = 'enable-govm-account-invite',
     HideProductPackage = 'hide-product-packages',
-    EnableOrgNameAutoComplete = 'enable-org-name-auto-complete'
+    EnableOrgNameAutoComplete = 'enable-org-name-auto-complete',
+    EnableBusinessTable = 'enable-business-table'
 }
 
 export enum DateFilterCodes {
