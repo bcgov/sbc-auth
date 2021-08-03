2f7e5da9492c21f00d1433609ce9e39737cd2914
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-08-03 10:46:43 -0700

    lint fixes

--- auth-api/src/auth_api/services/org.py
+++ auth-api/src/auth_api/services/org.py
@@ -134,7 +134,7 @@ class Org:  # pylint: disable=too-many-public-methods
         # Send an email to staff to remind review the pending account
         is_bceid_status_handling_needed = access_type in (AccessType.EXTRA_PROVINCIAL.value,
                                                           AccessType.REGULAR_BCEID.value) and not \
-                                              AffidavitModel.find_approved_by_user_id(user_id=user_id)
+            AffidavitModel.find_approved_by_user_id(user_id=user_id)
         user = UserModel.find_by_jwt_token()
         if is_bceid_status_handling_needed:
             Org._handle_bceid_status_and_notification(org, user)
