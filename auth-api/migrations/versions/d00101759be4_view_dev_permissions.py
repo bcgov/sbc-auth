ec6682f7d5fb0c0370d6185ed459fc1ad7eaf045
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-11-08 09:35:41 -0800

    PR comments

--- auth-api/migrations/versions/d00101759be4_view_dev_permissions.py
+++ auth-api/migrations/versions/d00101759be4_view_dev_permissions.py
@@ -17,25 +17,10 @@ depends_on = None
 
 
 def upgrade():
-    permissions_table = table('permissions',
-                              column('id', sa.Integer()),
-                              column('membership_type_code', sa.String(length=15)),
-                              column('org_status_code', sa.String(length=25)),
-                              column('actions', sa.String(length=100)))
-
-    conn = op.get_bind()
-    res = conn.execute(
-        f"select max(id) from permissions;")
-    latest_id = res.fetchall()[0][0]
-
-    # Insert code values
-    op.bulk_insert(
-        permissions_table,
-        [
-            {'id': latest_id + 1, 'membership_type_code': 'ADMIN', 'org_status_code': None,
-             'actions': 'VIEW_DEVELOPER_ACCESS'}
-        ]
-    )
+    permissions_table = table('permissions', column('membership_type_code', sa.String(length=15)), column('actions',
+                                                                                                          sa.String(
+                                                                                                              length=100)))  #
+    op.bulk_insert(permissions_table, [{'membership_type_code': 'ADMIN', 'actions': 'VIEW_DEVELOPER_ACCESS'}])
 
 
 def downgrade():
