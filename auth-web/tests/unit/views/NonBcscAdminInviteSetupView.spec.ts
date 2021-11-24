104711fd6fdc11abf719682cd25078768a2c8c82
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2021-11-24 09:23:20 -0800

    Feature/9326  - Two stepper for upload affidavit BCeid Admin invite(#1821)

---
+++ auth-web/tests/unit/views/NonBcscAdminInviteSetupView.spec.ts
@@ -0,0 +1,43 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+
+import NonBcscAdminInviteSetupView from '@/views/auth/create-account/non-bcsc/NonBcscAdminInviteSetupView.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+const vuetify = new Vuetify({})
+const router = new VueRouter()
+
+describe('NonBcscAdminInviteSetupView.vue', () => {
+  let wrapper: any
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    wrapper = shallowMount(NonBcscAdminInviteSetupView, {
+      localVue,
+      router,
+      vuetify
+    })
+  })
+
+  afterEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('update current step properly', async () => {
+    wrapper.vm.goToNextStep()
+    expect(wrapper.vm.currentStep).toEqual(2)
+    wrapper.vm.goBackPreviousStep()
+    expect(wrapper.vm.currentStep).toEqual(1)
+  })
+})
