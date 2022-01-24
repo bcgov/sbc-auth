e68bae4ef050b8a3d6231f09241ef9b49e0efe44
CameronFreshworks <53542131+cameron-freshworks@users.noreply.github.com>
2022-01-24 11:52:49 -0800

    Removed Config-Helper from Resource (#1889)
    * removed config helper in resource
    
    * fixed session assignment

--- auth-web/src/resources/BreadcrumbResources.ts
+++ auth-web/src/resources/BreadcrumbResources.ts
@@ -1,23 +1,22 @@
 import { BreadcrumbIF } from '@bcrs-shared-components/interfaces'
-import ConfigHelper from '@/util/config-helper'
 
 export const RegistryHomeBreadcrumb: BreadcrumbIF = {
   text: 'BC Registries and Online Services',
-  href: `${ConfigHelper.getValue('REGISTRY_HOME_URL')}`
+  href: `${sessionStorage.getItem('REGISTRY_HOME_URL')}`
 }
 
 export const RegistryDashboardBreadcrumb: BreadcrumbIF = {
   text: 'BC Registries Dashboard',
-  href: `${ConfigHelper.getValue('REGISTRY_HOME_URL')}dashboard`
+  href: `${sessionStorage.getItem('REGISTRY_HOME_URL')}dashboard`
 }
 
 export const MyBusinessRegistryBreadcrumb: BreadcrumbIF = {
   text: 'My Business Registry',
   to: { name: 'business' },
-  href: `${ConfigHelper.getValue('AUTH_WEB_URL')}/business`
+  href: `${sessionStorage.getItem('AUTH_WEB_URL')}/business`
 }
 
 export const StaffDashboardBreadcrumb: BreadcrumbIF = {
   text: 'Staff Dashboard',
-  href: `${ConfigHelper.getValue('AUTH_WEB_URL')}/staff/dashboard/active`
+  href: `${sessionStorage.getItem('AUTH_WEB_URL')}/staff/dashboard/active`
 }
