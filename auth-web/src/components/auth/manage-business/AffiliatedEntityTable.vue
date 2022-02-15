4e17db8b22dc2e862b3bbc01987aaedeb78690dc
Vysakh Menon <vysakh.menon@aot-technologies.com>
2022-02-15 09:21:37 -0800

    11134 moving business object into registration schema (#1909)
    * 11134 moving business object into registration schema
    
    * no message
    
    * no message

--- auth-web/src/components/auth/manage-business/AffiliatedEntityTable.vue
+++ auth-web/src/components/auth/manage-business/AffiliatedEntityTable.vue
@@ -397,14 +397,13 @@ export default class AffiliatedEntityTable extends Mixins(DateMixin) {
           name: FilingTypes.REGISTRATION,
           accountId: this.currentOrganization.id
         },
-        business: {
-          legalType: business.nameRequest.legalType,
-          natureOfBusiness: business.nameRequest.natureOfBusiness
-        },
         registration: {
           nameRequest: {
             legalType: business.nameRequest.legalType,
             nrNumber: business.businessIdentifier
+          },
+          business: {
+            natureOfBusiness: business.nameRequest.natureOfBusiness
           }
         }
       }
