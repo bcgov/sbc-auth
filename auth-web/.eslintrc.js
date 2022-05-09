6f9a9bf8cb7b1813166f390d499c575152095321
Travis Semple <travis8814@gmail.com>
2022-05-09 14:02:00 -0700

    Upgrade Node version. Set vue-template-compiler and Vue to 2.6.12, same version as @bcrs-shared-components/bread-crumb. Upgrade Vue-CLI to 4.5.17.

--- auth-web/.eslintrc.js
+++ auth-web/.eslintrc.js
@@ -12,9 +12,12 @@ module.exports = {
     'no-console': 'error',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'sort-imports': 'error',
-    'space-before-function-paren': 1
+    'space-before-function-paren': 1,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error'
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint']
   }
 }
