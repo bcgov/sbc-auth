1451307647d089ce40c2f465b1822d39835ede8b
Kial Jinnah <kialjinnah@gmail.com>
2022-05-27 11:57:20 -0700

    added business search product
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

---
+++ auth-api/migrations/versions/211deb552319_.py
@@ -0,0 +1,29 @@
+"""Insert Business search product code
+
+Revision ID: 211deb552319
+Revises: cd89e8e61dec
+Create Date: 2022-05-27 11:43:03.530212
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '211deb552319'
+down_revision = 'cd89e8e61dec'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("INSERT INTO public.product_codes "
+             "(code, description,\"default\", type_code, hidden, need_review, premium_only, url) "
+             "VALUES "
+             "('BUSINESS_SEARCH', 'Business Search', false, 'INTERNAL', false, false, true, "
+             "'https://www.bcregistry.ca/business/search')")
+    op.execute("commit")
+
+
+def downgrade():
+    op.execute("DELETE FROM product_codes WHERE code in ('BUSINESS_SEARCH')")
