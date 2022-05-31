c2b2dc9df67a53acf0aa2694fe160c86989db042
Travis Semple <travis8814@gmail.com>
2022-05-31 12:34:47 -0700

    Revert "Updated Assets Staff Tile and Configurations (#1972)" (#1973)
    This reverts commit 325eb18285a77d5f95c500fa3169434e0fd3b8b2.

--- auth-web/tests/unit/components/PPRLauncher.spec.ts
+++ auth-web/tests/unit/components/PPRLauncher.spec.ts
@@ -12,51 +12,29 @@ Vue.use(VueI18n)
 
 describe('PPRLauncher.vue', () => {
   let wrapper: Wrapper<any>
-  const assetLauncherText = 'Register or search for manufactured homes and register or search for legal claims on ' +
-      'personal property.'
-  const assetLauncherTitle = 'Staff Asset Registries'
-  const pprLauncherText = 'Register or search for legal claims on personal property.'
-  const pprLauncherTitle = 'Staff Personal Property Registry'
-  const mhrLauncherText = 'Register or search for legal claims on manufactured homes.'
-  const mhrLauncherTitle = 'Staff Manufactured Home Registry'
-
+  const title = 'Staff Personal Property Registry'
+  const text = 'Register or search for legal claims on personal property.'
   const pprUrl = 'ppr-web'
   const config = {
     'PPR_WEB_URL': pprUrl
   }
   sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
 
-  let userModule = {
-    namespaced: true,
-    state: {
-      currentUser: {
-        fullName: 'user2',
-        roles: ['ppr_staff']
-      }
-    }
-  }
-
   beforeEach(() => {
     const localVue = createLocalVue()
     localVue.use(Vuex)
+
     const vuetify = new Vuetify({})
 
     const store = new Vuex.Store({
       state: {},
       strict: false,
-      modules: { user: userModule }
+      modules: {}
     })
-
     const $t = (val: string) => {
-      switch (val) {
-        case 'pprLauncherTitle': return pprLauncherTitle
-        case 'pprLauncherText': return pprLauncherText
-        case 'mhrLauncherTitle': return mhrLauncherTitle
-        case 'mhrLauncherText': return mhrLauncherText
-        case 'assetLauncherTitle': return assetLauncherTitle
-        case 'assetLauncherText': return assetLauncherText
-        default: return ''
-      }
+      if (val === 'pprLauncherTitle') return title
+      if (val === 'pprLauncherText') return text
+      return ''
     }
     wrapper = mount(PPRLauncher, {
       store,
@@ -67,47 +45,18 @@ describe('PPRLauncher.vue', () => {
   })
   afterEach(() => {
     wrapper.destroy()
-    jest.resetModules()
-    jest.clearAllMocks()
   })
 
-  it('is a Vue instance', async () => {
+  it('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
-  it('renders img, title, text and button as PPR staff', async () => {
+  it('renders img, title, text and button', () => {
     expect(wrapper.find('.product-container').exists()).toBe(true)
     expect(wrapper.find('.product-img').exists()).toBe(true)
     expect(wrapper.find('.product-info').exists()).toBe(true)
     expect(wrapper.find('.product-info h2').text()).toBe('Staff Personal Property Registry')
     expect(wrapper.find('.product-info p').text()).toContain('claims on personal property.')
-    expect(wrapper.find('.action-btn').exists()).toBe(true)
-    expect(wrapper.find('.action-btn').text()).toContain('Open')
-    expect(wrapper.vm.pprUrl).toEqual(pprUrl)
-  })
-
-  it('renders img, title, text and button as MHR staff', async () => {
-    userModule.state.currentUser.roles = ['mhr_staff']
-    await Vue.nextTick()
-    expect(wrapper.find('.product-container').exists()).toBe(true)
-    expect(wrapper.find('.product-img').exists()).toBe(true)
-    expect(wrapper.find('.product-info').exists()).toBe(true)
-    expect(wrapper.find('.product-info h2').text()).toBe('Staff Manufactured Home Registry')
-    expect(wrapper.find('.product-info p').text()).toContain('claims on manufactured homes.')
-    expect(wrapper.find('.action-btn').exists()).toBe(true)
-    expect(wrapper.find('.action-btn').text()).toContain('Open')
-    expect(wrapper.vm.pprUrl).toEqual(pprUrl)
-  })
-
-  it('renders img, title, text and button as Asset staff', async () => {
-    userModule.state.currentUser.roles = ['mhr_staff', 'ppr_staff']
-    await Vue.nextTick()
-    expect(wrapper.find('.product-container').exists()).toBe(true)
-    expect(wrapper.find('.product-img').exists()).toBe(true)
-    expect(wrapper.find('.product-info').exists()).toBe(true)
-    expect(wrapper.find('.product-info h2').text()).toBe('Staff Asset Registries')
-    expect(wrapper.find('.product-info p').text()).toContain('manufactured homes and register or ' +
-        'search for legal claims on personal property.')
     expect(wrapper.find('.action-btn').exists()).toBe(true)
     expect(wrapper.find('.action-btn').text()).toContain('Open')
     expect(wrapper.vm.pprUrl).toEqual(pprUrl)
