3e2e83894f1c5c2ddd4050f0fc9927dd57b02518
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2022-06-08 16:41:08 -0700

    User Navigation after logging in#12301

--- auth-web/src/views/auth/SignoutView.vue
+++ auth-web/src/views/auth/SignoutView.vue
@@ -1,9 +1,10 @@
 <template>
-  <sbc-signout :redirect-url="redirectUrl"></sbc-signout>
+  <sbc-signout :redirect-url="redirectbackUrl"></sbc-signout>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import ConfigHelper from '@/util/config-helper'
 import SbcSignout from 'sbc-common-components/src/components/SbcSignout.vue'
 
 @Component({
@@ -16,6 +17,15 @@ import SbcSignout from 'sbc-common-components/src/components/SbcSignout.vue'
 
 export default class SignoutView extends Vue {
   @Prop() redirectUrl: string
+
+  get redirectbackUrl () {
+    // redirect to dashboard on logout
+    // TODO need to fix for one URL
+    if (!this.redirectUrl) {
+      return ConfigHelper.getRegistryHomeURL()
+    }
+    return this.redirectUrl
+  }
 
   async mounted () {
     this.$store.replaceState({})
