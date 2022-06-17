d67f12759e35e3d7fafe34ada4163f70fa9cc73e
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2022-06-17 09:21:37 -0700

    AUTH-WEB / COMMON-COMPONENTS - User Navigation while switching accounts#12363

--- auth-web/src/components/auth/mixins/NextPageMixin.vue
+++ auth-web/src/components/auth/mixins/NextPageMixin.vue
@@ -216,8 +216,8 @@ export default class NextPageMixin extends Vue {
       this.anyPendingRedirect = true
       this.$router.push(`/${Pages.UPDATE_ACCOUNT}`)
     } else if (this.currentMembership.membershipStatus === MembershipStatus.Active && this.$route.path.indexOf(Pages.PENDING_APPROVAL) > 0) {
-      // 1. If user was in a pending approval page and switched to an active account, take them to the home page
-      this.$router.push(`/home`)
+      // 1. If user was in a pending approval page and switched to an active account, take them to the back to page
+      this.anyPendingRedirect = false
     } else if (this.currentMembership.membershipStatus === MembershipStatus.Pending) {
       this.anyPendingRedirect = true
       const label = encodeURIComponent(btoa(this.currentAccountSettings?.label))
