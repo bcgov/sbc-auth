5261ba585b83249bcb7329e6f2bce2d606c3a4d1
Travis Semple <travis8814@gmail.com>
2022-06-29 20:42:21 -0700

    Remove legacy service worker.

--- auth-web/vue.config.js
+++ auth-web/vue.config.js
@@ -1,7 +1,14 @@
+const RemoveServiceWorkerPlugin = require('webpack-remove-serviceworker-plugin')
 var path = require('path')
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
+    plugins: [
+      // this is needed to remove existing service workers on users' systems
+      // ref: https://www.npmjs.com/package/webpack-remove-serviceworker-plugin
+      // ref: https://github.com/NekR/self-destroying-sw/tree/master/packages/webpack-remove-serviceworker-plugin
+      new RemoveServiceWorkerPlugin({ filename: 'service-worker.js' })
+    ],
     resolve: {
       alias: {
         'vue': path.resolve('./node_modules/vue'),
@@ -15,36 +22,6 @@ module.exports = {
     overlay: {
       warnings: true,
       errors: true
-    }
-  },
-  pwa: {
-    name: 'Business Registry',
-    themeColor: '#003366',
-    msTileColor: '#fcba19',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    manifestOptions: {
-      name: 'Business Registry',
-      short_name: 'Business Registry',
-      start_url: '/business/auth/',
-      display: 'standalone',
-      theme_color: '#003366',
-      background_color: '#fcba19',
-      scope: '.'
-    },
-    iconPaths: {
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
-      msTileImage: 'img/icons/msapplication-icon-144x144.png'
-    },
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/service-worker.js',
-      // skip precaching json files such as configs
-      exclude: [/\.json$/]
     }
   }
 }
