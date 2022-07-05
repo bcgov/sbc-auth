da894730eefaa978e347ea128c44f72eb6e5d57b
Travis Semple <travis8814@gmail.com>
2022-07-05 07:41:12 -0700

    Service Worker - Minor Tweaks (#2014)
    * Update package.json.
    Remove event handler for skipWaiting, just do it.
    Fix infinite loop loading issue.
    
    * Update package-lock.json

--- auth-web/src/service-worker.js
+++ auth-web/src/service-worker.js
@@ -5,9 +5,4 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings && workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-// install new service worker when ok, then reload page.
-self.addEventListener && self.addEventListener('message', msg => {
-  if (msg.data.action === 'skipWaiting') {
-    self.skipWaiting()
-  }
-})
+self.skipWaiting()
