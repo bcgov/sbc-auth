71d1ed75dc4c58305b73a30ab521d9cdc91e74e3
Travis Semple <travis8814@gmail.com>
2022-07-05 23:27:02 -0700

    12821 - Remove legacy service worker / PWA. (#2009) (#2010)
    * Remove legacy service worker. (#2009)
    
    * Put url back.
    
    * Small code clean up.
    
    * Fix nginx.conf.
    Remove unused lines in Dockerfile.
    
    * Update location for caching.
    
    * Remove no-cache, no-store, must-revalidate for all NGINX responses.
    Remove separate section regarding specific files that need to be cached.

--- auth-web/src/registerServiceWorker.ts
+++ auth-web/src/registerServiceWorker.ts
@@ -1,29 +1,8 @@
-/* eslint-disable no-console */
-
-import ConfigHelper from './util/config-helper'
-import { SessionStorageKeys } from './util/constants'
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered (event) {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated (updatedEvent) {
-      // write code for popup here
-      console.log('New content is available; refreshing page in 1 sec')
+    updated () {
       // remove older cached content
       // ref: https://santhoshkumarravi.medium.com/vue-pwa-disable-5463e44b1f7f
       caches.keys().then(names => {
@@ -31,23 +10,6 @@ if (process.env.NODE_ENV === 'production') {
           caches.delete(name)
         }
       })
-
-      // Remove Launch Darkly files from session if new build content is downloaded
-      // Once the app is reloaded after the timeout, the App.vue will initialize the LD againx
-      // Otherwise, user will only get the LD flags if he/she signout-and-signin or restart-browser
-      ConfigHelper.removeFromSession(SessionStorageKeys.LaunchDarklyFlags)
-
-      // timeout is for the service worker to get activated
-      setTimeout(() => {
-        window.location.reload(true)
-      }, 1000)
-    },
-    offline () {
-      // add offline indication here
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
     }
   })
 }
--- auth-web/src/service-worker.js
+++
@@ -1,8 +0,0 @@
-
-workbox.core.setCacheNameDetails({ prefix: 'auth-web' })
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings && workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
-self.skipWaiting()
