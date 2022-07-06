96ac3d1661d56510a988c084fe0878743b220abc
Travis Semple <travis8814@gmail.com>
2022-07-05 22:48:57 -0700

    Revert "Service Worker - Minor Tweaks (#2014)"
    This reverts commit da894730eefaa978e347ea128c44f72eb6e5d57b.

--- auth-web/src/service-worker.js
+++ auth-web/src/service-worker.js
@@ -5,4 +5,9 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings && workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-self.skipWaiting()
+// install new service worker when ok, then reload page.
+self.addEventListener && self.addEventListener('message', msg => {
+  if (msg.data.action === 'skipWaiting') {
+    self.skipWaiting()
+  }
+})
