6cfe3efc81d3488fe31c88cb46387bf5aa68bfa9
Lewis Chen <lewischenstudio@gmail.com>
2022-07-08 14:23:31 -0700

    12792: squash commits
    12792: pj
    
    12792: legal name
    
    12792: app version
    
    12792: affliate
    
    12792: unit tests
    
    12792: fixes
    
    12792: lint

--- auth-web/tests/unit/components/Certify.spec.ts
+++ auth-web/tests/unit/components/Certify.spec.ts
@@ -1,16 +1,43 @@
-import { Wrapper, mount } from '@vue/test-utils'
+import { Wrapper, createLocalVue, mount } from '@vue/test-utils'
 import Certify from '@/components/auth/manage-business/Certify.vue'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import Vuex from 'vuex'
 
 Vue.use(Vuetify)
 const vuetify = new Vuetify({})
 
 describe('Add Business Form', () => {
-  let wrapper: Wrapper<any>
+  let wrapper: Wrapper<Certify>
+  let userModule: any
+
+  userModule = {
+    namespaced: true,
+    state: {
+      userProfile: {
+        firstname: 'Nadia',
+        lastname: 'Woodie'
+      }
+    },
+    actions: {
+      getUserProfile: jest.fn()
+    }
+  }
 
   beforeAll(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const store = new Vuex.Store({
+      strict: false,
+      modules: {
+        user: userModule
+      }
+    })
+
     wrapper = mount(Certify, {
+      store,
+      localVue,
       vuetify,
       propsData: {
         legalName: 'Legal Name',
@@ -30,7 +57,7 @@ describe('Add Business Form', () => {
     expect(wrapper.find('#certify').isVisible()).toBe(true)
 
     // verify checkbox
-    expect(wrapper.find('.certify-checkbox label').text()).toContain('Legal Name')
+    expect(wrapper.find('.certify-checkbox label').text()).toContain('Woodie, Nadia')
     expect(wrapper.find('.certify-checkbox label').text()).toContain('certifies that')
     expect(wrapper.find('.certify-checkbox label').text()).toContain('of the entity')
     expect(wrapper.find('.certify-clause').text()).toBe('Lorem ipsum dolor sit amet.')
