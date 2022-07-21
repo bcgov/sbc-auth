c20d75d6832cbfaafbaab5c7cb099fd070d3cc50
Travis Semple <travis8814@gmail.com>
2022-07-20 17:21:06 -0700

    Fix unit test.

--- auth-api/tests/unit/api/test_activity_log.py
+++ auth-api/tests/unit/api/test_activity_log.py
@@ -100,7 +100,7 @@ def test_fetch_activity_log_masking(client, jwt, session):  # pylint:disable=unu
     assert schema_utils.validate(activity_logs, 'paged_response')[0]
     assert rv.status_code == http_status.HTTP_200_OK
     staff_actor = activity_logs.get('activityLogs')[0]
-    assert staff_actor.get('actor') == staff_user.username
+    assert staff_actor.get('actor') == f'{user.firstname} {user.lastname}'
 
     user_actor = activity_logs.get('activityLogs')[1]
     assert user_actor.get('actor') == f'{user.firstname} {user.lastname}'
