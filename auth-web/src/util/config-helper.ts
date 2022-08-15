b7959dfa23bea679cf9100319d0d1225b1b305d8
Kial Jinnah <kialjinnah@gmail.com>
2022-08-15 09:58:24 -0700

    added reg serch link to staff dashboard
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- auth-web/src/util/config-helper.ts
+++ auth-web/src/util/config-helper.ts
@@ -149,6 +149,10 @@ export default class ConfigHelper {
     return ConfigHelper.getValue('API_DOCUMENTATION_URL')
   }
 
+  static getRegistrySearchUrl () {
+    return ConfigHelper.getValue('REGISTRY_SEARCH_URL')
+  }
+
   static getValue (key: String) {
     // @ts-ignore
     return JSON.parse(sessionStorage.getItem(SessionStorageKeys.ApiConfigKey))[key]
