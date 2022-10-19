baf6252f0bcaf4263c8a4b7b477f953fa3e6377d
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2022-10-19 10:28:45 -0700

    - revert dynamic table height (#2117)
    - app version = 1.1.13

--- auth-web/src/components/auth/manage-business/AffiliatedEntityTable.vue
+++ auth-web/src/components/auth/manage-business/AffiliatedEntityTable.vue
@@ -9,6 +9,7 @@
         :class="{ 'header-high-layer': dropdown.includes(true) }"
         :headers="getHeaders"
         :items="businesses"
+        :height="getMaxHeight"
         :custom-sort="customSort"
         fixed-header
         disable-pagination
@@ -175,6 +176,11 @@ export default class AffiliatedEntityTable extends Mixins(DateMixin) {
   /** The headers we want to show. */
   private get getHeaders (): Array<any> {
     return this.headers?.filter(x => x.show)
+  }
+
+  /** The set height when affiliation count exceeds 5 */
+  private get getMaxHeight (): string {
+    return this.entityCount > 5 ? '32rem' : null
   }
 
   /** Returns true if the affiliation is a Name Request */
@@ -526,17 +532,6 @@ export default class AffiliatedEntityTable extends Mixins(DateMixin) {
     display: flex;
     background-color: $app-lt-blue;
     padding: .875rem;
-  }
-
-  // size the table according to window height
-  ::v-deep .v-data-table__wrapper {
-    min-height: 9rem; // height of no-data div
-    height: calc(100vh - 31rem); // full height minus footer
-    background-color: $gray1; // hide unused space below table
-
-    table {
-      background-color: white;
-    }
   }
 
   .names-block {
