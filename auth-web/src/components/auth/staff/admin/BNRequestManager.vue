a48254ca024fc1544d9fcb21b5046aa84f11010e
vysakh.menon <vysakh.menon@aot-technologies.com>
2022-10-31 13:27:15 -0700

    14004 BN Administrative - Improvement

--- auth-web/src/components/auth/staff/admin/BNRequestManager.vue
+++ auth-web/src/components/auth/staff/admin/BNRequestManager.vue
@@ -1,60 +1,101 @@
 <template>
   <v-card flat id="bn-request-manager">
-        <ResubmitRequestDialog v-if="requestDetails"
-          :dialog="resubmitRequestDialog"
-          :xmlData="requestDetails.request"
-          @resubmit="resubmitRequest($event)"
-          @close="hideResubmitRequestDialog($event)"
-          attach="#bn-request-manager"
-        />
+    <ResubmitRequestDialog
+      v-if="requestDetails && resubmitRequestDialog"
+      :dialog="resubmitRequestDialog"
+      :xmlData="requestDetails.request"
+      @resubmit="resubmitRequest($event)"
+      @close="hideResubmitRequestDialog($event)"
+      attach="#bn-request-manager"
+    />
+    <v-dialog
+      v-if="requestDetails && responseDialog"
+      v-model="responseDialog"
+      scrollable
+      attach="#bn-request-manager"
+    >
+      <v-card>
+        <v-card-title>BN Hub Response</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="pt-1 pb-1">
+          <v-textarea
+            ref="textarea"
+            auto-grow
+            outlined
+            readonly
+            hide-details
+            id="response-textarea"
+            :value="requestDetails.response"
+          />
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            large depressed
+            id="response-cancel-button"
+            class="ml-2"
+            @click="responseDialog = false"
+          >Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-      <div class="table-header d-flex">
-        <label>BN Request History</label>
-      </div>
-      <v-data-table
-        id="bn-request-table"
-        :headers="headers"
-        :items="bnRequests"
-        fixed-header
-        disable-pagination
-        hide-default-footer
-      >
-        <template v-slot:item="{ item, index }">
-          <tr>
-            <td>{{ item.requestType }}</td>
-            <td>
-              <v-icon v-if="item.isProcessed" color="green" class="names-text pr-1" small>mdi-check</v-icon>
-              <v-icon v-else color="red" class="names-text pr-1" small>mdi-close</v-icon>
-            </td>
-            <td>
-              <v-icon v-if="item.isAdmin" color="green" class="names-text pr-1" small>mdi-check</v-icon>
-              <v-icon v-else color="red" class="names-text pr-1" small>mdi-close</v-icon>
-            </td>
-            <td class="action-cell">
-              <div class="actions" :id="`action-menu-${index}`">
-                <span class="open-action">
-                  <v-btn
-                    small
-                    color="primary"
-                    min-width="5rem"
-                    min-height="2rem"
-                    class="open-action-btn"
-                    @click="showResubmitRequestDialog(item)"
-                  >
-                    Edit
-                  </v-btn>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </template>
-      </v-data-table>
+    <div class="table-header d-flex">
+      <label>BN Request History</label>
+    </div>
+    <v-data-table
+      id="bn-request-table"
+      :headers="headers"
+      :items="bnRequests"
+      fixed-header
+      disable-pagination
+      hide-default-footer
+    >
+      <template v-slot:item="{ item, index }">
+        <tr>
+          <td>{{ requestTypeText(item.requestType) }}</td>
+          <td>
+            <v-icon v-if="item.isProcessed" color="green" class="names-text pr-1" small>mdi-check</v-icon>
+            <v-icon v-else color="red" class="names-text pr-1" small>mdi-close</v-icon>
+          </td>
+          <td>
+            <v-icon v-if="item.isAdmin" color="green" class="names-text pr-1" small>mdi-check</v-icon>
+            <v-icon v-else color="red" class="names-text pr-1" small>mdi-close</v-icon>
+          </td>
+          <td>{{ formatDate(item.creationDate) }}</td>
+          <td class="action-cell">
+            <div class="actions" :id="`action-menu-${index}`">
+              <v-btn
+                small
+                color="primary"
+                min-width="5rem"
+                min-height="2rem"
+                class="mr-1"
+                @click="showResubmitRequestDialog(item)"
+              >
+                Edit
+              </v-btn>
+              <v-btn
+                small
+                color="primary"
+                min-height="2rem"
+                @click="showReponseDialog(item)"
+              >
+                View Response
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Watch } from 'vue-property-decorator'
 import { RequestTracker, ResubmitBNRequest } from '@/models/request-tracker'
+import { RequestTrackerType } from '@/util/constants'
 import ResubmitRequestDialog from '@/components/auth/staff/admin/ResubmitRequestDialog.vue'
 import Vue from 'vue'
 import { namespace } from 'vuex-class'
@@ -82,16 +123,42 @@ export default class BNRequestManager extends Vue {
     { text: 'Request Type', align: 'start', value: 'requestType', sortable: false, show: true },
     { text: 'Processed', value: 'isProcessed', sortable: false, show: true },
     { text: 'Admin Request', value: 'isAdmin', sortable: false, show: true },
+    { text: 'Creation Date', value: 'creationDate', sortable: false, show: true },
     { text: 'Actions', align: 'end', value: 'action', sortable: false, show: true }
   ]
 
   protected bnRequests: RequestTracker[] = []
   protected requestDetails: RequestTracker = null
   protected resubmitRequestDialog = false
+  protected responseDialog = false
 
   @Watch('businessIdentifier', { deep: true, immediate: true })
   async businessIdentifierChange () {
     this.bnRequests = await this.getBNRequests(this.businessIdentifier)
+  }
+
+  private requestTypeText (requestType: string) {
+    switch (requestType) {
+      case RequestTrackerType.InformCRA:
+        return 'CreateProgramAccountRequest'
+      case RequestTrackerType.ChangeDeliveryAddress:
+      case RequestTrackerType.ChangeMailingAddress:
+        return 'ChangeAddress'
+      case RequestTrackerType.ChangeName:
+      case RequestTrackerType.ChangeParty:
+        return 'ChangeName'
+      case RequestTrackerType.ChangeStatus:
+        return 'ChangeStatus'
+    }
+  }
+
+  private formatDate (date: string) {
+    return date ? date.substring(0, 10) : ''
+  }
+
+  private async showReponseDialog (item: RequestTracker): Promise<void> {
+    this.requestDetails = await this.getRequestTracker(item.id)
+    this.responseDialog = true
   }
 
   private async showResubmitRequestDialog (item: RequestTracker): Promise<void> {
@@ -128,10 +195,9 @@ export default class BNRequestManager extends Vue {
 }
 
 .action-cell {
-  max-width: 0;
+  white-space: nowrap;
   max-height: 30px !important;
   text-align: end !important;
-  padding-right: 5;
 }
 
 // table header overlaps with appHeader. setting z-index from 2 -> 1
--- auth-web/src/components/auth/staff/admin/ResubmitRequestDialog.vue
+++ auth-web/src/components/auth/staff/admin/ResubmitRequestDialog.vue
@@ -2,26 +2,28 @@
   <v-dialog
     v-model="dialog"
     persistent
+    scrollable
     :attach="attach"
     content-class="resubmit-request-dialog"
   >
     <v-card flat>
       <v-card-title id="dialog-title">Modify Request</v-card-title>
-      <v-card-text>
+      <v-divider></v-divider>
+      <v-card-text class="pt-1 pb-1">
         <v-textarea
-          ref="textarea"
+          ref="requestTextarea"
           filled
           outlined
-          auto-grow
-          rows="5"
+          no-resize
+          hide-details
           id="resubmit-request-textarea"
           :value="xmlData"
-          :autofocus="true"
           spellcheck="false"
           @input="emitInput($event)"
         />
       </v-card-text>
-      <v-card-actions class="pt-0">
+      <v-divider></v-divider>
+      <v-card-actions>
         <v-spacer></v-spacer>
         <div class="form__btns">
           <v-btn
@@ -66,7 +68,31 @@ export default class ResubmitRequest extends Vue {
 
   private modifiedXml: string
   private emitInput (val: string): void {
+    this.calculateInputHeight()
     this.modifiedXml = val
+  }
+
+  private mounted () {
+    this.modifiedXml = this.xmlData
+    this.calculateInputHeight()
+  }
+
+  private calculateInputHeight () {
+    // alternate to auto-grow in v-textarea.
+    // unable to use auto-grow (in an editable textarea) due to a bug in this version.
+    // we can use auto-grow and remove this when we upgrade vuetify to v3.0.0
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    const input = this.$refs.requestTextarea.$refs.input
+    if (!input) return
+
+    const height = input.scrollHeight
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    const minHeight = parseInt(this.$refs.requestTextarea.rows, 10) * parseFloat(this.$refs.requestTextarea.rowHeight)
+    input.style.height = Math.max(minHeight, height) + 'px'
   }
 
   @Emit('resubmit')
