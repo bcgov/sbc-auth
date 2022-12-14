aa018a7452bb08424c920be17aacf0a7c4331d82
Travis Semple <travis8814@gmail.com>
2022-12-14 14:14:01 -0800

    Fix for query.

--- docs/docs/database/bca_users_accounts.sql
+++ docs/docs/database/bca_users_accounts.sql
@@ -1,6 +1,4 @@
----BCA USERS/ BCA ACCOUNTS:  Naming Convention should be BCA_USERS_ACCOUNTS_YYYYMMDD
----(example:BCA_USERS_ACCOUNTS_20221001)
-
+-- Admins contacts of BCA accounts who purchased invoices in the month of December 2022
 select
 	ps.org_id as "ACCOUNT NUMBER",
 	o.type_code as "ACCOUNT TYPE",
@@ -10,28 +8,29 @@ select
 	o.branch_name as "BRANCH NAME",
 	m.membership_type_code as "USER TYPE",
 	u.first_name || ' ' ||u.last_name as "USER NAME",
-	u.email as "USER EMAIL", -- USER PROFILE	
-	(case when u.status = 1 then 'ACTIVE' else 'INACTIVE' end) as "USER STATUS" -- if 1 ACTIVE | 2 INACTIVE
+	u.email as "USER EMAIL",
+	(case when u.status = 1 then 'ACTIVE' else 'INACTIVE' end) as "USER STATUS",
 	c.street as "ADDRESS 1",
 	c.street_additional as "ADDRESS 2",
 	c.city as "CITY",
 	c.region as "PROVINCE",
 	c.country as "COUNTRY",
 	c.postal_code as "POSTAL CODE",
-	c.email as "FIRST ADMIN EMAIL", -- the contact info which pertains to the admin’s of an ORG
-	c.phone as "FIRST ADMIN PHONE", --the contact info which pertains to the admin’s of an ORG
-
-
+	c2.email as "ADMIN EMAIL",
+	c2.phone as "ADMIN PHONE"
 from
 orgs o
 INNER JOIN product_subscriptions ps ON o.id = ps.org_id
 INNER JOIN memberships m ON o.id = m.org_id
 INNER JOIN contact_links cl ON cl.org_id = o.id --the contact info which pertains to the admin’s of an ORG
-INNER join users u on u.id = m.user_id
-INNER join contacts c on c.id = cl.contact_id
+INNER JOIN users u on u.id = m.user_id
+INNER JOIN contacts c on c.id = cl.contact_id
+LEFT JOIN contact_links cl2 on cl2.user_id = m.user_id
+LEFT JOIN contacts c2 on c2.id = cl2.contact_id 
+
 where 
 ps.product_code = 'BCA' 
-and 
-to_char(o.created ,'YYYY-MM') = '2022-09'---change to month you need data for
+and membership_type_code = 'ADMIN'
+and o.id in ('') -- GRAB FROM PAY query.
 
 order by 3 desc;
