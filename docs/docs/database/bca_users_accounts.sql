a773cb5662290e5ff0546ed8e156259d26c3f5b5
Jia Xu <Jia.Xu@gov.bc.ca>
2022-12-20 15:32:28 -0800

    14507 added column of BCOL USER ID and added 'FIRST' to Admin Email&Phone column

--- docs/docs/database/bca_users_accounts.sql
+++ docs/docs/database/bca_users_accounts.sql
@@ -7,6 +7,7 @@ select
 	o.name as "ACCOUNT NAME",
 	o.branch_name as "BRANCH NAME",
 	m.membership_type_code as "USER TYPE",
+	o.bcol_user_id as "BCOL USER ID",
 	u.first_name || ' ' ||u.last_name as "USER NAME",
 	u.email as "USER EMAIL",
 	(case when u.status = 1 then 'ACTIVE' else 'INACTIVE' end) as "USER STATUS",
@@ -16,8 +17,8 @@ select
 	c.region as "PROVINCE",
 	c.country as "COUNTRY",
 	c.postal_code as "POSTAL CODE",
-	c2.email as "ADMIN EMAIL",
-	c2.phone as "ADMIN PHONE"
+	c2.email as "FIRST ADMIN EMAIL",
+	c2.phone as "FIRST ADMIN PHONE"
 from
 orgs o
 INNER JOIN product_subscriptions ps ON o.id = ps.org_id
@@ -31,6 +32,6 @@ LEFT JOIN contacts c2 on c2.id = cl2.contact_id
 where 
 ps.product_code = 'BCA' 
 and membership_type_code = 'ADMIN'
-and o.id in ('') -- GRAB FROM PAY query.
+-- and o.id in ('') -- GRAB FROM PAY query. "--This line removed, cause it has syntax error if not specify ids--"
 
 order by 3 desc;
