2fd225ee581efb0d1854ad12e7537be7c26c6b9d
Travis Semple <travis8814@gmail.com>
2023-01-12 12:28:37 -0800

    Unit test to ensure FAS Search component gets rendered correctly.

---
+++ auth-web/tests/unit/components/FasSearchComponent.spec.ts
@@ -0,0 +1,53 @@
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+import i18n from '@/plugins/i18n'
+
+// eslint-disable-next-line sort-imports
+import Search from 'fas-ui'
+
+Vue.use(Vuetify)
+Vue.use(VueI18n)
+
+describe('FasSearchComponent.vue', () => {
+  let wrapper: any
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const store = new Vuex.Store({
+      state: {},
+      strict: false,
+      modules: {}
+    })
+    localVue.use(Search, { store, i18n })
+
+    const vuetify = new Vuetify({})
+
+    var config = {
+      'FAS_WEB_URL': 'https://fas-dev.apps.silver.devops.gov.bc.ca/'
+    }
+
+    sessionStorage.__STORE__['AUTH_API_CONFIG'] = JSON.stringify(config)
+
+    wrapper = mount({ template: `<fas-search-component :isLibraryMode="true"/>` }, {
+      store,
+      i18n,
+      localVue,
+      vuetify
+    })
+
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it('is a Vue instance and has default text', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    // It should display the receipt number, and the no data message at the very least.
+    expect(wrapper.find('.header-receiptNumber')).toBeTruthy()
+    expect(wrapper.find('.no-data')).toBeTruthy()
+  })
+})
