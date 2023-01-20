6d83c86b48596f9b206a33b5334a55cb34264d09
Matthew Cai <matthew.cai@gov.bc.ca>
2023-01-19 16:59:20 -0800

    fixes based on feedback

--- auth-web/tests/unit/components/DeactivateCard.spec.ts
+++ auth-web/tests/unit/components/DeactivateCard.spec.ts
@@ -1,12 +1,11 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 import { Account } from '@/util/constants'
 import DeactivateCard from '@/components/auth/account-deactivate/DeactivateCard.vue'
+import MockI18n from '../test-utils/test-data/MockI18n'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
-// import i18n from '@/plugins/i18n'
-import MockI18n from '../test-utils/test-data/MockI18n'
 
 Vue.use(VueRouter)
 Vue.use(Vuetify)
--- auth-web/tests/unit/test-utils/test-data/MockI18n.ts
+++ auth-web/tests/unit/test-utils/test-data/MockI18n.ts
@@ -1,33 +1,22 @@
-import i18n from '@/plugins/i18n'
-import VueI18n, { LocaleMessages } from 'vue-i18n'
 import { castToVueI18n, createI18n } from 'vue-i18n-bridge'
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 
 export default {
-    mock() {
-        Vue.use(VueI18n, { bridge: true })
+  mock () {
+    Vue.use(VueI18n, { bridge: true })
 
-        function loadLocaleMessages (): LocaleMessages {
-            const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-            const messages: LocaleMessages = {}
-            locales.keys().forEach((key) => {
-              const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-              if (matched && matched.length > 1) {
-                const locale = matched[1]
-                messages[locale] = locales(key)
-              }
-            })
-            return messages
+    const i18n = castToVueI18n(createI18n({
+      legacy: false,
+      locale: 'en',
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          msg: 'Test Message'
         }
+      }
+    }, VueI18n))
 
-        const i18n = castToVueI18n(createI18n({
-            legacy: false,
-            locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-            fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-            messages: loadLocaleMessages()
-          }, VueI18n))
-
-        return i18n
-          
-    }
+    return i18n
+  }
 }
