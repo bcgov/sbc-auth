9b99f08a0c3b43a401e9cab9e3cb92d9478589fc
Matthew Cai <matthew.cai@gov.bc.ca>
2023-02-02 09:22:51 -0800

    fixed linting

--- auth-web/tests/unit/components/EntityDetailsAlert.spec.ts
+++ auth-web/tests/unit/components/EntityDetailsAlert.spec.ts
@@ -1,9 +1,9 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 
-import EntityDetailsAlert from '@/components/auth/manage-business/EntityDetailsAlert.vue'
-import VueCompositionAPI from '@vue/composition-api'
 import { EntityAlertTypes } from '@/util/constants'
+import EntityDetailsAlert from '@/components/auth/manage-business/EntityDetailsAlert.vue'
 import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
@@ -18,26 +18,26 @@ const vuetify = new Vuetify({})
 const router = new VueRouter()
 
 describe('Entity Details Alert tests', () => {
-    let wrapper: any
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
+  let wrapper: any
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
 
-    it('Basic render test', () => {
-        wrapper = mount(EntityDetailsAlert, {
-            vuetify,
-            localVue,
-            router,
-            propsData: {
-                details: [EntityAlertTypes.FROZEN]
-            }
-        })
-
-        expect(wrapper.isVueInstance()).toBeTruthy()
-        expect(wrapper.find(EntityDetailsAlert).exists()).toBeTruthy()
-        expect(wrapper.props('details')).toEqual(expect.arrayContaining([EntityAlertTypes.FROZEN]))
-        expect(wrapper.find('.mdi-alert').exists()).toBeTruthy()
+  it('Basic render test', () => {
+    wrapper = mount(EntityDetailsAlert, {
+      vuetify,
+      localVue,
+      router,
+      propsData: {
+        details: [EntityAlertTypes.FROZEN]
+      }
     })
 
-    // can't test the tooltip renders on mouse over because it is renderred outside the wrapper
-    // can't test the tooltip renderes content because it's rendered outside the wrapper
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.find(EntityDetailsAlert).exists()).toBeTruthy()
+    expect(wrapper.props('details')).toEqual(expect.arrayContaining([EntityAlertTypes.FROZEN]))
+    expect(wrapper.find('.mdi-alert').exists()).toBeTruthy()
+  })
+
+  // can't test the tooltip renders on mouse over because it is renderred outside the wrapper
+  // can't test the tooltip renderes content because it's rendered outside the wrapper
 })
