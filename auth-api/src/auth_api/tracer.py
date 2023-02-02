03893f955fd900513b203b1c3198d33a143db723
Travis Semple <travis8814@gmail.com>
2023-02-02 12:17:48 -0800

    Lint fix.

--- auth-api/src/auth_api/tracer.py
+++ auth-api/src/auth_api/tracer.py
@@ -35,7 +35,8 @@ class Tracer():  # pylint: disable=too-few-public-methods
     def __init__(self):
         """Virtually private constructor."""
         if Tracer.__instance is not None:
-            raise Exception('Attempt made to create multiple tracing instances')  # pylint: disable=line-too-long, broad-exception-raised
+            # pylint: disable=broad-exception-raised
+            raise Exception('Attempt made to create multiple tracing instances')
 
         api_tracer = ApiTracer()
         Tracer.__instance = ApiTracing(api_tracer.tracer)   # pylint: disable=unused-private-member
