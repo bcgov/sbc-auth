98d31c087313c45148368a21b9bbf3bcb9bc5fe9
Travis Semple <travis8814@gmail.com>
2023-03-31 09:13:53 -0700

    14375 - Fix for footer to show application versions. (#2304)
    * Fix for footer to show application versions.
    
    * fix code smell.

--- auth-web/vue.config.js
+++ auth-web/vue.config.js
@@ -1,5 +1,25 @@
 const RemoveServiceWorkerPlugin = require('webpack-remove-serviceworker-plugin')
 var path = require('path')
+const webpack = require('webpack')
+const fs = require('fs')
+const packageJson = fs.readFileSync('./package.json')
+const appName = JSON.parse(packageJson).appName
+const appVersion = JSON.parse(packageJson).version
+const sbcName = JSON.parse(packageJson).sbcName
+const sbcVersion = JSON.parse(packageJson).dependencies['sbc-common-components']
+const aboutText1 = (appName && appVersion) ? `${appName} v${appVersion}` : ''
+const aboutText2 = (sbcName && sbcVersion) ? `${sbcName} v${sbcVersion}` : ''
+const generateAboutText = (aboutText1, aboutText2) => {
+  if (aboutText1 && aboutText2) {
+    return `"${aboutText1}<br>${aboutText2}"`
+  } else if (aboutText1) {
+    return `"${aboutText1}"`
+  } else if (aboutText2) {
+    return `"${aboutText2}"`
+  } else {
+    return ''
+  }
+}
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
@@ -7,7 +27,10 @@ module.exports = {
       // this is needed to remove existing service workers on users' systems
       // ref: https://www.npmjs.com/package/webpack-remove-serviceworker-plugin
       // ref: https://github.com/NekR/self-destroying-sw/tree/master/packages/webpack-remove-serviceworker-plugin
-      new RemoveServiceWorkerPlugin({ filename: 'service-worker.js' })
+      new RemoveServiceWorkerPlugin({ filename: 'service-worker.js' }),
+      new webpack.DefinePlugin({
+        'process.env.ABOUT_TEXT': generateAboutText(aboutText1, aboutText2)
+      })
     ],
     resolve: {
       alias: {
