5f9e1215059c632e1d285be7c77be64a6b8fc645
Jia Xu <Jia.Xu@gov.bc.ca>
2023-04-25 13:04:40 -0700

    fixed some bug from code review

--- auth-web/src/models/affiliation.ts
+++ auth-web/src/models/affiliation.ts
@@ -32,7 +32,6 @@ export interface AffiliationResponse {
   identifier?: string
   draftType?: CorpTypes
   legalType?: CorpTypes
-  state?: string
   businessNumber?: string
   name?: string
   legalName?: string
