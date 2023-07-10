c10483c5527dd814e983edb12ed7dd1165ba16e8
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-07-10 16:33:27 -0700

    Moving mounted to be part of setup(onMounted); consequently methods are moved to setup call too. Updating refs to be a single reactive object componentState.

--- auth-web/src/components/auth/manage-business/AccountAuthorizationRequest.vue
+++ auth-web/src/components/auth/manage-business/AccountAuthorizationRequest.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="pa-5">
-    <div v-if="isLoading" class="loading-inner-container">
-      <v-progress-circular size="50" width="5" color="primary" :indeterminate="isLoading"/>
+    <div v-if="!!componentState.isLoading" class="loading-inner-container">
+      <v-progress-circular size="50" width="5" color="primary" :indeterminate="!!componentState.isLoading"/>
     </div>
 
-    <div v-else-if="!isLoading && accounts && accounts.length > 0">
+    <div v-else-if="!componentState.isLoading && componentState.accounts && componentState.accounts.length > 0">
       <span>Select the account you want to perform Registries activities for <strong>{{ businessName }}</strong></span>
       <v-select
         id="account-authorization-request-request-account-select"
         filled req persistent-hint validate-on-blur
         label="Select authorizing account"
         aria-label="Select Authorizing account"
-        v-model="selectedAccount"
-        :disabled="accounts.length < 2"
+        v-model="componentState.selectedAccount"
+        :disabled="componentState.accounts.length < 2"
         class="business-identifier mb-n2"
-        :items="accounts"
+        :items="componentState.accounts"
         item-text="name"
         item-value="uuid"
-        @change="emitSelected(selectedAccount)"
+        @change="emitSelected(componentState.selectedAccount)"
       />
       <span>You can add a message that will be included as part of your authorization request. </span>
       <v-textarea
@@ -26,8 +26,8 @@
         id="account-authorization-request-additional-message-textarea"
         label="Request access additional message"
         aria-label="Request access additional message"
-        v-model.trim="requestAccessMessage"
-        @change="$emit('changeRequestAccessMessage', requestAccessMessage)"
+        v-model.trim="componentState.requestAccessMessage"
+        @change="$emit('changeRequestAccessMessage', componentState.requestAccessMessage)"
         maxlength="400"
         counter="400"
       />
@@ -39,22 +39,12 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, getCurrentInstance, onMounted, reactive, ref } from '@vue/composition-api'
 import OrgService from '@/services/org.services'
 
 export default defineComponent({
   name: 'AccountAuthorizationRequest',
   emits: ['changeRequestAccessMessage', 'selectAccount'],
-  methods: {
-    emitSelected: function (uuid) {
-      const selectedAcc = this.accounts.find(acc => acc.uuid === uuid)
-      this.emitSelectedAccount(selectedAcc)
-    },
-    emitSelectedAccount: function (selectedAcc) {
-      this.$emit('selectAccount', selectedAcc)
-    }
-
-  },
   props: {
     businessName: {
       type: String,
@@ -65,41 +55,44 @@ export default defineComponent({
       required: true
     }
   },
-  setup () {
-    const accounts = ref([])
-    const selectedAccount = ref(null)
-    const requestAccessMessage = ref('')
-    const isLoading = ref(true)
+  setup (props, { emit }) {
+    const componentState = reactive({
+      accounts: [],
+      selectedAccount: { },
+      requestAccessMessage: '',
+      isLoading: true
+    })
+
+    const emitSelectedAccount = (selectedAcc) => {
+      emit('selectAccount', selectedAcc)
+    }
+
+    const emitSelected = (uuid) => {
+      const selectedAcc = componentState.accounts.find(acc => acc.uuid === uuid)
+
+      emitSelectedAccount(selectedAcc)
+    }
+
+    onMounted(async () => {
+      const response = await OrgService.getOrganizationsNameAndUuidByAffiliation(props.businessIdentifier)
+      if (response.data && response.data.orgsDetails) {
+        componentState.accounts = response.data.orgsDetails
+
+        if (componentState.accounts.length === 1) {
+          Object.assign(componentState.selectedAccount, componentState.accounts[0])
+          emitSelectedAccount(componentState.selectedAccount)
+        }
+      } else {
+        console.error('OrgService.getOrganizationsNameAndUuidByAffiliation', response.status)
+      }
+      componentState.isLoading = false
+    })
 
     return {
-      selectedAccount,
-      requestAccessMessage,
+      emitSelected,
 
-      accounts,
-      isLoading
+      componentState
     }
-  },
-  mounted () {
-    OrgService.getOrganizationsNameAndUuidByAffiliation(this.businessIdentifier).then(
-      response => {
-        if (response.data && response.data.orgsDetails) {
-          this.accounts = response.data.orgsDetails
-
-          if (this.accounts.length === 1) {
-            this.selectedAccount = this.accounts[0]
-            this.emitSelectedAccount(this.selectedAccount)
-          }
-        }
-      }
-    ).catch(
-      err => {
-        console.log(err)
-      }
-    ).finally(
-      () => {
-        this.isLoading = false
-      }
-    )
   }
 })
 </script>
