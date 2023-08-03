b8e6c193eb9c9104290b9cf68dec36467a3fbb11
Rodrigo Barraza <rodrigo.barraza@gov.bc.ca>
2023-08-03 11:10:37 -0700

    Expanding test cases

--- auth-web/tests/unit/components/ManageBusinessDialog.spec.ts
+++ auth-web/tests/unit/components/ManageBusinessDialog.spec.ts
@@ -1,5 +1,6 @@
 import { Wrapper, createLocalVue, shallowMount } from '@vue/test-utils'
 import ManageBusinessDialog from '@/components/auth/manage-business/ManageBusinessDialog.vue'
+import HelpDialog from '@/components/auth/common/HelpDialog.vue'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
@@ -11,14 +12,51 @@ const vuetify = new Vuetify({})
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-const tests = [
+const testCaseList = [
   {
-    description: 'Should render Manage Business Dialog component'
+    description: 'Should render for a BC account',
+    businessIdentifier: 'BC0000000',
+    passcodeInputLabel: 'Password',
+    certifyExists: false,
+    forgotButtonLabel: 'Help',
+    isGovStaffAccount: false
+  },
+  {
+    description: 'Should render for a CP account',
+    businessIdentifier: 'CP0000000',
+    passcodeInputLabel: 'Passcode',
+    certifyExists: false,
+    forgotButtonLabel: 'Help',
+    isGovStaffAccount: false
+  },
+  {
+    description: 'Should render for a FM Client account',
+    businessIdentifier: 'FM0000000',
+    passcodeInputLabel: 'Proprietor or Partner Name (e.g., Last Name, First Name Middlename)',
+    certifyExists: true,
+    forgotButtonLabel: null,
+    isGovStaffAccount: false
+  },
+  {
+    description: 'Should render for a FM Staff account',
+    businessIdentifier: 'FM0000000',
+    passcodeInputLabel: 'Proprietor or Partner Name (e.g., Last Name, First Name Middlename)',
+    certifyExists: true,
+    forgotButtonLabel: null,
+    isGovStaffAccount: false
+  },
+  {
+    description: 'Should render for a FM SBC Staff account',
+    businessIdentifier: 'FM0000000',
+    passcodeInputLabel: 'Proprietor or Partner Name (e.g., Last Name, First Name Middlename)',
+    certifyExists: true,
+    forgotButtonLabel: null,
+    isGovStaffAccount: false
   }
 ]
 
-tests.forEach(test => {
-  describe('Manage Business Form', () => {
+testCaseList.forEach(testCase => {
+  describe('ManageBusinessDialog Component', () => {
     let wrapper: Wrapper<any>
 
     beforeAll(() => {
@@ -30,11 +68,9 @@ tests.forEach(test => {
           }
         }
       }
-
       const businessModule = {
         namespaced: true,
         state: {
-
         },
         action: {
           addBusiness: jest.fn(),
@@ -55,7 +91,8 @@ tests.forEach(test => {
         store,
         vuetify,
         propsData: {
-          dialog: true
+          dialog: true,
+          isGovStaffAccount: testCase.isGovStaffAccount
         }
       })
     })
@@ -64,14 +101,32 @@ tests.forEach(test => {
       wrapper.destroy()
     })
 
-    it(test.description, async () => {
+    it(testCase.description, async () => {
       wrapper.setData({
+        businessIdentifier: testCase.businessIdentifier,
         businessName: 'My Business Inc'
       })
       await flushPromises()
-
+      // parent component
       expect(wrapper.attributes('id')).toBe('manage-business-dialog')
       expect(wrapper.find('#manage-business-dialog').isVisible()).toBe(true)
+      expect(wrapper.findComponent(HelpDialog).exists()).toBe(true)
+      // input components
+      expect(wrapper.find('.passcode').attributes('label')).toBe(testCase.passcodeInputLabel)
+      expect(wrapper.find('.certify').exists()).toBe(testCase.certifyExists)
+      expect(wrapper.find('.authorization').exists()).toBe(testCase.isGovStaffAccount)
+      if (testCase.isGovStaffAccount) {
+        expect(wrapper.find('.authorization').attributes('label')).toContain('Legal name of Authorized Person')
+      }
+      // button components
+      expect(wrapper.find('#cancel-button span').text()).toBe('Cancel')
+      expect(wrapper.find('#add-button span').text()).toBe('Manage This Business')
+      if (!testCase.certifyExists) {
+        expect(wrapper.find('#forgot-button').exists()).toBe(!!testCase.forgotButtonLabel)
+      }
+      if (testCase.forgotButtonLabel) {
+        expect(wrapper.find('#forgot-button span').text()).toBe(testCase.forgotButtonLabel)
+      }
     })
   })
 })
