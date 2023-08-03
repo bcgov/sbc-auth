43c2fb5f65b7aff0464965db02d436ee464dd529
Jia Xu <Jia.Xu@gov.bc.ca>
2023-08-03 15:34:02 -0700

    15772 - Name Request : Affiliation process using application information - no magic link (#2419)

--- auth-web/tests/unit/components/AddNameRequestForm.spec.ts
+++ auth-web/tests/unit/components/AddNameRequestForm.spec.ts
@@ -29,9 +29,6 @@ describe('Add Name Request Form', () => {
     }
 
     wrapper = wrapperFactory()
-    wrapper.vm.applicantPhoneNumber = '250-123-4567'
-    wrapper.vm.applicantEmail = '123@test.com'
-    wrapper.vm.nrNumber = 'NR 1234567'
   })
 
   it('renders the component properly', () => {
@@ -46,7 +43,6 @@ describe('Add Name Request Form', () => {
     expect(wrapper.findComponent(HelpDialog).exists()).toBe(true)
 
     // verify input fields
-    expect(wrapper.find('[data-test="nr-number"]').exists()).toBe(true)
     expect(wrapper.find('[data-test="applicant-phone-number"]').exists()).toBe(true)
     expect(wrapper.find('[data-test="applicant-email"]').exists()).toBe(true)
 
@@ -59,56 +55,39 @@ describe('Add Name Request Form', () => {
     wrapper.destroy()
   })
 
-  // Name Request number validation
-  it('verifies nr input valid', async () => {
-    wrapper.vm.nrNumber = 'NR 1234567'
+  // Applicant phone number validation
+  it('verifies phone input valid', async () => {
+    wrapper.vm.applicantPhoneNumber = '250-123-4567'
+    wrapper.vm.applicantEmail = ''
     await flushPromises()
 
-    expect(wrapper.vm.nrNumber).toEqual('NR 1234567')
-    expect(wrapper.vm.isFormValid()).toBe(true)
+    expect(wrapper.vm.applicantPhoneNumber).toEqual('250-123-4567')
+    expect(wrapper.vm.applicantPhoneNumberRules.every(rule => rule(wrapper.vm.applicantPhoneNumber) === true)).toBe(true)
   })
 
-  it('verifies nr input valid', async () => {
-    wrapper.vm.nrNumber = '1234567'
+  it('verifies phone input invalid', async () => {
+    wrapper.vm.applicantPhoneNumber = '250-123-456-789'
+    wrapper.vm.applicantEmail = ''
     await flushPromises()
 
-    wrapper.find('[data-test="nr-number"]').trigger('input')
-
-    expect(wrapper.vm.nrNumber).toEqual('NR 1234567')
-    expect(wrapper.vm.isFormValid()).toBe(true)
+    expect(wrapper.vm.applicantPhoneNumberRules.some(rule => rule(wrapper.vm.applicantPhoneNumber) !== true)).toBe(true)
   })
 
-  it('verifies nr input valid', async () => {
-    wrapper.vm.nrNumber = 'NR    1234567'
+  // Applicant email validation
+  it('verifies email input valid', async () => {
+    wrapper.vm.applicantPhoneNumber = ''
+    wrapper.vm.applicantEmail = '123@test.com'
     await flushPromises()
 
-    wrapper.find('[data-test="nr-number"]').trigger('input')
-
-    expect(wrapper.vm.nrNumber).toEqual('NR 1234567')
-    expect(wrapper.vm.isFormValid()).toBe(true)
+    expect(wrapper.vm.applicantEmail).toEqual('123@test.com')
+    expect(wrapper.vm.applicantEmailRules.every(rule => rule(wrapper.vm.applicantEmail) === true)).toBe(true)
   })
 
-  it('verifies nr input valid', async () => {
-    wrapper.vm.nrNumber = 'NR1234567'
+  it('verifies email input invalid', async () => {
+    wrapper.vm.applicantPhoneNumber = ''
+    wrapper.vm.applicantEmail = '123@test'
     await flushPromises()
 
-    wrapper.find('[data-test="nr-number"]').trigger('input')
-
-    expect(wrapper.vm.nrNumber).toEqual('NR 1234567')
-    expect(wrapper.vm.isFormValid()).toBe(true)
-  })
-
-  it('verifies nr input invalid', async () => {
-    wrapper.vm.nrNumber = 'NR 123456'
-    await flushPromises()
-
-    expect(wrapper.vm.isFormValid()).toBe(false)
-  })
-
-  it('verifies nr input invalid', async () => {
-    wrapper.vm.nrNumber = '   NR 1234567'
-    await flushPromises()
-
-    expect(wrapper.vm.isFormValid()).toBe(false)
+    expect(wrapper.vm.applicantEmailRules.some(rule => rule(wrapper.vm.applicantEmail) !== true)).toBe(true)
   })
 })
