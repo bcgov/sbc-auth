23f91f256bd0ee699d5ffded952d603be4d209ca
Travis Semple <travis8814@gmail.com>
2023-08-16 13:51:29 -0700

    More lint fixes.

--- auth-web/.eslintrc.js
+++ auth-web/.eslintrc.js
@@ -17,7 +17,10 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': ['error', { 'functions': false, 'classes': true, 'variables': true }],
     'no-unused-expressions': 'off',
     '@typescript-eslint/no-unused-expressions': ['error', { 'allowShortCircuit': true, 'allowTernary': true }],
-    'vue/multi-word-component-names': ['error', { 'ignores': ['Transactions'] }]
+    'vue/multi-word-component-names': ['error', { 'ignores': ['Transactions'] }],
+    // Not ideal but shallowOnly option isn't working for this, so leaving it off for now.
+    // https://eslint.vuejs.org/rules/no-mutating-props.html
+    'vue/no-mutating-props': 'off'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
