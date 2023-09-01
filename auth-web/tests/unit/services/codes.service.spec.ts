4035a8a29705d34a83d6b39ed41bd01461e3381f
Travis Semple <travis8814@gmail.com>
2023-08-31 22:06:38 -0700

    Fix service tests so they're more consistent.

--- auth-web/tests/unit/services/codes.service.spec.ts
+++ auth-web/tests/unit/services/codes.service.spec.ts
@@ -6,21 +6,22 @@ const mockob = {
   'AUTH_API_URL': 'https://auth-api-dev.apps.silver.devops.gov.bc.ca/api/v1'
 }
 
-describe('Codes service', () => {
-  beforeAll(() => {
-    sessionStorage['AUTH_API_CONFIG'] = JSON.stringify(mockob)
-    vi.clearAllMocks()
-  })
+const testCodeSuspensionReasons: Code[] = [{
+  code: 'testSuspensionCode',
+  desc: 'testSuspensionDesc',
+  default: true
+}]
 
-  it('call getCodes() for suspended_reason_codes ', () => {
-    const testCodeSuspensionReasons: Code[] = [{
-      code: 'testSuspensionCode',
-      desc: 'testSuspensionDesc',
-      default: true
-    }]
+const mocks = vi.hoisted(() => ({
+  get: vi.fn().mockReturnValue({ data: testCodeSuspensionReasons })
+}))
+
+describe('Codes service', () => {
+  beforeEach(() => {
+    sessionStorage['AUTH_API_CONFIG'] = JSON.stringify(mockob)
     vi.doMock('axios', () => {
       return {
-        get: vi.fn().mockReturnValue({ data: testCodeSuspensionReasons }),
+        get: mocks.get,
         interceptors: {
           request: {
             use: vi.fn(),
@@ -33,6 +34,9 @@ describe('Codes service', () => {
         }
       }
     })
+  })
+
+  it('call getCodes() for suspended_reason_codes ', () => {
     CodesService.getCodes('suspension-reason-codes').then((response) => {
       expect(response).toEqual(testCodeSuspensionReasons)
     })
