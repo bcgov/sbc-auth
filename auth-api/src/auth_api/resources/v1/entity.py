5c4c01d5dc22fb99e8bea7c48e0448883470c3dc
Travis Semple <travis8814@gmail.com>
2023-09-12 13:53:31 -0700

    Use False instead of 'f' (#2534)

--- auth-api/src/auth_api/resources/v1/entity.py
+++ auth-api/src/auth_api/resources/v1/entity.py
@@ -139,7 +139,7 @@ def get_entity_authentication(business_identifier):
     # It's used by the business dashboard for magic link.
     if ((entity := EntityService.find_by_business_identifier(business_identifier, skip_auth=True)) and
             (contact := entity.get_contact())):
-        has_valid_pass_code = (entity.pass_code_claimed == 'f' and entity.pass_code is not None) or \
+        has_valid_pass_code = (entity.pass_code_claimed is False and entity.pass_code is not None) or \
             entity.corp_type in ['SP', 'GP']
         return {
                 'contactEmail': mask_email(contact.email),
