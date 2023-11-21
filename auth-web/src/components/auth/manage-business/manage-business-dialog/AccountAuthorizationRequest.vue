a93a8e53bfdfdd5f6e4b63b998b150d4c141feb1
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-11-21 13:19:53 -0800

    18522, 18525 - updates to REQUEST affiliation (#2642)
    * 18522, 18525 - backend changes for filtering and branch name in fetching org details for affiliated orgs.
    
    * 18522 - UI updates.
    
    * fixed tests.
    
    * npm run lint + lint fixes.
    
    * Parametrizing the service.

--- auth-web/src/components/auth/manage-business/manage-business-dialog/AccountAuthorizationRequest.vue
+++ auth-web/src/components/auth/manage-business/manage-business-dialog/AccountAuthorizationRequest.vue
@@ -26,10 +26,31 @@
         :disabled="accounts.length < 2"
         class="business-identifier mb-n2"
         :items="accounts"
-        item-text="name"
         item-value="uuid"
         @change="emitSelected"
-      />
+      >
+        <template #selection="data">
+          <!-- HTML that describe how select should render selected items -->
+          <span
+            v-if="data.item.branchName"
+            data-test="account-authorization-request-selection"
+          >{{ data.item.name }} - {{ data.item.branchName }}</span>
+          <span
+            v-else
+            data-test="account-authorization-request-selection"
+          >{{ data.item.name }}</span>
+        </template>
+        <template #item="data">
+          <span
+            v-if="data.item.branchName"
+            data-test="account-authorization-request-option"
+          >{{ data.item.name }} - {{ data.item.branchName }}</span>
+          <span
+            v-else
+            data-test="account-authorization-request-option"
+          >{{ data.item.name }}</span>
+        </template>
+      </v-select>
       <span>You can add a message that will be included as part of your authorization request. </span>
       <v-textarea
         id="account-authorization-request-additional-message-textarea"
