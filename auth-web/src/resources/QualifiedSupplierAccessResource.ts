6f666aeb4dd788f5f431e3700b80845064277643
Cameron Bowler <cameron.bowler@ca.ey.com>
2024-01-19 08:42:32 -0800

    Support for MF and HD QS Applicant Reviews

--- auth-web/src/resources/QualifiedSupplierAccessResource.ts
+++ auth-web/src/resources/QualifiedSupplierAccessResource.ts
@@ -14,12 +14,31 @@ export const userAccessRequirements: Partial<Record<TaskType, QualifiedSupplierR
        forms), will be provided within 7 business days, at the fee level set by the Registrar.`
     }
   ],
-  [TaskType.MHR_MANUFACTURERS]: [],
-  [TaskType.MHR_DEALERS]: []
+  [TaskType.MHR_MANUFACTURERS]: [
+    {
+      boldText: `Have comprehensive general liability insurance `,
+      regularText: `equal to or greater than $2,000,000.00.`
+    },
+    {
+      boldText: `Manufactured homes built are CSA approved `,
+      regularText: `(Z240 or A277).`
+    },
+    {
+      boldText: `All filed documents will be stored for 7 years. `,
+      regularText: `If requested, a copy or certified copy of filed documents (such as the Bill of Sale, or other 
+        signed forms), will be provided within 7 business days, at the fee level set by the Registrar. `
+    }
+  ],
+  [TaskType.MHR_DEALERS]: [
+    {
+      boldText: `Have comprehensive general liability insurance `,
+      regularText: `equal to or greater than $2,000,000.00.`
+    }
+  ]
 }
 
 export const userAccessDisplayNames: Partial<Record<TaskType, string>> = {
   [TaskType.MHR_LAWYER_NOTARY]: 'Lawyers and Notaries',
-  [TaskType.MHR_MANUFACTURERS]: 'Manufacturers',
+  [TaskType.MHR_MANUFACTURERS]: 'Home Manufacturers',
   [TaskType.MHR_DEALERS]: 'Home Dealers'
 }
