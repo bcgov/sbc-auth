afcdc32ea8f8b2beef68ca5637606ada54a92aca
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2024-01-22 07:56:05 -0800

    Support for MF and HD QS Applicant Reviews (#2687)

--- auth-web/src/components/auth/staff/review-task/QsApplication.vue
+++ auth-web/src/components/auth/staff/review-task/QsApplication.vue
@@ -24,46 +24,76 @@
     </v-row>
 
     <!-- QS Applicant Info -->
-    <v-row>
-      <v-col class="cols-12 mt-5 py-2">
-        <h3>Qualified Supplier ({{ qsApplicationTypeDisplay }}) Information</h3>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="cols-12 col-sm-3 py-2 pr-2">
-        Qualified Supplier Name
-      </v-col>
-      <v-col
-        class="py-2"
-        data-test="qs-org-name"
+    <template v-if="qsApplicantData">
+      <v-row>
+        <v-col class="cols-12 mt-5 py-2">
+          <h3>Qualified Supplier ({{ qsApplicationTypeDisplay }}) Information</h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="cols-12 col-sm-3 py-2 pr-2">
+          Qualified Supplier Name
+        </v-col>
+        <v-col
+          class="py-2"
+          data-test="qs-org-name"
+        >
+          {{ qsApplicantData.businessName }}
+        </v-col>
+      </v-row>
+      <v-row v-if="!isLawyerNotaryApplication">
+        <v-col class="cols-12 col-sm-3 py-2 pr-2">
+          DBA / Operating Name
+        </v-col>
+        <v-col
+          class="py-2"
+          data-test="qs-dba-name"
+        >
+          {{ qsApplicantData.dbaName || '(Not Entered)' }}
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="cols-12 col-sm-3 py-2">
+          Phone Number
+        </v-col>
+        <v-col
+          class="py-2"
+          data-test="qs-phone"
+        >
+          {{ qsApplicantPhone }}
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="cols-12 col-sm-3 py-2">
+          Mailing Address
+        </v-col>
+        <v-col class="py-2">
+          <BaseAddressForm
+            v-if="qsApplicantData"
+            :schema="null"
+            :editing="false"
+            :address="formatAddress(qsApplicantData.address)"
+          />
+        </v-col>
+      </v-row>
+      <v-row
+        v-if="isManufacturerApplication"
+        data-test="qs-mf-location-row"
       >
-        {{ qsApplicantData && qsApplicantData.businessName }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="cols-12 col-sm-3 py-2">
-        Phone Number
-      </v-col>
-      <v-col
-        class="py-2"
-        data-test="qs-phone"
-      >
-        {{ qsApplicantData && qsApplicantPhone }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="cols-12 col-sm-3 py-2">
-        Mailing Address
-      </v-col>
-      <v-col class="py-2">
-        <BaseAddressForm
-          v-if="qsApplicantData"
-          :schema="null"
-          :editing="false"
-          :address="formatAddress(qsApplicantData.address)"
-        />
-      </v-col>
-    </v-row>
+        <v-col class="cols-12 col-sm-3 py-2">
+          Location of the Manufactured Home(s)
+        </v-col>
+        <v-col class="py-2">
+          <BaseAddressForm
+            v-if="qsApplicantData.mfLocation"
+            :schema="null"
+            :editing="false"
+            :address="formatAddress(qsApplicantData.mfLocation)"
+            data-test="qs-mf-location"
+          />
+        </v-col>
+      </v-row>
+    </template>
 
     <!-- Submitting Party Info -->
     <v-row>
@@ -147,7 +177,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="cols-12 py-2 icon-text">
+      <v-col class="cols-12 py-0 icon-text">
         <v-icon
           class="pr-2"
           color="success"
@@ -166,8 +196,10 @@
     </v-row>
     <v-row>
       <v-col class="py-2">
-        Legal name of the person authorized to complete and submit this application. <b>Note:</b> The authorized person
-        must be an active B.C. lawyer or notary in good standing.
+        Legal name of the person authorized to complete and submit this application.
+        <span v-if="isLawyerNotaryApplication">
+          <b>Note:</b> The authorized person must be an active B.C. lawyer or notary in good standing.
+        </span>
       </v-col>
     </v-row>
     <v-row>
@@ -199,7 +231,11 @@
 </template>
 
 <script lang="ts">
-import { QualifiedSupplierApplicant, QualifiedSupplierRequirementsConfig } from '@/models/external'
+import {
+  MhrManufacturerInfoIF,
+  QualifiedSupplierApplicant,
+  QualifiedSupplierRequirementsConfig
+} from '@/models/external'
 import { computed, defineComponent, onMounted, reactive, toRefs } from '@vue/composition-api'
 import { userAccessDisplayNames, userAccessRequirements } from '@/resources/QualifiedSupplierAccessResource'
 import { Address } from '@/models/address'
@@ -209,6 +245,7 @@ import { Contact } from '@/models/contact'
 import { Organization } from '@/models/Organization'
 import { Task } from '@/models/Task'
 import TaskService from '@/services/task.services'
+import { TaskType } from '@/util/constants'
 import { User } from '@/models/user'
 import moment from 'moment/moment'
 
@@ -236,22 +273,58 @@ export default defineComponent({
       }),
       qsRequirements: computed((): QualifiedSupplierRequirementsConfig[] => {
         return userAccessRequirements[props.taskDetails?.type]
-      })
+      }),
+      isLawyerNotaryApplication: computed((): boolean => props.taskDetails?.type === TaskType.MHR_LAWYER_NOTARY),
+      isManufacturerApplication: computed((): boolean => props.taskDetails?.type === TaskType.MHR_MANUFACTURERS)
     })
 
+    /**
+     * Converts manufacturer data to a qualified supplier applicant.
+     * @param {MhrManufacturerInfoIF} mfData - The manufacturer data to convert.
+     * @returns {QualifiedSupplierApplicant} The converted qualified supplier applicant.
+     */
+    const formatManufacturer = (mfData: MhrManufacturerInfoIF): QualifiedSupplierApplicant => {
+      // Always 1 group with 1 owner in Manufacturer Owner record
+      const mfOwner = mfData.ownerGroups[0].owners[0]
+      return {
+        address: mfOwner.address,
+        authorizationName: mfData.authorizationName,
+        businessName: mfOwner.organizationName,
+        dbaName: mfData.dbaName,
+        phoneNumber: mfOwner.phoneNumber,
+        termsAccepted: mfData.termsAccepted,
+        mfLocation: mfData.location.address
+      }
+    }
+
+    /** On Mount: Fetch and parse Qualified Supplier applicant into local model. */
     onMounted(async () => {
-      await TaskService.getQsApplicantForTaskReview(props.accountUnderReview.id).then(response => {
+      // Fetch Qualified Suppler application data
+      await TaskService.getQsApplicantForTaskReview(props.accountUnderReview.id, props.taskDetails?.type).then(response => {
         if (!response?.data) {
           throw new Error('Invalid API response')
         }
-        localState.qsApplicantData = response.data
+        // Format response to fit QS model when Manufacturer
+        localState.qsApplicantData = localState.isManufacturerApplication
+          ? formatManufacturer(response.data)
+          : response.data
       }).catch(error => { console.error(error) })
     })
 
+    /**
+     * Formats the given date into a string representation.
+     * @param {Date} date - The date object to format.
+     * @returns {string} The formatted date string.
+     */
     const formatDate = (date: Date): string => {
       return moment(date).format('MMM DD, YYYY')
     }
 
+    /**
+     * Formats the address by converting the city and street values to lowercase.
+     * @param {Address} address - The address object to be formatted.
+     * @returns {Address} - The formatted address object.
+     */
     const formatAddress = (address: Address): Address => {
       address && Object.keys(address).forEach(key => {
         if (['city', 'street'].includes(key)) {
--- auth-web/tests/unit/components/QsApplication.spec.ts
+++ auth-web/tests/unit/components/QsApplication.spec.ts
@@ -2,13 +2,14 @@ import { createLocalVue, shallowMount } from '@vue/test-utils'
 import QsApplication from '@/components/auth/staff/review-task/QsApplication.vue'
 import { TaskType } from '@/util/constants'
 import Vuetify from 'vuetify'
+import { userAccessDisplayNames } from '@/resources/QualifiedSupplierAccessResource'
 
 // Mocked props
 const mockProps = {
   tabNumber: 1,
   title: 'Test Application',
   taskDetails: {
-    type: TaskType.MHR_LAWYER_NOTARY,
+    type: null,
     created: new Date()
   },
   accountUnderReview: {
@@ -26,71 +27,85 @@ const mockProps = {
     phone: '123-456-7890'
   }
 }
-
 const mockQsApplicantData = {
   businessName: 'Test Business',
   phoneNumber: '987-654-3210',
   authorizationName: 'Authorized Person',
-  address: {
-  }
+  dbaName: 'DBA Test Business',
+  address: {}
 }
+const taskTypes: TaskType[] = [TaskType.MHR_LAWYER_NOTARY, TaskType.MHR_MANUFACTURERS, TaskType.MHR_DEALERS]
 
-describe('QsApplication.vue', () => {
-  let wrapper
-  const localVue = createLocalVue()
-  const vuetify = new Vuetify({})
+for (const taskType of taskTypes) {
+  describe(`QsApplication.vue: ${taskType}`, () => {
+    let wrapper
+    const localVue = createLocalVue()
+    const vuetify = new Vuetify({})
 
-  beforeEach(() => {
-    wrapper = shallowMount(QsApplication, {
-      localVue,
-      vuetify,
-      propsData: mockProps
+    beforeEach(() => {
+      wrapper = shallowMount(QsApplication, {
+        localVue,
+        vuetify,
+        propsData: { ...mockProps, taskDetails: { type: taskType } }
+      })
+
+      // Set the mock qsApplicantData
+      wrapper.vm.qsApplicantData = mockQsApplicantData
     })
 
-    // Set the mock qsApplicantData
-    wrapper.vm.qsApplicantData = mockQsApplicantData
-  })
+    afterEach(() => {
+      wrapper.destroy()
+    })
 
-  afterEach(() => {
-    wrapper.destroy()
-  })
+    it('renders the component', () => {
+      expect(wrapper.exists()).toBe(true)
+    })
 
-  it('renders the component', () => {
-    expect(wrapper.exists()).toBe(true)
-  })
+    it('displays the correct tabNumber in the title', () => {
+      const title = wrapper.find('h2').text()
+      expect(title).toContain('1. Test Application')
+    })
 
-  it('displays the correct tabNumber in the title', () => {
-    const title = wrapper.find('h2').text()
-    expect(title).toContain('1. Test Application')
-  })
+    it('displays the correct title', () => {
+      const title = wrapper.find('h2').text()
+      expect(title).toContain('Test Application')
+    })
 
-  it('displays the correct title', () => {
-    const title = wrapper.find('h2').text()
-    expect(title).toContain('Test Application')
-  })
+    it('displays the correct task type', () => {
+      const taskTypeContent = wrapper.findAll('h3').at(1).text()
+      expect(taskTypeContent).toBe(`Qualified Supplier (${userAccessDisplayNames[taskType]}) Information`)
+    })
 
-  it('displays the correct task type', () => {
-    const taskType = wrapper.findAll('h3').at(1).text()
-    expect(taskType).toBe('Qualified Supplier (Lawyers and Notaries) Information')
-  })
+    it('displays the correct applicant organization name', () => {
+      const orgName = wrapper.find('[data-test="qs-org-name"]').text()
+      expect(orgName).toContain('Test Business')
+    })
 
-  it('displays the correct applicant organization name', () => {
-    const orgName = wrapper.find('[data-test="qs-org-name"]').text()
-    expect(orgName).toContain('Test Business')
-  })
+    it.runIf([TaskType.MHR_MANUFACTURERS, TaskType.MHR_DEALERS].includes(taskType))(
+      'displays the correct applicant Dba name when Dealers or Manufacturers', () => {
+        const dbaName = wrapper.find('[data-test="qs-dba-name"]').text()
+        expect(dbaName).toContain('DBA Test Business')
+      })
 
-  it('displays the correct admin full name', () => {
-    const adminName = wrapper.find('[data-test="qs-username"]').text()
-    expect(adminName).toContain('John Doe')
-  })
+    it('displays the correct admin full name', () => {
+      const adminName = wrapper.find('[data-test="qs-username"]').text()
+      expect(adminName).toContain('John Doe')
+    })
 
-  it('displays the correct admin contact email', () => {
-    const adminEmail = wrapper.find('[data-test="qs-email"]').text()
-    expect(adminEmail).toContain('john.doe@example.com')
-  })
+    it('displays the correct admin contact email', () => {
+      const adminEmail = wrapper.find('[data-test="qs-email"]').text()
+      expect(adminEmail).toContain('john.doe@example.com')
+    })
 
-  it('displays the correct applicant contact phone number', () => {
-    const adminPhone = wrapper.find('[data-test="qs-phone"]').text()
-    expect(adminPhone).toContain('(987) 654-3210')
+    it('displays the correct applicant contact phone number', () => {
+      const adminPhone = wrapper.find('[data-test="qs-phone"]').text()
+      expect(adminPhone).toContain('(987) 654-3210')
+    })
+
+    it.runIf([TaskType.MHR_MANUFACTURERS].includes(taskType))(
+      'displays the applicant Location when Manufacturer', () => {
+        const mfLocationRow = wrapper.find('[data-test="qs-mf-location-row"]')
+        expect(mfLocationRow.exists()).toBe(true)
+      })
   })
-})
+}
