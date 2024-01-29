0236c8431c22f3be41c5cdb21df3e8febaaca68b
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2024-01-29 11:34:29 -0800

    17572/17565 - SubProduct Application Status Messaging (#2694)
    * Product Application Status Messaging
    
    * ProductPackage refactor to comp api

--- auth-web/src/components/auth/account-settings/product/ProductPackage.vue
+++ auth-web/src/components/auth/account-settings/product/ProductPackage.vue
@@ -39,6 +39,7 @@
           <Product
             v-if="!product.parentCode"
             :productDetails="product"
+            :activeSubProduct="subProduct"
             :userName="currentUser.fullName"
             :orgName="currentOrganization.name"
             :isexpandedView="product.code === expandedProductCode"
@@ -53,7 +54,28 @@
             @set-selected-product="setSelectedProduct"
             @toggle-product-details="toggleProductDetails"
             @save:saveProductFee="saveProductFee"
-          />
+          >
+            <!-- Show product status message content for pending or rejected sub-product applications -->
+            <template
+              v-if="product.code === ProductEnum.MHR && mhrSubProductMsgContent()"
+              #productContentSlot
+            >
+              <CautionBox
+                setImportantWord="Note"
+                :setAlert="mhrSubProductMsgContent().status === ProductStatus.REJECTED"
+                :setMsg="mhrSubProductMsgContent().msg"
+              >
+                <template #prependSLot>
+                  <v-icon
+                    class="mr-2"
+                    :color="mhrSubProductMsgContent().color"
+                  >
+                    {{ mhrSubProductMsgContent().icon }}
+                  </v-icon>
+                </template>
+              </CautionBox>
+            </template>
+          </Product>
         </div>
         <div class="align-right-container">
           <p
@@ -115,183 +137,261 @@
 </template>
 
 <script lang="ts">
-import { AccessType, Account, ProductStatus, Role } from '@/util/constants'
-import { AccountFee, OrgProduct, OrgProductCode, OrgProductFeeCode, OrgProductsRequestBody, Organization } from '@/models/Organization'
-import { Action, State } from 'pinia-class'
-import { Component, Mixins } from 'vue-property-decorator'
-import AccountChangeMixin from '@/components/auth/mixins/AccountChangeMixin.vue'
-import { KCUserProfile } from 'sbc-common-components/src/models/KCUserProfile'
+import {
+  AccessType,
+  Account,
+  AccountStatus,
+  Product as ProductEnum,
+  ProductStatus,
+  Role,
+  TaskType
+} from '@/util/constants'
+import {
+  OrgProduct,
+  OrgProductCode,
+  OrgProductsRequestBody
+} from '@/models/Organization'
+import { computed, defineComponent, onMounted, reactive, ref, toRefs } from '@vue/composition-api'
+import CautionBox from '@/components/auth/common/CautionBox.vue'
 import ModalDialog from '@/components/auth/common/ModalDialog.vue'
 import Product from '@/components/auth/common/Product.vue'
+import { ProductStatusMsgContentIF } from '@/models/external'
+import { storeToRefs } from 'pinia'
+import { useAccountChangeHandler } from '@/composables'
 import { useOrgStore } from '@/stores/org'
 import { useUserStore } from '@/stores/user'
+import { userAccessDisplayNames } from '@/resources/QualifiedSupplierAccessResource'
 
-@Component({
+export default defineComponent({
+  name: 'ProductPackage',
   components: {
+    CautionBox,
     Product,
     ModalDialog
+  },
+  setup () {
+    const confirmDialog: InstanceType<typeof ModalDialog> = ref(null)
+
+    const { currentUser } = useUserStore()
+    const { setAccountChangedHandler } = useAccountChangeHandler()
+    const {
+      resetCurrentSelectedProducts,
+      getOrgProducts,
+      addOrgProducts,
+      addToCurrentSelectedProducts,
+      syncCurrentAccountFees,
+      fetchOrgProductFeeCodes,
+      updateAccountFees
+    } = useOrgStore()
+
+    const {
+      currentOrganization,
+      productList,
+      currentSelectedProducts
+    } = storeToRefs(useOrgStore())
+
+    const localState = reactive({
+      isBtnSaved: false,
+      disableSaveBtn: false,
+      isLoading: false,
+      isProductActionLoading: false,
+      dialogTitle: '',
+      dialogText: '',
+      dialogIcon: '',
+      submitRequestValidationError: '',
+      expandedProductCode: '',
+      AccountEnum: Account,
+      orgProductsFees: [],
+      orgProductFeeCodes: [],
+      isProductActionCompleted: false,
+      isVariableFeeAccount: computed(() => {
+        const accessType:any = currentOrganization.value.accessType
+        return ([AccessType.GOVM, AccessType.GOVN].includes(accessType)) || false
+      }),
+      canManageAccounts: computed((): boolean => {
+        // check for role and account can have service fee (GOVM and GOVN account)
+        return currentUser?.roles?.includes(Role.StaffManageAccounts) && localState.isVariableFeeAccount
+      }),
+      /** Return any sub-product that has a status indicating activity **/
+      subProduct: computed((): OrgProduct => {
+        return productList.value?.find(product =>
+          !!product.parentCode && product.subscriptionStatus !== ProductStatus.NOT_SUBSCRIBED
+        )
+      })
+    })
+
+    const setSelectedProduct = async (productDetails) => {
+      // add/remove product from the currentselectedproducts store
+      const productCode = productDetails.code
+      const forceRemove = productDetails.forceRemove
+      if (productCode) {
+        addToCurrentSelectedProducts({ productCode: productCode, forceRemove })
+      }
+    }
+
+    const loadProduct = async () => {
+      // refactor on next ticket
+      try {
+        await getOrgProducts(currentOrganization.value.id)
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log('Error while loading products ', err)
+      }
+    }
+
+    const setup = async () => {
+      localState.isLoading = true
+      resetCurrentSelectedProducts()
+      await loadProduct()
+      // if staff need to load product fee also
+      if (localState.canManageAccounts) {
+        localState.orgProductsFees = await syncCurrentAccountFees(currentOrganization.value.id)
+        localState.orgProductFeeCodes = await fetchOrgProductFeeCodes()
+      }
+
+      localState.isLoading = false
+    }
+
+    onMounted(async () => {
+      setAccountChangedHandler(setup)
+      await setup()
+    })
+
+    const toggleProductDetails = (productCode:string) => {
+      // control product expand here to collapse all other product
+      localState.expandedProductCode = productCode
+    }
+    const orgProductDetails = (product) => {
+      const { code: productCode, subscriptionStatus } = product
+
+      let productData
+      if (localState.orgProductsFees && localState.orgProductsFees.length > 0) {
+        const orgProd = localState.orgProductsFees.filter((orgProduct) => orgProduct.product === productCode)
+        productData = (orgProd && orgProd[0])
+      }
+
+      if (!productData && subscriptionStatus !== ProductStatus.NOT_SUBSCRIBED) {
+        // set default value
+        productData = {
+          'product': productCode,
+          'applyFilingFees': true,
+          'serviceFeeCode': productCode === 'ESRA' ? 'TRF03' : 'TRF01'
+        }
+      }
+
+      return productData || {}
+    }
+
+    const closeError = () => {
+      confirmDialog.value.close()
+    }
+
+    /** Product status message content for CautionBox component */
+    const mhrSubProductMsgContent = (): ProductStatusMsgContentIF => {
+      // Return when no sub-products in pending or rejected state
+      if (!localState.subProduct) return
+
+      const helpEmail = localState.subProduct.description === TaskType.MHR_LAWYER_NOTARY
+        ? 'bcolhelp@gov.bc.ca'
+        : 'bcregistries@gov.bc.ca'
+
+      switch (localState.subProduct.subscriptionStatus) {
+        case ProductStatus.PENDING_STAFF_REVIEW:
+          return {
+            status: localState.subProduct.subscriptionStatus,
+            icon: 'mdi-clock-outline',
+            color: 'darkGray',
+            msg: `Your application for Qualified Supplier – ${userAccessDisplayNames[localState.subProduct.description]}
+          access is under review. You will receive email notification once your request has been reviewed.`
+          }
+        case ProductStatus.REJECTED:
+          return {
+            status: localState.subProduct.subscriptionStatus,
+            icon: 'mdi-alert',
+            color: 'error',
+            msg: `Your application for Qualified Supplier – ${userAccessDisplayNames[localState.subProduct.description]}
+                access has been rejected. Refer to your notification email or contact
+                <a href="mailto:${helpEmail}">${helpEmail}</a> for details. You can submit a new Qualified Supplier
+                access request once you have all of the required information.`
+          }
+        case ProductStatus.ACTIVE:
+        default:
+          return null
+      }
+    }
+
+    const submitProductRequest = async () => {
+      try {
+        if (currentSelectedProducts.value.length === 0) {
+          localState.submitRequestValidationError = 'Select at least one product or service to submit request'
+        } else {
+          localState.submitRequestValidationError = ''
+          const productsSelected: OrgProductCode[] = currentSelectedProducts.value.map((code: any) => {
+            return { productCode: code }
+          })
+
+          const addProductsRequestBody: OrgProductsRequestBody = {
+            subscriptions: productsSelected
+          }
+          await addOrgProducts(addProductsRequestBody)
+          await setup()
+
+          // show confirm modal
+          localState.dialogTitle = 'Access Requested'
+          localState.dialogText = 'Request has been submitted. Account will immediately have access to the requested ' +
+              'product and service unless staff review is required.'
+          localState.dialogIcon = 'mdi-check'
+          confirmDialog.value.open()
+        }
+      } catch (ex) {
+        // open when error
+        localState.dialogTitle = 'Product Request Failed'
+        localState.dialogText = ''
+        localState.dialogIcon = 'mdi-alert-circle-outline'
+        confirmDialog.value.open()
+
+        // eslint-disable-next-line no-console
+        console.log('Error while trying to submit product request')
+      }
+    }
+
+    const saveProductFee = async (accountFees) => {
+      const accountFee = { accoundId: currentOrganization.value.id, accountFees }
+      localState.isProductActionLoading = true
+      localState.isProductActionCompleted = false
+      try {
+        await updateAccountFees(accountFee)
+        localState.orgProductsFees = await syncCurrentAccountFees(currentOrganization.value.id)
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log('Error while updating product fee ', err)
+      } finally {
+        localState.isProductActionLoading = false
+        localState.isProductActionCompleted = true
+      }
+    }
+
+    return {
+      setup,
+      closeError,
+      saveProductFee,
+      orgProductDetails,
+      setSelectedProduct,
+      toggleProductDetails,
+      submitProductRequest,
+      mhrSubProductMsgContent,
+      confirmDialog,
+      productList,
+      currentUser,
+      currentOrganization,
+      currentSelectedProducts,
+      AccountStatus,
+      ProductStatus,
+      ProductEnum,
+      ...toRefs(localState)
+    }
   }
 })
-export default class ProductPackage extends Mixins(AccountChangeMixin) {
-  @State(useOrgStore) public currentOrganization!: Organization
-  @State(useUserStore) public currentUser!: KCUserProfile
-  @State(useOrgStore) public productList!: OrgProduct[]
-  @State(useOrgStore) public currentSelectedProducts!: []
-
-  @Action(useOrgStore) public resetCurrentSelectedProducts!:() => void
-
-  @Action(useOrgStore) public getOrgProducts!:(orgId: number) => Promise<OrgProduct>
-  @Action(useOrgStore) public addOrgProducts!:(product:OrgProductsRequestBody) => Promise<OrgProduct>
-  @Action(useOrgStore) public addToCurrentSelectedProducts!:(productCode:any) => Promise<void>
-  @Action(useOrgStore) public syncCurrentAccountFees!:(accoundId:number) => Promise<AccountFee[]>
-  @Action(useOrgStore) public fetchOrgProductFeeCodes!:() => Promise<OrgProductFeeCode>
-  @Action(useOrgStore) public updateAccountFees!:(accountFee) => Promise<any>
-
-  public isBtnSaved = false
-  public disableSaveBtn = false
-  public isLoading: boolean = false
-  public isProductActionLoading: boolean = false
-  public dialogTitle = ''
-  public dialogText = ''
-  public dialogIcon = ''
-  public submitRequestValidationError = ''
-  public expandedProductCode: string = ''
-  public AccountEnum = Account
-  public orgProductsFees:any = ''
-  public orgProductFeeCodes:any = ''
-  public isProductActionCompleted: boolean = false
-
-  $refs: {
-      confirmDialog: InstanceType<typeof ModalDialog>
-  }
-
-  public async setSelectedProduct (productDetails) {
-    // add/remove product from the currentselectedproducts store
-    const productCode = productDetails.code
-    const forceRemove = productDetails.forceRemove
-    if (productCode) {
-      this.addToCurrentSelectedProducts({ productCode: productCode, forceRemove })
-    }
-  }
-
-  private async setup () {
-    this.isLoading = true
-    this.resetCurrentSelectedProducts()
-    await this.loadProduct()
-    // if staff need to load product fee also
-    if (this.canManageAccounts) {
-      this.orgProductsFees = await this.syncCurrentAccountFees(this.currentOrganization.id)
-      this.orgProductFeeCodes = await this.fetchOrgProductFeeCodes()
-    }
-
-    this.isLoading = false
-  }
-
-  public toggleProductDetails (productCode:string) {
-    // controll product expand here to collapse all other product
-    this.expandedProductCode = productCode
-  }
-
-  public async mounted () {
-    this.setAccountChangedHandler(this.setup)
-    await this.setup()
-  }
-
-  public async loadProduct () {
-    // refactor on next ticket
-    try {
-      await this.getOrgProducts(this.currentOrganization.id)
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log('Error while loading products ', err)
-    }
-  }
-
-  private get canManageAccounts () {
-    // check for role and account can have service fee (GOVM and GOVN account)
-    return this.currentUser?.roles?.includes(Role.StaffManageAccounts) && this.isVariableFeeAccount()
-  }
-  private orgProductDetails (product) {
-    const { code: productCode, subscriptionStatus } = product
-
-    let productData
-    if (this.orgProductsFees && this.orgProductsFees.length > 0) {
-      const orgProd = this.orgProductsFees.filter((orgProduct) => orgProduct.product === productCode)
-      productData = (orgProd && orgProd[0])
-    }
-
-    if (!productData && subscriptionStatus !== ProductStatus.NOT_SUBSCRIBED) {
-      // set default value
-      productData = {
-        'product': productCode,
-        'applyFilingFees': true,
-        'serviceFeeCode': productCode === 'ESRA' ? 'TRF03' : 'TRF01'
-      }
-    }
-
-    return productData || {}
-  }
-
-  private isVariableFeeAccount () {
-    const accessType:any = this.currentOrganization.accessType
-    return ([AccessType.GOVM, AccessType.GOVN].includes(accessType)) || false
-  }
-
-  private closeError () {
-    this.$refs.confirmDialog.close()
-  }
-
-  private async submitProductRequest () {
-    try {
-      if (this.currentSelectedProducts.length === 0) {
-        this.submitRequestValidationError = 'Select at least one product or service to submit request'
-      } else {
-        this.submitRequestValidationError = ''
-        const productsSelected: OrgProductCode[] = this.currentSelectedProducts.map((code: any) => {
-          return { productCode: code }
-        })
-
-        const addProductsRequestBody: OrgProductsRequestBody = {
-          subscriptions: productsSelected
-        }
-        await this.addOrgProducts(addProductsRequestBody)
-        await this.setup()
-
-        // show confirm modal
-        this.dialogTitle = 'Access Requested'
-        this.dialogText = 'Request has been submitted. Account will immediately have access to the requested ' +
-          'product and service unless staff review is required.'
-        this.dialogIcon = 'mdi-check'
-        this.$refs.confirmDialog.open()
-      }
-    } catch (ex) {
-      // open when error
-      this.dialogTitle = 'Product Request Failed'
-      this.dialogText = ''
-      this.dialogIcon = 'mdi-alert-circle-outline'
-      this.$refs.confirmDialog.open()
-
-      // eslint-disable-next-line no-console
-      console.log('Error while trying to submit product request')
-    }
-  }
-
-  public async saveProductFee (accountFees) {
-    const accountFee = { accoundId: this.currentOrganization.id, accountFees }
-    this.isProductActionLoading = true
-    this.isProductActionCompleted = false
-    try {
-      await this.updateAccountFees(accountFee)
-      this.orgProductsFees = await this.syncCurrentAccountFees(this.currentOrganization.id)
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log('Error while updating product fee ', err)
-    } finally {
-      this.isProductActionLoading = false
-      this.isProductActionCompleted = true
-    }
-  }
-}
 </script>
 
 <style lang="scss" scoped>
