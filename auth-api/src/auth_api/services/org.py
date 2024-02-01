4271b9c5a6cbd7c88d3c067f450b5befc4bd7248
Travis Semple <travis8814@gmail.com>
2024-02-01 10:29:34 -0800

    Add in branchName to auth-api to update pay-api

--- auth-api/src/auth_api/services/org.py
+++ auth-api/src/auth_api/services/org.py
@@ -199,7 +199,8 @@ class Org:  # pylint: disable=too-many-public-methods
         pay_request = {
             'accountId': org_model.id,
             # pay needs the most unique idenitfier.So combine name and branch name
-            'accountName': org_name_for_pay
+            'accountName': org_name_for_pay,
+            'branchName': org_model.branch_name
         }
 
         if payment_method:
