cf06309be2bd6bffc089877ca7365155238029d7
Travis Semple <travis8814@gmail.com>
2024-02-28 11:19:48 -0800

    20037 - Fix NSF unlock receipt (#2742)
    * Small changes to pass in all the params to generate a receipt
    
    * lint + tests
    
    * remove unused import
    
    * Add in decent test data
    
    * remove comments
    
    * Update unit test

--- queue_services/account-mailer/src/account_mailer/worker.py
+++ queue_services/account-mailer/src/account_mailer/worker.py
@@ -106,16 +106,15 @@ async def process_event(event_message: dict, flask_app):
             logo_url = email_msg.get('logo_url')
 
             email_dict = {
-                'account_name': email_msg.get('toOrgName'),
+                'template_vars': email_msg,
                 'logo_url': logo_url,
                 'template_name': template_name,
                 'subject': subject,
                 'org_id': org_id,
                 'admin_coordinator_emails': admin_coordinator_emails,
-                'filing_identifier': email_msg.get('filing_identifier'),
             }
 
-            email_dict = account_unlock.process(email_msg=email_dict, token=token)
+            email_dict = account_unlock.process(data=email_dict, token=token)
 
         elif message_type == MessageType.ACCOUNT_CONFIRMATION_PERIOD_OVER.value:
             template_name = TemplateType.ACCOUNT_CONF_OVER_TEMPLATE_NAME.value
