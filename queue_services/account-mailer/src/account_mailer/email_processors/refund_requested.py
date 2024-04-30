23ecce837cad5cc0245a05adadcce65803a8f612
Travis Semple <travis8814@gmail.com>
2024-04-30 16:02:41 -0700

    Refactor account-mailer.

--- queue_services/account-mailer/src/account_mailer/email_processors/refund_requested.py
+++ queue_services/account-mailer/src/account_mailer/email_processors/refund_requested.py
@@ -21,10 +21,9 @@ from jinja2 import Template
 from account_mailer.email_processors import generate_template
 
 
-def process(event_message: dict) -> dict:
+def process(email_msg: dict) -> dict:
     """Build the email for Payment Completed notification."""
-    current_app.logger.debug('refund_request notification: %s', event_message)
-    email_msg = event_message.get('data')
+    current_app.logger.debug('refund_request notification: %s', email_msg)
     template_name = 'bcol_refund_request_email'
     recepients = current_app.config.get('REFUND_REQUEST').get('bcol').get('recipients')
     refund_date = datetime.strptime(email_msg.get('refundDate'), '%Y%m%d').strftime('%Y-%m-%d')
