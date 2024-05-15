fd26d4b29f6e673e385463d2d274c56bedc8792f
Travis Semple <travis8814@gmail.com>
2024-05-14 17:00:51 -0700

    Change logging a bit, fix make files so it's rapid fire.

--- queue_services/auth-queue/src/auth_queue/__init__.py
+++ queue_services/auth-queue/src/auth_queue/__init__.py
@@ -20,12 +20,14 @@ from auth_api.resources.ops import bp as ops_bp
 from auth_api.services.flags import flags
 from auth_api.services.gcp_queue import queue
 from auth_api.utils.cache import cache
+from auth_api.utils.util_logging import setup_logging
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-from auth_queue import config
+from auth_queue import config, _Config
 from auth_queue.resources.worker import bp as worker_endpoint
 
+setup_logging(os.path.join(_Config.PROJECT_ROOT, 'logging.conf'))  # important to do this first
 
 def register_endpoints(app: Flask):
     """Register endpoints with the flask application."""
