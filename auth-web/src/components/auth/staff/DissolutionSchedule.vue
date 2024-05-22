06c8641def09bc076ba4313ca7a6649f6a9e3554
Karim El Jazzar <122301442+JazzarKarim@users.noreply.github.com>
2024-05-22 16:32:31 -0700

    19026 - Implemented the number of businesses ready for D1 dissolution (#2831)
    * 19026 - Implemented the number of businesses ready for D1 dissolution
    
    * Updated the endpoint url
    
    * added fallback
    
    * removed fallback
    
    * Added spinner while waiting to grab the number of businesses ready for D1 Dissolution
    
    * removed unnecessary line
    
    * Updated as per my conversation with Travis
    
    * Changed in response to conversation with Travis part 2

--- auth-web/src/components/auth/staff/DissolutionSchedule.vue
+++ auth-web/src/components/auth/staff/DissolutionSchedule.vue
@@ -121,8 +121,8 @@ export default defineComponent({
   setup () {
     const state = reactive({
       menu: false,
-      numberOfBusinessesEdit: 0,
-      numberOfBusinessesNonEdit: 0,
+      numberOfBusinessesEdit: -1,
+      numberOfBusinessesNonEdit: -1,
       numberOfBusinessesRef: null,
       isEdit: false,
       isSaving: false
@@ -135,7 +135,7 @@ export default defineComponent({
       await staffStore.getDissolutionConfigurations()
 
       // Get the batch size current value (number of businesses to be dissolved per job run)
-      const numDissolutions = staffStore.involuntaryDissolutionConfigurations.configurations.find(
+      const numDissolutions = staffStore.involuntaryDissolutionConfigurations?.configurations?.find(
         config => config.name === 'NUM_DISSOLUTIONS_ALLOWED').value
       state.numberOfBusinessesNonEdit = parseInt(numDissolutions)
     })
