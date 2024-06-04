b4eb472fd586caa20c3fa840c399a453a79f94cf
Jia Xu <Jia.Xu@gov.bc.ca>
2024-06-04 11:55:46 -0700

    20159 - Add view_address permission to admin account (#2848)
    * add view_address permission to admin account
    
    * tweaks
    
    * package update
    
    * PR feedback
    
    * solve merge conflict

---
+++ auth-api/migrations/versions/2024_05_30_39b4f331c003_admin_view_address_permission.py
@@ -0,0 +1,26 @@
+"""add view address permission to admin
+
+Revision ID: 39b4f331c003
+Revises: 41fa6588c76e
+Create Date: 2024-05-30 20:21:15.422128
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '39b4f331c003'
+down_revision = '41fa6588c76e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        "INSERT INTO permissions (membership_type_code, actions) VALUES ('ADMIN', 'view_address')"
+    )
+
+
+def downgrade():
+    op.execute('DELETE FROM permissions WHERE membership_type_code=\'ADMIN\' and actions=\'view_address\'')
