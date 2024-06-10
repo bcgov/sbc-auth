b9824b75636e3ccb9321eb8bf6d3ad4b4bb52ee0
Travis Semple <travis8814@gmail.com>
2024-06-10 14:35:47 -0700

    Small fix for unlock account

--- queue_services/auth-queue/src/auth_queue/resources/worker.py
+++ queue_services/auth-queue/src/auth_queue/resources/worker.py
@@ -119,7 +119,8 @@ def process_pay_lock_unlock_event(event_message: SimpleCloudEvent):
     elif message_type == QueueMessageTypes.NSF_UNLOCK_ACCOUNT.value:
         org.status_code = OrgStatus.ACTIVE.value
         org.suspension_reason_code = None
-        publish_to_mailer(QueueMessageTypes.NSF_UNLOCK_ACCOUNT.value, data)
+        # Unlock requires a significant amount of data to generate a receipt, just carry forward queue data.
+        publish_to_mailer(QueueMessageTypes.NSF_UNLOCK_ACCOUNT.value, queue_data)
 
     org.flush()
     db.session.commit()
