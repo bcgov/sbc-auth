64ddb9a359b5dd1ac68e97a80b0e13a1b22c5b74
Travis Semple <travis8814@gmail.com>
2024-06-20 12:31:43 -0700

    Remove AUTH_WEB_TOKEN_CONFIRM_PATH

--- auth-api/src/auth_api/services/org.py
+++ auth-api/src/auth_api/services/org.py
@@ -858,7 +858,7 @@ class Org:  # pylint: disable=too-many-public-methods
         task = TaskModel.find_by_task_relationship_id(task_relationship_type=task_relationship_type,
                                                       relationship_id=relationship_id)
         context_path = f'review-account/{task.id}'
-        app_url = f"{g.get('origin_url', '')}/{current_app.config.get('AUTH_WEB_TOKEN_CONFIRM_PATH')}"
+        app_url = f"{g.get('origin_url', '')}/"
         review_url = f'{app_url}/{context_path}'
         first_name = user.firstname
         last_name = user.lastname
@@ -887,7 +887,7 @@ class Org:  # pylint: disable=too-many-public-methods
             notification_type = QueueMessageTypes.NON_BCSC_ORG_REJECTED_NOTIFICATION.value
         else:
             return  # Don't send mail for any other status change
-        app_url = f"{origin_url}/{current_app.config.get('AUTH_WEB_TOKEN_CONFIRM_PATH')}"
+        app_url = f"{origin_url}/"
         data = {
             'accountId': org_id,
             'emailAddresses': receipt_admin_emails,
@@ -913,7 +913,7 @@ class Org:  # pylint: disable=too-many-public-methods
             notification_type = QueueMessageTypes.GOVM_REJECTED_NOTIFICATION.value
         else:
             return  # Don't send mail for any other status change
-        app_url = f"{origin_url}/{current_app.config.get('AUTH_WEB_TOKEN_CONFIRM_PATH')}"
+        app_url = f"{origin_url}/"
         data = {
             'accountId': org_id,
             'emailAddresses': receipt_admin_email,
