e63237184a6aa46a122f6aedb2ffa14a0b6475cf
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2024-08-09 12:50:55 -0700

    Dump anon (#2957)

---
+++ jobs/gcp-db-data-masking/sql/mask.sql
@@ -0,0 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS anon CASCADE;
+SELECT anon.init();
+
+SECURITY LABEL FOR anon ON COLUMN users.last_name
+  IS 'MASKED WITH FUNCTION anon.fake_last_name()';
+
+SECURITY LABEL FOR anon ON COLUMN users.first_name
+  IS 'MASKED WITH FUNCTION anon.fake_first_name()';
+
+SECURITY LABEL FOR anon ON COLUMN users.email
+  IS 'MASKED WITH FUNCTION anon.fake_email()';
+
+SECURITY LABEL FOR anon ON COLUMN contacts.phone
+  IS 'MASKED WITH FUNCTION anon.random_phone()';
+
+SECURITY LABEL FOR anon ON COLUMN contacts.email
+  IS 'MASKED WITH FUNCTION anon.fake_email()';
+
+SELECT anon.anonymize_database();
