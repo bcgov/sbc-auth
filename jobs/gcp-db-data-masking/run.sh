130690fbb6716bc5eabf4e117a67f1785ce6fc04
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-08-08 23:58:50 -0700

    anonymize

--- jobs/gcp-db-data-masking/run.sh
+++ jobs/gcp-db-data-masking/run.sh
@@ -4,12 +4,17 @@ cd $root_dir
 
 echo "recreating sandbox db"
 gcloud sql instances restart "${DB_NAME}-tools"
+gcloud sql instances patch "${DB_NAME}-tools" --database-flags max_connections=0
+gcloud sql instances restart "${DB_NAME}-tools"
+
 gcloud --quiet sql databases delete $DB_NAME --instance="${DB_NAME}-tools"
 gcloud --quiet sql databases create $DB_NAME --instance="${DB_NAME}-tools"
 gsutil cp "gs://${DB_NAME}-dump-${ENV}/${DB_NAME}.sql.gz" ${DB_NAME}.sql.gz
 
-echo "starting mask script"
-sh db_mask.sh
+# TODO
+# ALTER SYSTEM SET max_connections = 1;
+# or REVOKE CONNECT ON DATABASE auth-db FROM PUBLIC;
+
 echo "loading dump into sandbox db"
 gcloud --quiet sql import sql "${DB_NAME}-tools" "gs://${DB_NAME}-dump-${ENV}/${DB_NAME}.sql.gz" --database=$DB_NAME --user=$DB_USER
 
@@ -29,3 +34,6 @@ echo "GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO auth;" >> r
 echo "applying readonly user changes ..."
 gsutil cp readonly.sql "gs://${DB_NAME}-dump-${ENV}/"
 gcloud --quiet sql import sql "${DB_NAME}-tools" "gs://${DB_NAME}-dump-${ENV}/readonly.sql" --database=$DB_NAME --user=$DB_USER
+gcloud --quiet sql import sql "${DB_NAME}-tools" "gs://${DB_NAME}-dump-${ENV}/mask.sql" --database=$DB_NAME --user=$DB_USER
+gcloud sql instances patch "${DB_NAME}-tools" --database-flags max_connections=500
+gcloud sql instances restart "${DB_NAME}-tools"
