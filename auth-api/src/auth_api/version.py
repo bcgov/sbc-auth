41633f5af66f3172ecd259534b0e059392505540
Avni Salhotra <avni.salhotra@gov.bc.ca>
2024-08-28 10:18:35 -0700

    update version number for release

--- auth-api/src/auth_api/version.py
+++ auth-api/src/auth_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.10.4'  # pylint: disable=invalid-name
+__version__ = '2.10.5'  # pylint: disable=invalid-name
