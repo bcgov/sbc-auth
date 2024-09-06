47795d865a1d7eda98119720db12d21021ba02da
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2024-09-06 09:08:26 -0700

    Feature poetry auth queue (#2995)

--- queue_services/auth-queue/app.py
+++ queue_services/auth-queue/app.py
@@ -21,5 +21,5 @@ from auth_queue import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '5001')
+    server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')
