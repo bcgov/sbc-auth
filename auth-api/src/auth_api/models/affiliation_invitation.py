b9609b82b8618504866fe817ae0a7ef9bee7274d
Patrick Wei <44277752+pwei1018@users.noreply.github.com>
2024-09-20 16:57:41 -0700

    Update affiliation_invitation.py

--- auth-api/src/auth_api/models/affiliation_invitation.py
+++ auth-api/src/auth_api/models/affiliation_invitation.py
@@ -232,10 +232,7 @@ class AffiliationInvitation(BaseModel):  # pylint: disable=too-many-instance-att
     def find_all_related_to_org(cls, org_id: int, search_filter=AffiliationInvitationSearch()):
         """Return all affiliation invitations that are related to the org (from org or to org) filtered by statuses."""
         query = db.session.query(AffiliationInvitation).filter(
-            or_(
-                AffiliationInvitation.to_org_id.cast(Integer) == org_id.cast(Integer),
-                AffiliationInvitation.from_org_id.cast(Integer) == org_id.cast(Integer),
-            )
+            or_(int(AffiliationInvitation.to_org_id) == int(org_id), int(AffiliationInvitation.from_org_id) == int(org_id))
         )
 
         return cls.filter_by(search_filter=search_filter, query=query)
