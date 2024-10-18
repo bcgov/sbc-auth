78310840d2e5e8dd086fa65ba653797aba7ad846
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2024-10-18 09:57:11 -0700

    update gunicorn configs (#3082)

--- queue_services/auth-queue/gunicorn_config.py
+++ queue_services/auth-queue/gunicorn_config.py
@@ -19,6 +19,7 @@ import os
 
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))  # pylint: disable=invalid-name
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '0'))  # pylint: disable=invalid-name
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))  # pylint: disable=invalid-name
 
 forwarded_allow_ips = '*'  # pylint: disable=invalid-name
