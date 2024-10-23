db88d8bd1481c5e85927143e9452ee6dad9433a3
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2024-10-22 18:12:17 -0700

    Cloud sql connector (#3094)

--- queue_services/account-mailer/src/account_mailer/__init__.py
+++ queue_services/account-mailer/src/account_mailer/__init__.py
@@ -26,13 +26,42 @@ from auth_api.services.flags import flags
 from auth_api.services.gcp_queue import queue
 from auth_api.utils.cache import cache
 from flask import Flask
+from google.cloud.sql.connector import Connector
 
 from account_mailer import config
 from account_mailer.resources.worker import bp as worker_endpoint
 
 
-def register_endpoints(app: Flask):
-    """Register endpoints with the flask application."""
+def getconn(connector: Connector, instance: str, unix_sock: str, database: str, user: str, password: str) -> object:
+    """Create a database connection.
+
+    Args:
+        unix_sock (str): The Unix socket path for the database connection.
+        database (str): The name of the database to connect to.
+        user (str): The username for the database.
+        password (str): The password for the database.
+
+    Returns:
+        object: A connection object to the database.
+    """
+    conn = connector.connect(
+        instance_connection_string=instance,
+        ip_type='unix',
+        socket_path=unix_sock,
+        user=user,
+        password=password,
+        db=database,
+        driver='pg8000'
+    )
+    return conn
+
+
+def register_endpoints(app: Flask) -> None:
+    """Register endpoints with the Flask application.
+
+    Args:
+        app (Flask): The Flask application instance.
+    """
     # Allow base route to match with, and without a trailing slash
     app.url_map.strict_slashes = False
 
@@ -44,10 +73,30 @@ def register_endpoints(app: Flask):
 
 
 def create_app(run_mode=os.getenv('DEPLOYMENT_ENV', 'production')) -> Flask:
-    """Return a configured Flask App using the Factory method."""
+    """Return a configured Flask App using the Factory method.
+
+    Args:
+        run_mode (str): The running mode of the application (e.g., production, development).
+
+    Returns:
+        Flask: The configured Flask application instance.
+    """
     app = Flask(__name__)
     app.config.from_object(config.get_named_config(run_mode))
     app.config['ENV'] = run_mode
+
+    if app.config.get('DB_UNIX_SOCKET'):
+        connector = Connector()
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+            'creator': lambda: getconn(
+                connector,
+                app.config.get('DB_UNIX_SOCKET').replace('/cloudsql/', ''),
+                app.config.get('DB_UNIX_SOCKET'),
+                app.config.get('DB_NAME'),
+                app.config.get('DB_USER'),
+                app.config.get('DATABASE_PASSWORD')
+            )
+        }
 
     db.init_app(app)
     flags.init_app(app)
