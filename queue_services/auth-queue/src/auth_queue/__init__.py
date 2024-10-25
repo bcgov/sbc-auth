0fa2d1ad12d066cf0dc38e6c872c89f58b861845
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-10-24 22:36:56 -0700

    switch to cloud sql connector for auth queue

--- queue_services/auth-queue/src/auth_queue/__init__.py
+++ queue_services/auth-queue/src/auth_queue/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """Resource package for the auth-queue service."""
 import os
+from dataclasses import dataclass
 
 from auth_api.exceptions import ExceptionHandler
 from auth_api.models import db
@@ -21,9 +22,37 @@ from auth_api.services.flags import flags
 from auth_api.services.gcp_queue import queue
 from auth_api.utils.cache import cache
 from flask import Flask
+from google.cloud.sql.connector import Connector
 
-from auth_queue import config
+from auth_queue import config as app_config
 from auth_queue.resources.worker import bp as worker_endpoint
+
+
+@dataclass
+class DBConfig:
+    """Database configuration settings."""
+    unix_sock: str
+    database: str
+    user: str
+    password: str
+
+
+def getconn(connector: Connector, db_config: DBConfig) -> object:
+    """Create a database connection.
+    Args:
+        connector (Connector): The Google Cloud SQL connector instance.
+        db_config (DBConfig): The database configuration.
+    Returns:
+        object: A connection object to the database.
+    """
+    return connector.connect(
+        instance_connection_string=db_config.unix_sock.replace('/cloudsql/', ''),
+        ip_type='private',
+        user=db_config.user,
+        password=db_config.password,
+        db=db_config.database,
+        driver='pg8000',
+    )
 
 
 def register_endpoints(app: Flask):
@@ -41,8 +70,20 @@ def register_endpoints(app: Flask):
 def create_app(run_mode=os.getenv('DEPLOYMENT_ENV', 'production')) -> Flask:
     """Return a configured Flask App using the Factory method."""
     app = Flask(__name__)
-    app.config.from_object(config.get_named_config(run_mode))
+    app.config.from_object(app_config.get_named_config(run_mode))
     app.config['ENV'] = run_mode
+
+    if app.config.get('DB_UNIX_SOCKET'):
+        connector = Connector(refresh_strategy='lazy')
+        db_config = DBConfig(
+            unix_sock=app.config.get('DB_UNIX_SOCKET'),
+            database=app.config.get('DB_NAME'),
+            user=app.config.get('DB_USER'),
+            password=app.config.get('DB_PASSWORD')
+        )
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+            'creator': lambda: getconn(connector, db_config)
+        }
 
     db.init_app(app)
     flags.init_app(app)
