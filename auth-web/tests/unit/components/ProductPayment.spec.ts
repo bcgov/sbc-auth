1e91cca6c91b06788b816911b9894c603d9ba4e2
Jia Xu <Jia.Xu@gov.bc.ca>
2025-01-14 15:53:42 -0800

    add unit test for ProductPayment

--- auth-web/tests/unit/components/ProductPayment.spec.ts
+++ auth-web/tests/unit/components/ProductPayment.spec.ts
@@ -1,29 +1,17 @@
-import { Account, ProductStatus } from '@/util/constants'
 import { createLocalVue, mount } from '@vue/test-utils'
-import ModalDialog from '@/components/auth/common/ModalDialog.vue'
-import Product from '@/components/auth/common/Product.vue'
+import OrgService from '@/services/org.services'
 import ProductPackage from '@/components/auth/account-settings/product/ProductPayment.vue'
 import Vuetify from 'vuetify'
 import { useOrgStore } from '@/stores/org'
 import { useUserStore } from '@/stores/user'
-import OrgService from '@/services/org.services'
 
 const vuetify = new Vuetify({})
-const pprProduct = {
-  code: 'PPR',
-  description: 'ppr',
-  url: 'url',
-  type: 'PARTNER',
-  subscriptionStatus: ProductStatus.NOT_SUBSCRIBED,
-  premiumOnly: false,
-  parentCode: '',
-  hidden: false,
-  needReview: false
-}
 
-const productList = [pprProduct] as any
+vi.mock('../../../src/services/user.services')
 
-// vi.mock('../../../src/services/org.services')
+// vi.mock('@/services/org.services', () => ({
+//   addProducts: vi.fn().mockResolvedValue({ data: null })
+// }))
 
 describe('Account settings ProductPackage.vue', () => {
   let wrapper: any
@@ -31,7 +19,7 @@ describe('Account settings ProductPackage.vue', () => {
 
   const ob = {
     'PAY_API_URL': 'https://pay-api-dev.apps.silver.devops.gov.bc.ca/api/v1',
-    'AUTH_API_URL': 'https://auth-api-post-dev.pathfinder.gov.bc.ca/api/v1',
+    'AUTH_API_URL': 'https://auth-api-post-dev.pathfinder.gov.bc.ca/api/v1'
   }
 
   sessionStorage['AUTH_API_CONFIG'] = JSON.stringify(ob)
@@ -46,10 +34,8 @@ describe('Account settings ProductPackage.vue', () => {
     } as any
     const orgStore = useOrgStore()
     orgStore.currentOrganization = {
-      name: 'test org',
-      orgType: Account.PREMIUM
+      name: 'test org'
     }
-    orgStore.productList = productList
 
     wrapperFactory = (propsData) => {
       return mount(ProductPackage, {
@@ -59,8 +45,7 @@ describe('Account settings ProductPackage.vue', () => {
           ...propsData
         },
         mocks: {
-          $t: (mock) => mock,
-          $te: (mock) => mock
+          $t: (mock) => mock
         }
       })
     }
@@ -86,43 +71,39 @@ describe('Account settings ProductPackage.vue', () => {
     expect(wrapper.find('h2').text()).toBe('Products and Payment')
   })
 
-  it('emits event when checkbox is clicked non-TOS product and shows "Product Added" dialog', async () => {
-    pprProduct.subscriptionStatus = ProductStatus.ACTIVE
-    // useOrgStore().addOrgProducts = vi.fn().mockResolvedValue([pprProduct])
-    OrgService.addProducts = vi.fn().mockResolvedValue({ data: [pprProduct] })
+  it('handles modal dialog add product correctly', async () => {
+    const orgStore = useOrgStore()
+    orgStore.currentSelectedProducts = [{ code: 'TEST_PRODUCT' }]
+    wrapper.vm.addProductOnAccountAdmin = true
+    const mockOpen = vi.fn()
+    wrapper.vm.$refs.confirmDialog.open = mockOpen
 
-    wrapper.vm.isLoading = false
-    await wrapper.vm.$nextTick()
+    const addProducts = vi.spyOn(OrgService, 'addProducts').mockResolvedValue(null)
 
-    const productComponents = wrapper.findAllComponents(Product)
-    expect(productComponents.length).toBe(productList.length)
-    const productComponent = productComponents.at(0)
+    await wrapper.vm.submitProductRequest()
 
-    const checkbox = productComponent.find("[data-test='check-product-PPR']")
-    await checkbox.trigger('change')
-    expect(checkbox.exists()).toBe(true)
-
-    expect(productComponent.vm.productSelected).toBe(true)
-    await wrapper.vm.$nextTick()
-
-    expect(wrapper.findComponent(ModalDialog).exists()).toBe(true)
-    const v = wrapper.findComponent(ModalDialog).html()
-    expect(wrapper.findComponent(ModalDialog).vm.title).toBe('Product Added')
-    expect(wrapper.findComponent(ModalDialog).vm.text).toBe('Your account now has access to the selected product.')
+    expect(addProducts).toHaveBeenCalled()
+    expect(mockOpen).toHaveBeenCalled()
+    expect(wrapper.vm.dialogTitle).toBe('Product Added')
+    expect(wrapper.vm.dialogText).toBe('Your account now has access to the selected product.')
+    expect(wrapper.vm.dialogError).toBe(false)
   })
 
-  it('failed dialog', async () => {
-    wrapper.vm.isLoading = false
-    await wrapper.vm.$nextTick()
+  it('handles modal dialog failed', async () => {
+    const orgStore = useOrgStore()
+    orgStore.currentSelectedProducts = [{ code: 'TEST_PRODUCT' }]
+    wrapper.vm.addProductOnAccountAdmin = true
+    const mockOpen = vi.fn()
+    wrapper.vm.$refs.confirmDialog.open = mockOpen
 
-    const productComponents = wrapper.findAllComponents(Product)
-    const productComponent = productComponents.at(0)
+    const addProducts = vi.spyOn(OrgService, 'addProducts').mockRejectedValue(new Error('Failed to add products'))
 
-    const checkbox = productComponent.find("[data-test='check-product-PPR']")
-    await checkbox.trigger('change')
-    await wrapper.vm.$nextTick()
+    await wrapper.vm.submitProductRequest()
 
-    expect(wrapper.findComponent(ModalDialog).exists()).toBe(true)
-    expect(wrapper.findComponent(ModalDialog).vm.title).toBe('Payment Update Failed')
+    expect(addProducts).toHaveBeenCalled()
+    expect(mockOpen).toHaveBeenCalled()
+    expect(wrapper.vm.dialogTitle).toBe('Product Request Failed')
+    expect(wrapper.vm.dialogText).toBe('')
+    expect(wrapper.vm.dialogError).toBe(true)
   })
 })
