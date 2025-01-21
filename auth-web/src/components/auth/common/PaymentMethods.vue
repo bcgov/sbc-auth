decde119918723de2c6e3fb6f125e715423faec8
Travis Semple <travis8814@gmail.com>
2025-01-21 11:45:13 -0800

    Fix focus going grey

--- auth-web/src/components/auth/common/PaymentMethods.vue
+++ auth-web/src/components/auth/common/PaymentMethods.vue
@@ -475,6 +475,7 @@ export default defineComponent({
   .v-input--radio-group__input {
     display: block
   }
+  
 }
 
 .payment-card {
@@ -489,6 +490,9 @@ export default defineComponent({
                 0 3px 1px -2px rgba(0,0,0,.2),
                 0 2px 2px 0 rgba(0,0,0,.14),
                 0 1px 5px 0 rgba(0,0,0,.12) !important;
+  }
+  &:focus {
+    outline: none;
   }
 }
 
