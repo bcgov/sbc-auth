984e800c983a82f794b6f246f811ff5720f1300f
Travis Semple <travis8814@gmail.com>
2025-02-24 09:10:21 -0800

    Add in some org logging (#3274)

--- auth-api/src/auth_api/resources/v1/org.py
+++ auth-api/src/auth_api/resources/v1/org.py
@@ -361,7 +361,7 @@ def get_organization_affiliations(org_id):
 
         # get affiliation identifiers and the urls for the source data
         affiliations = AffiliationModel.find_affiliations_by_org_id(org_id, env)
-        affiliations_details_list = asyncio.run(AffiliationService.get_affiliation_details(affiliations))
+        affiliations_details_list = asyncio.run(AffiliationService.get_affiliation_details(affiliations, org_id))
         # Use orjson serializer here, it's quite a bit faster.
         response, status = (
             current_app.response_class(
