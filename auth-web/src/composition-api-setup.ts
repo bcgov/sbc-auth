e1bfe48edb092ef22b642e828467d46087ad5f8d
Travis Semple <travis8814@gmail.com>
2025-02-24 17:12:55 -0800

    Update composition-api-setup.ts

--- auth-web/src/composition-api-setup.ts
+++ auth-web/src/composition-api-setup.ts
@@ -2,5 +2,6 @@ import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
 import { getPiniaStore } from '@/stores'
 Vue.use(VueCompositionAPI)
+ConfigHelper.saveConfigToSessionStorage()
 // Need to do this before Router gets hoisted.
 getPiniaStore()
