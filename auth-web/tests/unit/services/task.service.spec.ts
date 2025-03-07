b00df23571d6f5ec8ff62f3eea518a0063ef7196
Jia Xu <Jia.Xu@gov.bc.ca>
2025-03-07 11:20:27 -0800

    two unit fix

--- auth-web/tests/unit/services/task.service.spec.ts
+++ auth-web/tests/unit/services/task.service.spec.ts
@@ -1,24 +1,27 @@
 import { Task } from '@/models/Task'
 import TaskService from '../../../src/services/task.services'
 
+const mockTask = vi.hoisted(() => ({
+  data: [{
+    'accountId': 2628,
+    'created': new Date('2021-04-19T16:21:28.989168+00:00'),
+    'createdBy': 'BCREGTEST Jing SIXTEEN',
+    'dateSubmitted': new Date('2021-04-19T16:22:28.989168+00:00'),
+    'id': 44,
+    'modified': new Date('2021-04-19T16:23:28.989168+00:00'),
+    'name': 'sb 16.3',
+    'relationshipId': 3674,
+    'relationshipStatus': 'PENDING_STAFF_REVIEW',
+    'relationshipType': 'PRODUCT',
+    'status': 'OPEN',
+    'type': 'Wills Registry'
+  }] as Task[]
+}))
+
 const mockob = {
   'PAY_API_URL': 'https://pay-api-dev.pathfinder.gov.bc.ca/api/v1',
   'AUTH_API_URL': 'https://auth-api-dev.silver.devops.gov.bc.ca/api/v1'
 }
-const mockTask: Task[] = [{
-  'accountId': 2628,
-  'created': new Date('2021-04-19T16:21:28.989168+00:00'),
-  'createdBy': 'BCREGTEST Jing SIXTEEN',
-  'dateSubmitted': new Date('2021-04-19T16:22:28.989168+00:00'),
-  'id': 44,
-  'modified': new Date('2021-04-19T16:23:28.989168+00:00'),
-  'name': 'sb 16.3',
-  'relationshipId': 3674,
-  'relationshipStatus': 'PENDING_STAFF_REVIEW',
-  'relationshipType': 'PRODUCT',
-  'status': 'OPEN',
-  'type': 'Wills Registry'
-}]
 
 const mocks = vi.hoisted(() => ({
   get: vi.fn().mockReturnValue(mockTask),
@@ -47,7 +50,7 @@ describe('Task service', () => {
 
   it('call getTaskById() for task Details ', () => {
     TaskService.getTaskById(1).then((response) => {
-      expect(response).toEqual(mockTask)
+      expect(response).toEqual(mockTask.data)
     })
   })
   it('call fetchTasks() for all tasks  ', () => {
@@ -59,19 +62,19 @@ describe('Task service', () => {
     }
 
     TaskService.fetchTasks(taskFilter).then((response) => {
-      expect(response).toEqual(mockTask)
+      expect(response).toEqual(mockTask.data)
     })
   })
 
   it('call approvePendingTask() to approve request ', async () => {
-    TaskService.approvePendingTask(mockTask).then((response) => {
-      expect(response).toEqual(mockTask)
+    TaskService.approvePendingTask(mockTask.data).then((response) => {
+      expect(response).toEqual(mockTask.data)
     })
   })
 
   it('call rejectPendingTask() to reject request ', async () => {
-    TaskService.rejectPendingTask(mockTask).then((response) => {
-      expect(response).toEqual(mockTask)
+    TaskService.rejectPendingTask(mockTask.data).then((response) => {
+      expect(response).toEqual(mockTask.data)
     })
   })
 })
