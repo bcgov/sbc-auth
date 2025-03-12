42ddd96d571490dcfe11970238ef78e3c7569697
Travis Semple <travis8814@gmail.com>
2025-03-12 13:08:53 -0700

    tweak

--- auth-web/src/stores/codes.ts
+++ auth-web/src/stores/codes.ts
@@ -78,9 +78,7 @@ export const useCodesStore = defineStore('codes', () => {
 
   async function getProductPaymentMethods (productCode?: string): Promise<any> {
     const data = await CodesService.getProductPaymentMethods(productCode)
-    if (data?.BUSINESSSearch) {
-      data.BUSINESS_SEARCH = data.BUSINESSSearch // Force to match enum.
-    }
+    data.BUSINESS_SEARCH = data?.BUSINESSSearch // Force to match enum.
     state.productPaymentMethods = data
   }
 
