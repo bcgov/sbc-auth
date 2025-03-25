7604082d6fa9a364cf9af54a7af4b56ae157a21e
Travis Semple <travis8814@gmail.com>
2025-03-25 13:43:30 -0700

    linting issue

--- auth-api/src/auth_api/services/api_gateway.py
+++ auth-api/src/auth_api/services/api_gateway.py
@@ -23,7 +23,6 @@ from structured_logging import StructuredLogging
 from auth_api.exceptions import BusinessException, Error
 from auth_api.models.membership import Membership as MembershipModel
 from auth_api.models.org import Org as OrgModel
-from auth_api.models.user_status_code import UserStatusCode
 from auth_api.services.authorization import check_auth
 from auth_api.services.flags import flags
 from auth_api.services.keycloak import KeycloakService
