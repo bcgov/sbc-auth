d1d630a8664fa6df4286f97b4e67f70bc56131dd
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-04-01 08:32:11 -0700

    remove unused gcp key

--- queue_services/auth-queue/src/auth_queue/config.py
+++ queue_services/auth-queue/src/auth_queue/config.py
@@ -77,8 +77,6 @@ class _Config:  # pylint: disable=too-few-public-methods
 
     # PUB/SUB - PUB: account-mailer-dev, SUB: auth-event-dev and namex-nr-state-dev
     ACCOUNT_MAILER_TOPIC = os.getenv('ACCOUNT_MAILER_TOPIC', 'account-mailer-dev')
-    # If blank in PUB/SUB, this should match the https endpoint the subscription is pushing to.
-    GCP_AUTH_KEY = os.getenv('AUTHPAY_GCP_AUTH_KEY', None)
 
     PAY_API_URL = os.getenv('PAY_API_URL') + os.getenv('PAY_API_VERSION')
 
