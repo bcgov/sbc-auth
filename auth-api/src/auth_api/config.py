5174c5667fec2260f045e1f76b894e7cb8fd6085
Travis Semple <travis8814@gmail.com>
2025-04-09 10:07:26 -0700

    27099 - Fix HTTP_ORIGIN issue for API users (#3360)

--- auth-api/src/auth_api/config.py
+++ auth-api/src/auth_api/config.py
@@ -317,6 +317,7 @@ NrQw+2OdQACBJiEHsdZzAkBcsTk7frTH4yGx0VfHxXDPjfTj4wmD6gZIlcIr9lZg
     API_GW_CONSUMERS_API_URL = "https://bcregistry-bcregistry-mock.apigee.net/mockTarget"
     API_GW_CONSUMERS_SANDBOX_API_URL = "https://bcregistry-bcregistry-mock.apigee.net/mockTarget"
     API_GW_CONSUMER_EMAIL = "test.all.mc@gov.bc.ca"
+    WEB_APP_URL = "https://localhost.com/"
 
 
 class ProdConfig(_Config):  # pylint: disable=too-few-public-methods
