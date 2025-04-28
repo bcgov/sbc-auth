a09d918043d0b97828d432327bf6b02bf5b5765e
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2025-04-28 10:09:24 -0700

    27100-govm invite fix (#3369)

--- auth-web/src/components/auth/mixins/NextPageMixin.vue
+++ auth-web/src/components/auth/mixins/NextPageMixin.vue
@@ -57,8 +57,6 @@ export default class NextPageMixin extends Vue {
         orgName = encodeURIComponent(btoa(this.currentAccountSettings?.label))
         if (this.currentUser.roles.includes(Role.Staff)) {
           return `/${Pages.SEARCH_BUSINESS}`
-        } else if (this.currentUser.roles.includes(Role.ExternalStaffReadonly)) {
-          return `/${Pages.STAFF_DASHBOARD_ACTIVE}`
         } else if (!this.userProfile?.userTerms?.isTermsOfUseAccepted) {
           return `/${Pages.USER_PROFILE_TERMS}`
         } else if (this.currentOrganization && this.currentOrganization.statusCode === AccountStatus.PENDING_INVITE_ACCEPT) {
@@ -70,6 +68,8 @@ export default class NextPageMixin extends Vue {
         } else if (this.currentOrganization && this.currentOrganization.statusCode === AccountStatus.PENDING_STAFF_REVIEW) {
           // redirect to pending page
           return `/${Pages.SETUP_GOVM_ACCOUNT_SUCCESS}`
+        } else if (this.currentUser.roles.includes(Role.ExternalStaffReadonly)) {
+          return `/${Pages.STAFF_DASHBOARD_ACTIVE}`
         } else {
           return dashboardUrl
         }
