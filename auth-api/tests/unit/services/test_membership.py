55599c660ad7d4406ce1d818f8d116617a6a985e
phungchithanh <phungchithanh@gmail.com>
2025-05-14 10:33:37 -0600

    linting fix

--- auth-api/tests/unit/services/test_membership.py
+++ auth-api/tests/unit/services/test_membership.py
@@ -22,8 +22,9 @@ from unittest.mock import ANY, patch
 from sbc_common_components.utils.enums import QueueMessageTypes
 
 import auth_api
-from auth_api.models import MembershipStatusCode as MembershipStatusCodeModel, User as UserModel
+from auth_api.models import MembershipStatusCode as MembershipStatusCodeModel
 from auth_api.models import OrgStatus as OrgStatusModel
+from auth_api.models import User as UserModel
 from auth_api.models.dataclass import Activity
 from auth_api.services import ActivityLogPublisher
 from auth_api.services import Membership as MembershipService
@@ -172,7 +173,7 @@ def test_remove_member_removes_group_to_the_user(publish_mock, session, monkeypa
 
 
 def test_has_nsf_or_suspended_membership_returns_true(session, monkeypatch):
-    """Test that has_nsf_or_suspended_membership returns True when have an NSF_SUSPENDED or SUSPENDED membership."""    
+    """Test that has_nsf_or_suspended_membership returns True when have an NSF_SUSPENDED or SUSPENDED membership."""
     user = session.query(UserModel).first()
     user_id = user.id
     existing_org_status = session.query(OrgStatusModel).filter_by(code=OrgStatus.NSF_SUSPENDED.value).first()
@@ -188,7 +189,7 @@ def test_has_nsf_or_suspended_membership_returns_true(session, monkeypatch):
 
 
 def test_has_nsf_or_suspended_membership_returns_false(session, monkeypatch):
-    """Test that has_nsf_or_suspended_membership returns False when no NSF_SUSPENDED or SUSPENDED membership."""    
+    """Test that has_nsf_or_suspended_membership returns False when no NSF_SUSPENDED or SUSPENDED membership."""
     user = session.query(UserModel).first()
     user_id = user.id
     existing_org_status = session.query(OrgStatusModel).filter_by(code=OrgStatus.ACTIVE.value).first()
