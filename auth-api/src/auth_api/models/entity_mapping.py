35ec1d6320ddf0f93d45522719d00d81143eef24
Travis Semple <travis8814@gmail.com>
2025-05-27 15:34:15 -0700

    remove unused function

--- auth-api/src/auth_api/models/entity_mapping.py
+++ auth-api/src/auth_api/models/entity_mapping.py
@@ -13,10 +13,3 @@ class EntityMapping(BaseModel):  # pylint: disable=too-few-public-methods, too-m
     business_identifier = Column("business_identifier", String(75), index=True, unique=True, nullable=True)
     bootstrap_identifier = Column("bootstrap_identifier", String(75), index=True, unique=True, nullable=True)
     nr_identifier = Column("nr_identifier", String(75), index=True, unique=True, nullable=True)
-
-    @classmethod
-    def find_by_nr_identifier(cls, nr_identifier: str):
-        """Find an Affiliation Mapping instance that matches the provided id."""
-        if nr_identifier is None:
-            return None
-        return cls.query.filter_by(nr_identifier=nr_identifier).first()
