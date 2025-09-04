f29b1feceb83168aef556bb787c2e70a8efe007b
Travis Semple <travis8814@gmail.com>
2025-09-04 13:25:36 -0700

    change lint and others

--- auth-api/src/auth_api/services/activity_log.py
+++ auth-api/src/auth_api/services/activity_log.py
@@ -103,6 +103,7 @@ class ActivityLog:  # pylint: disable=too-many-instance-attributes
             ActivityAction.ACCOUNT_ADDRESS_CHANGE.value: ActivityLog._account_address_changes,
             ActivityAction.AUTHENTICATION_METHOD_CHANGE.value: ActivityLog._authentication_method_changes,
             ActivityAction.ACCOUNT_SUSPENSION.value: ActivityLog._account_suspension,
+            ActivityAction.ACCOUNT_DEACTIVATION.value: ActivityLog._account_deactivation,
             ActivityAction.ADD_PRODUCT_AND_SERVICE.value: ActivityLog._adding_products_and_services,
         }.get(activity.action)
         return mapping(activity) if (mapping) else activity.action
@@ -196,6 +197,11 @@ class ActivityLog:  # pylint: disable=too-many-instance-attributes
         """Account was suspended due to [Suspension reason]."""
         suspension_reason = activity.item_value.replace("_", " ")
         return f"The account was suspended due to {suspension_reason}"
+
+    @staticmethod
+    def _account_deactivation(activity: ActivityLogModel) -> str:
+        """Account was deactivated."""
+        return "The account was deactivated"
 
     @staticmethod
     def _adding_products_and_services(activity: ActivityLogModel) -> str:
