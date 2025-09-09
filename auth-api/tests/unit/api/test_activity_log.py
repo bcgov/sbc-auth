5c8ef5d00d46af63261860e745524485467abeb3
Travis Semple <travis8814@gmail.com>
2025-09-09 13:44:32 -0700

    unit test fix

--- auth-api/tests/unit/api/test_activity_log.py
+++ auth-api/tests/unit/api/test_activity_log.py
@@ -154,7 +154,7 @@ def test_fetch_activity_log_masking(client, jwt, session):  # pylint:disable=unu
             ActivityAction.STATEMENT_RECIPIENT_CHANGE.value,
             "",
             "None|test@example.com|enabled",
-            "Changed statement recipient(s) to test@example.com. Statement notification emails are disabled.",
+            "Changed statement recipient(s) to test@example.com. Statement notification emails are enabled.",
         ),
     ],
 )
