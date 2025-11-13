576ca40fed4ff0294dba1524e98c559d021efd4c
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2025-11-13 15:28:43 -0800

    mask update

--- auth-api/src/auth_api/schemas/affiliation_invitation.py
+++ auth-api/src/auth_api/schemas/affiliation_invitation.py
@@ -38,6 +38,7 @@ class AffiliationInvitationSchema(BaseSchema):  # pylint: disable=too-many-ances
             "expires_on",
             "accepted_date",
             "status",
+            "token",
             "type",
             "affiliation_id",
             "additional_message",
@@ -54,7 +55,8 @@ class AffiliationInvitationSchemaPublic(AffiliationInvitationSchema):
     """This is the public schema for the Affiliation Invitation model that masks the email."""
 
     @post_dump(pass_many=False)
-    def _mask_recipient_email_field(self, data, many):  # noqa: ARG002
+    def mask_email_hide_token(self, data, many):  # noqa: ARG002
         """Mask recipient email field."""
         data["recipient_email"] = mask_email(data.get("recipient_email"))
+        data.pop("token", None)
         return data
