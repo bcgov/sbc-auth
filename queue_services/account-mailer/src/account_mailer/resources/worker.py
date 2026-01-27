d8593f303d809949d557c03e42ecad49813df92c
Jia Xu <Jia.Xu@gov.bc.ca>
2026-01-27 10:39:47 -0800

    tweaks

--- queue_services/account-mailer/src/account_mailer/resources/worker.py
+++ queue_services/account-mailer/src/account_mailer/resources/worker.py
@@ -398,7 +398,6 @@ def handle_product_actions(message_type, email_msg):
     email_dict = common_mailer.process(
         **{
             "org_id": email_msg.get("orgId", None),
-            "org_name": email_msg.get("orgName", None),
             "recipients": email_msg.get("emailAddresses"),
             "template_name": TemplateType[f"{QueueMessageTypes(message_type).name}_TEMPLATE_NAME"].value,
             "subject": subject_type.format(subject_descriptor=subject_descriptor),
