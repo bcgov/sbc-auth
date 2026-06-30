2c8bd586044bb5a2473b924cc09f7d72d530b1d7
Jia Xu <Jia.Xu@gov.bc.ca>
2026-06-30 14:30:26 -0700

    Alert Messages during vendor redirect

---
+++ auth-web/src/components/auth/vendor-linking/VendorLinkingAccountSelectAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-alert
+    class="vendor-linking-account-select-alert mb-6"
+    type="warning"
+    prominent
+    data-test="vendor-linking-account-select-alert"
+  >
+    {{ $t('vendorLinkingAccountSelectAlert') }}
+  </v-alert>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'VendorLinkingAccountSelectAlert'
+})
+</script>
---
+++ auth-web/src/components/auth/vendor-linking/VendorLinkingLoginAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-alert
+    class="vendor-linking-login-alert mb-6"
+    type="warning"
+    prominent
+    data-test="vendor-linking-login-alert"
+  >
+    <div class="font-weight-bold mb-2">
+      {{ $t('vendorLinkingLoginAlertTitle') }}
+    </div>
+    <div>{{ $t('vendorLinkingLoginAlertBody') }}</div>
+    <div class="mt-3">
+      {{ $t('vendorLinkingLoginAlertNote') }}
+    </div>
+  </v-alert>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'VendorLinkingLoginAlert'
+})
+</script>
